1ca545b0ef2bd7a0905f47083f5241f3f552c0a6
Robert Jensen <jensen.robert@gmail.com>
2012-03-08 14:42:33 +0100

    Now fully functional data logger, free of LabView!

--- main.py
+++ main.py
@@ -9,11 +9,25 @@ import nidaq
 import xgs600
 
 
-"""
-quit = False
-ion_gauge_pressure = 0
-ion_pump_pressure = 0
-turbo_pump_temp = 0
+def sqlTime():
+	sqltime = datetime.now().isoformat(' ')[0:19]
+	return(sqltime)
+
+def sqlInsert(query):
+	try:
+		cnxn = pyodbc.connect('DSN=new_db')
+		#cnxn = pyodbc.connect('DSN=robertj')
+		cursor = cnxn.cursor()
+	except:
+		print "Unable to connect to database"
+		return()
+	try:
+		cursor.execute(query)
+		cnxn.commit()
+	except:
+		print "SQL-error, query written below:"
+		print query
+	cnxn.close()
 
 class XGSClass(threading.Thread):
 	def run(self):
@@ -26,93 +40,112 @@ class XGSClass(threading.Thread):
 			except:
 				print "av"
 
-
 class AnalogClass(threading.Thread):
 	def run(self):
 		global ion_pump_pressure
 		global turbo_pump_temp
 		while not quit:
 			(ion_pump_pressure,turbo_pump_temp) = nidaq.readPressureAndTemperature()
+
+class XGSSaver(threading.Thread):
+	
+	def __init__(self):
+		threading.Thread.__init__(self)
+		self.last_recorded_value = 0
+		self.last_recorded_time = 1
+	
+	def run(self):
+		while not quit:
+			time.sleep(1)
+			time_trigged = (time.time() - self.last_recorded_time) > 60
+			val_trigged = not (self.last_recorded_value * 0.9 < ion_gauge_pressure < self.last_recorded_value * 1.1)
+			#print (time.time() - self.last_recorded_time)
+			if (time_trigged or val_trigged) and ion_gauge_pressure > 0:
+				self.last_recorded_value = ion_gauge_pressure
+				self.last_recorded_time = time.time()
+				meas_time = sqlTime()
+				val = "%.5g" % ion_gauge_pressure
+				gauge_sql = "insert into pressure_tof_iongauge set time=\"" +  meas_time + "\", pressure = " + val
+				print gauge_sql
+				sqlInsert(gauge_sql)
+		
+class IonPumpSaver(threading.Thread):
+	
+	def __init__(self):
+		threading.Thread.__init__(self)
+		self.last_recorded_value = 0
+		self.last_recorded_time = 1
+	
+	def run(self):
+		while not quit:
+			time.sleep(1)
+			time_trigged = (time.time() - self.last_recorded_time) > 60
+			val_trigged = not (self.last_recorded_value * 0.9 < ion_pump_pressure < self.last_recorded_value * 1.1)
+			#print (time.time() - self.last_recorded_time)
+			if (time_trigged or val_trigged) and (ion_pump_pressure > 0):
+				self.last_recorded_value = ion_pump_pressure
+				self.last_recorded_time = time.time()
+				meas_time = sqlTime()
+				val = "%.5g" % ion_pump_pressure
+				gauge_sql = "insert into pressure_tof_ionpump set time=\"" +  meas_time + "\", pressure = " + val
+				print gauge_sql
+				sqlInsert(gauge_sql)
+
+class TurboTempSaver(threading.Thread):
+	
+	def __init__(self):
+		threading.Thread.__init__(self)
+		self.last_recorded_value = 0
+		self.last_recorded_time = 1
+	
+	def run(self):
+		while not quit:
+			time.sleep(1)
+			time_trigged = (time.time() - self.last_recorded_time) > 60
+			val_trigged = not ((self.last_recorded_value - 0.5) < turbo_pump_temp < (self.last_recorded_value + 0.5))
+			#print (time.time() - self.last_recorded_time)
+			if (time_trigged or val_trigged) and (turbo_pump_temp > 0):
+				self.last_recorded_value = turbo_pump_temp
+				self.last_recorded_time = time.time()
+				meas_time = sqlTime()
+				val = "%.5g" % turbo_pump_temp
+				gauge_sql = "insert into temperature_tof_turbopump set time=\"" +  meas_time + "\", temperature = " + val
+				print gauge_sql
+				sqlInsert(gauge_sql)
+
 				
-				
+quit = False
+ion_gauge_pressure = 0
+ion_pump_pressure = 0
+turbo_pump_temp = 0
+
+	
 P = XGSClass()
 A = AnalogClass()
+PressSaver = XGSSaver()
+IonSaver = IonPumpSaver()
+TurboSaver = TurboTempSaver()
 P.start()
 A.start()
+PressSaver.start()
+IonSaver.start()
+TurboSaver.start()
 
-for i in range(0,50):
-	time.sleep(0.25)
-	print ion_gauge_pressure
-	print ion_pump_pressure
-	print turbo_pump_temp
-quit = True
-
-def sqlTime():
-	sqltime = datetime.now().isoformat(' ')[0:19]
-	return(sqltime)
-
-def sqlInsert(query):
+while not quit:
 	try:
-		#cnxn = pyodbc.connect('DSN=new_db')
-		cnxn = pyodbc.connect('DSN=robertj')
-		cursor = cnxn.cursor()
+		time.sleep(1)
+		#print "Ion Gauge: " + "%.5g" % ion_gauge_pressure
+		#print "Ion Pump: " +  "%.5g" % ion_pump_pressure
+		#print "Turbo Temperature: " +  "%.5g" % turbo_pump_temp
 	except:
-		print "Unable to connect to database"
-		return()
-	try:
-		cursor.execute(query)
-		cnxn.commit()
-	except:
-		print "SQL-error, query written below:"
-		print query
-	cnxn.close()
-	
+		quit = True
 
-	
-
-#sqlInsert("test")
-"""
-def writePressuresToFile():
-	pressures = xgs600.readAllPressures()
-	try:
-		if pressures[1]>0:
-			f = open('c:\pressures\main_chamber.pressure','w+')
-			f.write(str(pressures[1]))
-			f.close()
-		#print pressures[1]
-	except:
-		print pressures
-
-
-
-while True:
-	writePressuresToFile()
 
 """
 	
-last_recorded_pressure = 0
-last_recorded_press_time = 1
-	
-#Get the gauge pressure from the driver, set negetive pressure if an error is returned
-pressures = xgs600.readAllPressures()
-try:
-	gauge_pressure = pressures[1]
-except:
-	gauge_pressure = -9
-
-time_trigged = (time.time() - last_recorded_press_time) > 60
-val_trigged = not (last_recorded_pressure * 0.9 < gauge_pressure < last_recorded_pressure * 1.1)
-if (time_trigged or val_trigged) and gauge_pressure > 0:
-	meas_time = sqlTime()
-	gauge_pressure = "%.5g" % gauge_pressure
-	gauge_sql = "insert into pressure_tof_iongauge set time=\"" +  meas_time + "\", pressure = " + gauge_pressure
-	sqlInsert(gauge_sql)
-
 
 #Get the analog measurements
 (ionpump,turbo) = nidaq.readPressureAndTemperature()
-
-
 
 ionpump_pressure = "%.5g" % ionpump
 turbo_temp = "%.5g" % turbo
@@ -145,4 +178,23 @@ while True:
 	q.task_done()
 	
 	
+"""
+
+"""
+
+def writePressuresToFile():
+	pressures = xgs600.readAllPressures()
+	try:
+		if pressures[1]>0:
+			f = open('c:\pressures\main_chamber.pressure','w+')
+			f.write(str(pressures[1]))
+			f.close()
+		#print pressures[1]
+	except:
+		print pressures
+
+
+
+while True:
+	writePressuresToFile()
 """
