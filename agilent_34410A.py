9a23b76d931bee28c9d7291505f406133c089d7c
Robert Jensen <jensen.robert@gmail.com>
2012-04-04 11:08:44 +0200

    Turned the driver into a self-contained Python class

--- agilent_34410A.py
+++ agilent_34410A.py
@@ -1,44 +1,67 @@
 import time
 
-def scpi_comm(command):
-	#ser = serial.Serial(0)
-	#comm = "#00" + command + "\r"
+class Agilent34410ADriver:
 
-    f = open('/dev/usbtmc0', 'w')
-    f.write(command)
-    f.close()
+    def scpi_comm(self,command):
+        #ser = serial.Serial(0)
+        #comm = "#00" + command + "\r"
 
-    time.sleep(0.01)
-    return_string = ""    
-    if command[-1]=='?':
-        a = ' '
-        f = open('/dev/usbtmc0', 'r')
-        while not (ord(a) == 10):
-            a = f.read(1)
-            return_string += a
+        f = open('/dev/usbtmc0', 'w')
+        f.write(command)
         f.close()
-    return return_string
-    
+
+        time.sleep(0.01)
+        return_string = ""    
+        if command[-1]=='?':
+            a = ' '
+            f = open('/dev/usbtmc0', 'r')
+            while not (ord(a) == 10):
+                a = f.read(1)
+                return_string += a
+            f.close()
+        return return_string
+        
+    def readSoftwareVersion(self):
+        version_string = scpi_comm("*IDN?")
+        return(version_string)
+
+    def resetDevice(self):
+        scpi_comm("*RST")
+        return(True)
+
+    def deviceClear(self):
+        scpi_comm("*abort")
+        return(True)
+
+    def clearErrorQueue(self):
+        error = scpi_comm("*ESR?")
+        scpi_comm("*cls")
+        return(error)
 
 
-def readSoftwareVersion():
-	version_string = scpi_comm("*IDN?")
-	return(version_string)
+    def configCurrentMeasurement(self):
+        scpi_comm("CONFIGURE:CURRENT:DC") #Take parameter to also be able to select AC
+        return(True)
 
-def resetDevice():
-	scpi_comm("*RST")
-	return(True)
+    def configResistanceMeasurement(self):
+        scpi_comm("CONFIGURE:RESISTANCE") #Take parameter to also be able to select 4W
+        return(True)
 
-def deviceClear():
-	scpi_comm("*abort")
-	return(True)
+    def selectMeasurementFunction(self,function):
+        values = ['CAPACITANCE','CONTINUITY','CURRENT','DIODE','FREQUENCY','RESISTANCE','TEMPERATURE','VOLTAGE']
 
-def read():
-    value = scpi_comm("READ?")
-    return value
-    
+    def readConfiguration(self):
+        response = scpi_comm("CONFIGURE?")
+        response = response.replace(' ',',')
+        conf = response.split(',')
+        conf_string = "Measurement type: " + conf[0] + "\nRange: " + conf[1] + "\nResolution: " + conf[2]
+        return(conf_string)
 
-print read()
-#deviceClear()
-#resetDevice()
-#readSoftwareVersion()
+
+    def read(self):
+        value = self.scpi_comm("READ?")
+        return value
+
+
+driver  = Agilent34410ADriver()
+print driver.read()
