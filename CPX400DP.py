2dd216af2b953cafe305a42ee9277becb498acb0
Robert Jensen <jensen.robert@gmail.com>
2012-04-09 22:44:15 +0200

    Added a few more functions

--- CPX400DP.py
+++ CPX400DP.py
@@ -10,11 +10,45 @@ class CPX400DPDriver(SCPI):
 
     def __init__(self):
         SCPI.__init__(self,'/dev/ttyACM0','serial')
+
+    def GetLock(self):
+        function_string = 'IFLOCK'
+        self.scpi_comm(function_string)
+        function_string = 'IFLOCK?'
+        status = int(self.scpi_comm(function_string))
+        return_message = ""
+        if status == 0:
+            return_message = "Not successful"
+        if status == -1:
+            return_message = "Device alreadt locked"
+        if status == 1:
+            return_message = "Lock acquired"
+        return(return_message)
+
+    def SetVoltage(self, output,value):
+        if not (output == 1 or output == 2):
+            raise InterfaceOutOfBoundsError(output)
+        function_string = 'V' + str(output)
+        return(self.scpi_comm(function_string))
+
+    def SetCurrentLimit(self, output,value):
+        if not (output == 1 or output == 2):
+            raise InterfaceOutOfBoundsError(output)
+        function_string = 'I' + str(output)
+        return(self.scpi_comm(function_string))
     
     def ReadSetVoltage(self, output):
         if not (output == 1 or output == 2):
             raise InterfaceOutOfBoundsError(output)
         function_string = 'V' + str(output) + '?'
+        return(self.scpi_comm(function_string))
+
+    def ReadCurrentLimit(self, output):
+        if not (output == 1 or output == 2):
+            raise InterfaceOutOfBoundsError(output)
+        function_string = 'I' + str(output) + '?'
+        return(self.scpi_comm(function_string))
+
 
     def ReadActualVoltage(self, output):
         if not (output == 1 or output == 2):
