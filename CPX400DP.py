c8e3cc738c92ad2336bfe090e0192410a363bfca
Robert Jensen <jensen.robert@gmail.com>
2012-04-09 22:16:52 +0200

    Driver now able to read a few parameters from the device

--- CPX400DP.py
+++ CPX400DP.py
@@ -4,4 +4,27 @@ class CPX400DPDriver(SCPI):
 
     def __init__(self):
         SCPI.__init__(self,'/dev/ttyACM0','serial')
+        
+    
+    def ReadSetVoltage(self, output):
+        if output == 1 or output == 2:
+            function_string = 'V' + str(output) + '?'
+            return(self.scpi_comm(function_string))
+        else:
+            return False
 
+
+    def ReadActualVoltage(self, output):
+        if output == 1 or output == 2:
+            function_string = 'V' + str(output) + 'O?'
+            return(self.scpi_comm(function_string))
+        else:
+            return False
+        
+
+    def ReadActualCurrent(self, output):
+        if output == 1 or output == 2:
+            function_string = 'I' + str(output) + 'O?'
+            return(self.scpi_comm(function_string))
+        else:
+            return False
