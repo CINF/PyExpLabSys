1dae295276097b5b7e984b3e5f284beaf5f0a269
Robert Jensen <jensen.robert@gmail.com>
2012-04-09 22:24:49 +0200

    Better (i think?) error-handling

--- CPX400DP.py
+++ CPX400DP.py
@@ -1,30 +1,29 @@
 from SCPI import SCPI
 
+class InterfaceOutOfBoundsError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
 class CPX400DPDriver(SCPI):
 
     def __init__(self):
         SCPI.__init__(self,'/dev/ttyACM0','serial')
-        
     
     def ReadSetVoltage(self, output):
-        if output == 1 or output == 2:
-            function_string = 'V' + str(output) + '?'
-            return(self.scpi_comm(function_string))
-        else:
-            return False
-
+        if not (output == 1 or output == 2):
+            raise InterfaceOutOfBoundsError(output)
+        function_string = 'V' + str(output) + '?'
 
     def ReadActualVoltage(self, output):
-        if output == 1 or output == 2:
-            function_string = 'V' + str(output) + 'O?'
-            return(self.scpi_comm(function_string))
-        else:
-            return False
-        
+        if not (output == 1 or output == 2):
+            raise InterfaceOutOfBoundsError(output)
+        function_string = 'V' + str(output) + 'O?'
+        return(self.scpi_comm(function_string))
 
     def ReadActualCurrent(self, output):
-        if output == 1 or output == 2:
-            function_string = 'I' + str(output) + 'O?'
-            return(self.scpi_comm(function_string))
-        else:
-            return False
+        if not (output == 1 or output == 2):
+            raise InterfaceOutOfBoundsError(output)
+        function_string = 'I' + str(output) + 'O?'
+        return(self.scpi_comm(function_string))
