94ea790a47b255df4dea8125602142e221e16119
Robert Jensen <jensen.robert@gmail.com>
2012-08-15 14:20:40 +0200

    First actual working temperature controller

--- devel_test.py
+++ devel_test.py
@@ -5,6 +5,8 @@ import subprocess
 import random
 import curses
 import RTD_Calculator
+import PID
+
 #output = 'print'
 output = 'curses'
 
@@ -57,19 +59,26 @@ rtd = RTD_Calculator.RTD_Calculator(tc_temperature,cal_resistance)
 
 CPXdriver.SetVoltage(0)
 CPXdriver.OutputStatus(True)
+pid = PID.PID()
+pid.UpdateSetpoint(0)
 
-#print a
-#print type(a)
 quit = False
-#quit = True
+power = 0
+i = 22*50
 while not quit:
     try:
-        time.sleep(0.5)
-        setpoint = VBoxRead('setpoint')
-        #setpoint = random.randrange(0, 100)/100.0
-        CPXdriver.SetVoltage(setpoint)
+        i = i+1
+        time.sleep(0.25)
+        #setpoint = VBoxRead('setpoint')
+        setpoint = i/50.0
+
+        pid.UpdateSetpoint(setpoint)
+        
+        #RIGHT NOW WHENEVER POWER IS REPLACED WIDTH VOLTAGE!!!!!
+        CPXdriver.SetVoltage(power)
         I = CPXdriver.ReadActualCurrent()
         U = CPXdriver.ReadActualVoltage()
+
         if I<-99999998:
             del CPXdriver
             time.sleep(1)
@@ -78,12 +87,18 @@ while not quit:
             TellTheWorld("PS:" + str(U/I) + "         ",[2,5])
         else:
             TellTheWorld("PS: No current         ",[2,5])
+
         rtd_val = AgilentDriver.Read()
-        TellTheWorld("Setpoint: " + str(setpoint),[2,4])
+        TellTheWorld("Setpoint: " + str(setpoint) + "  ",[2,4])
         TellTheWorld("Agilent: " + str(rtd_val) + "    ",[2,6])
         RTD_Temp = rtd.FindTemperature(rtd_val)
         VBoxWrite("RTD_Temp",RTD_Temp)
-        TellTheWorld("Temperature: " + str(RTD_Temp) + "    ",[2,7])
+        power = pid.WantedPower(RTD_Temp)
+        TellTheWorld("Temperature: " + str(RTD_Temp) + "       ",[2,7])
+        
+        TellTheWorld("Actual Current: " + str(I) + "           ",[2,9])
+        TellTheWorld("Actual Voltage: " + str(U) + "          ",[2,10])
+        TellTheWorld("Wanted power: " + str(power) + "         ",[2,11])
 
     except:
         quit = True
