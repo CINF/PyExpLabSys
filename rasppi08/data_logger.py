03dc0c17c8022a9df1e9a16c4a9e016dde2e8a12
Thomas Andersen <andersens.mail@gmail.com>
2012-11-26 11:20:47 +0100

    Anders' changes to pressure and temperature logging of omicron

--- rasppi08/data_logger.py
+++ rasppi08/data_logger.py
@@ -4,11 +4,11 @@ import time
 from datetime import datetime
 import serial
 import sys
-sys.path.append('../')
+#sys.path.append('../')
 import MySQLdb
 
 
-import NGC2D
+import NGC2D as NGC2D
 import omegabus as OmegaBus
 
 def sqlTime():
@@ -19,7 +19,7 @@ def sqlInsert(query):
 	try:
 		cnxn = MySQLdb.connect(host="servcinf",user="omicron",passwd="omicron",db="cinfdata")
 		#cnxn = pyodbc.connect('DSN=new_db')
-		#cnxn = pyodbc.connect('DSN=robertj'
+		#cnxn = pyodbc.connect('DSN=robertj')
 		cursor = cnxn.cursor()
 	except:
 		print "Unable to connect to database"
@@ -33,33 +33,46 @@ def sqlInsert(query):
 	cnxn.close()
 
 class NGC2DClass(threading.Thread):
-	def run(self):
-		global ion_gauge_pressure
-		while not quit:
-			time.sleep(0.5)
-			press = NGC2D.ReadPressure()
-			#print press
-			try:
-				ion_gauge_pressure = press
-				isinstance(press, float)
-			except:
-				print "av-NGC2DClass"
+        def __init__(self):
+                threading.Thread.__init__(self)
+                self.NGC2D = NGC2D.NGC2D_comm('/dev/ttyUSB1')
+                #self.NGC2D = NGC2D.NGC2D_comm()
+        def run(self):
+                global ion_gauge_pressure
+                while not quit:
+                        time.sleep(1)
+                        press = self.NGC2D.ReadPressure()
+                        #print press
+                        try:
+                                ion_gauge_pressure = float(press)
+                                isinstance(press, float)
+                        except:
+                                print "av-NGC2DClass"
 
 class OmegaBusClass(threading.Thread):
+        def __init__(self):
+                threading.Thread.__init__(self)
+                self.omega = OmegaBus.OmegaBus('/dev/ttyUSB0')
 	def run(self):
 		global temp_oldclustersource
 		global temp_nanobeam
 		while not quit:
-			time.sleep(0.2)
-			temp0 = OmegaBus.ReadValue(0) + 273
-			print temp0
-			time.sleep(0.2)
-			temp1 = OmegaBus.ReadValue(1) + 273
-			print temp1
+			time.sleep(1)
+			temp0 = self.omega.ReadValue(1)# + 273.15
+			#print 'temp 0: ' + str(temp0)
+			time.sleep(1)
+			temp1 = self.omega.ReadValue(2)# + 273.15
+			#print 'temp 1: ' + str(temp1)
+			#time.sleep(1)
+			#temp2 = self.omega.ReadValue(2)# + 273.15
+			#print 'temp 2: ' + str(temp2)
+			#time.sleep(1)
+			#temp3 = self.omega.ReadValue(3)# + 273.15
+			#print 'temp 3: ' + str(temp3)
 			try:
-				temp_oldclustersource = temp0
+				temp_nanobeam = temp0
 				isinstance(temp0, float)
-				temp_nanobeam = temp1
+				temp_oldclustersource = temp1
 				isinstance(temp1, float)
 			except:
 				print "av-OmegaBusClass"
@@ -73,7 +86,7 @@ class NGC2DSaver(threading.Thread):
 	def run(self):
 		while not quit:
 			time.sleep(1)
-			time_trigged = (time.time() - self.last_recorded_time) > 300
+			time_trigged = (time.time() - self.last_recorded_time) > 30
 			#print time_trigged
 			val_trigged = not (self.last_recorded_value * 0.9 < ion_gauge_pressure < self.last_recorded_value * 1.1)
                         #print val_trigged
@@ -97,16 +110,16 @@ class temp_oldclustersource_Saver(threading.Thread):
 	def run(self):
 		while not quit:
 			time.sleep(1)
-			time_trigged = (time.time() - self.last_recorded_time) > 300
+			time_trigged = (time.time() - self.last_recorded_time) > 30
 			val_trigged = not (self.last_recorded_value - 2.0 < temp_oldclustersource < self.last_recorded_value + 2)
-			if (time_trigged or val_trigged) and temp_oldclustersource > 0:
+			if (time_trigged or val_trigged) and temp_oldclustersource < 300:
 				self.last_recorded_value = temp_oldclustersource
 				self.last_recorded_time = time.time()
 				meas_time = sqlTime()
 				val = "%.5g" % temp_oldclustersource
 				sql_statement = "insert into temperature_oldclusterce_aggregation set time=\"" +  meas_time + "\", temperature = " + val
 				print sql_statement
-				sqlInsert(sql_statement)
+				#sqlInsert(sql_statement)
 
 class temp_nanobeam_Saver(threading.Thread):
 	def __init__(self):
@@ -117,9 +130,9 @@ class temp_nanobeam_Saver(threading.Thread):
 	def run(self):
 		while not quit:
 			time.sleep(1)
-			time_trigged = (time.time() - self.last_recorded_time) > 300
+			time_trigged = (time.time() - self.last_recorded_time) > 30
 			val_trigged = not (self.last_recorded_value - 2.0 < temp_nanobeam < self.last_recorded_value + 2.0)
-			if (time_trigged or val_trigged) and temp_nanobeam > 0:
+			if (time_trigged or val_trigged) and temp_nanobeam < 300:
 				self.last_recorded_value = temp_nanobeam
 				self.last_recorded_time = time.time()
 				meas_time = sqlTime()
@@ -130,9 +143,9 @@ class temp_nanobeam_Saver(threading.Thread):
 		
 
 quit = False
-ion_gauge_pressure = 0
-temp_oldclustersource = 0
-temp_nanobeam = 0
+ion_gauge_pressure = -1
+temp_oldclustersource = 1000
+temp_nanobeam = 1000
 
 	
 P = NGC2DClass()
