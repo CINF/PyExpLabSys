9af162aaafa1658539ba1a9d0ddb674fa73d7d0b
Robert Jensen <jensen.robert@gmail.com>
2013-01-19 22:34:34 +0100

    Added network support and PEP8'ified the code

--- agilent_34410A.py
+++ agilent_34410A.py
@@ -1,44 +1,53 @@
 from SCPI import SCPI
 
-    
+
 class Agilent34410ADriver(SCPI):
 
     def __init__(self):
-        SCPI.__init__(self,'/dev/usbtmc0','file')
+        #SCPI.__init__(self, '/dev/usbtmc0', 'file')
+        SCPI.__init__(self, 'microreactor-agilent-34410A', 'lan')
 
-    def configCurrentMeasurement(self):
-        self.scpi_comm("CONFIGURE:CURRENT:DC") #Take parameter to also be able to select AC
+    def config_current_measurement(self):
+        # FIXME: Take parameter to also be able to select AC
+        self.scpi_comm("CONFIGURE:CURRENT:DC")
         return(True)
 
-    def configResistanceMeasurement(self):
-        self.scpi_comm("CONFIGURE:RESISTANCE") #Take parameter to also be able to select 4W
+    def config_resistance_measurement(self):
+        # FIXME: Take parameter to also be able to select 4W
+        self.scpi_comm("CONFIGURE:RESISTANCE")
         return(True)
 
-    def SelectMeasurementFunction(self,function):
-        values = ['CAPACITANCE','CONTINUITY','CURRENT','DIODE','FREQUENCY','RESISTANCE','FRESISTANCE','TEMPERATURE','VOLTAGE']
+    def select_measurement_function(self, function):
+        values = ['CAPACITANCE', 'CONTINUITY', 'CURRENT', 'DIODE', 'FREQUENCY',
+                  'RESISTANCE', 'FRESISTANCE', 'TEMPERATURE', 'VOLTAGE']
         return_value = False
         if function in values:
             return_value = True
             function_string = "FUNCTION " + "\"" + function + "\""
             self.scpi_comm(function_string)
-            
         return(return_value)
 
-    def readConfiguration(self):
+    def read_configuration(self):
         response = self.scpi_comm("CONFIGURE?")
-        response = response.replace(' ',',')
+        response = response.replace(' ', ',')
         conf = response.split(',')
-        conf_string = "Measurement type: " + conf[0] + "\nRange: " + conf[1] + "\nResolution: " + conf[2]
+        conf_string = ""
+        conf_string += "Measurement type: " + conf[0] + "\n"
+        conf_string += "Range: " + conf[1] + "\n"
+        conf_string += "Resolution: " + conf[2]
         return(conf_string)
 
-    def setAutoInputZ(self, auto=False):
+    def set_auto_input_z(self, auto=False):
         if auto:
             self.scpi_comm("VOLT:IMP:AUTO ON")
         else:
             self.scpi_comm("VOLT:IMP:AUTO OFF")
 
-    def Read(self):
+    def read(self):
         value = float(self.scpi_comm("READ?"))
         return value
 
 
+if __name__ == "__main__":
+    driver = Agilent34410ADriver()
+    print driver.read()
