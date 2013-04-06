9da70a5afea17d3715e73155bc140c2319f09c9a
Robert Jensen <jensen.robert@gmail.com>
2013-04-06 21:50:17 +0200

    Seperated the threaded SQL-saver into its own class

---
+++ SQL-saver.py
@@ -0,0 +1,23 @@
+import threading
+import Queue
+import MySQLdb
+
+class sql_saver(threading.Thread):
+    def __init__(self, queue, username):
+        threading.Thread.__init__(self)
+        self.queue = queue
+        self.cnxn = MySQLdb.connect(host="servcinf", user=username, passwd=username, db="cinfdata")
+        self.cursor = self.cnxn.cursor()
+        self.commits = 0
+        self.commit_time = 0
+        
+    def run(self):
+        while True:
+            self.queue
+            start = time.time()
+            query = self.queue.get()
+            self.cursor.execute(query)
+            self.cnxn.commit()
+            self.commits += 1
+            self.commit_time = time.time() - start
+        self.cnxn.close()
