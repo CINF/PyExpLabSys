327f46d4ef30382bf2bfad84c84c5713849d6ad0
Robert Jensen <jensen.robert@gmail.com>
2013-09-16 21:34:39 +0200

    First working version of datalogger for sputterchamber.

--- rasppi21/data_logger.py
+++ rasppi21/data_logger.py
@@ -6,7 +6,12 @@ import MySQLdb
 import socket
 import sys
 
-import credentials
+import sql_credentials as credentials
+
+sys.path.append('../')
+import FindSerialPorts
+import InficonSQM160 as qcm
+import polyscience_4100
 
 def network_comm(host, port, string):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -34,27 +39,82 @@ def sqlInsert(query):
 	print query
     cnxn.close()
 
-class ChillerReader(threading.Thread):
+class QcmReader(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
-        self.chiller_temp = -9999
-        self.chiller_flow = -9999
-        self.chiller_temp_amb = -9999
-        self.chiller_pressure = -9999
-        self.chiller_temp_setpoint = -9999
+        for port in ports:
+            try:
+                inficon = qcm.InficonSQM160('/dev/' + port)
+                inficon.show_version()
+                print 'QCM: ' + port
+                break
+            except IndexError:
+                pass
+        ports.remove(port)
+        self.inficon = inficon
+        self.frequency = -1
+        self.lifetime = -1
+        self.thickness = -1
 
     def run(self):
         while not quit:
-            try:
-                self.chiller_temp = network_comm('130.225.86.120', 9759, 'read_temperature')
-                self.chiller_flow = network_comm('130.225.86.120', 9759, 'read_flow_rate')
-                self.chiller_temp_amb = network_comm('130.225.86.120', 9759, 'read_ambient_temperature')
-                self.chiller_pressure = network_comm('130.225.86.120', 9759, 'read_pressure')
-                self.chiller_temp_setpoint = network_comm('130.225.86.120', 9759, 'read_setpoint')
-                time.sleep(5)
-            except:
-                print 'Did not fetch values'
+            self.frequency = self.inficon.frequency()
+            self.crystal_life = self.inficon.crystal_life()
+            self.thickness =  self.inficon.thickness()
+            time.sleep(5)
 
+
+class QcmSaver(threading.Thread):
+    def __init__(self, reader):
+        threading.Thread.__init__(self)
+        self.last_recorded_time = 1
+        self.reader = reader
+
+    def run(self):
+        while not quit:
+            time.sleep(1)
+            time_trigged = (time.time() - self.last_recorded_time) > 200
+            if (time_trigged):
+                self.last_recorded_time = time.time()
+                meas_time = sqlTime()
+                sql_thickness = "insert into sputterchamber_qcm_thickness set time=\"" +  meas_time + "\", value = " + str(self.reader.thickness)
+                sql_crystal_life = "insert into sputterchamber_qcm_crystal_life set time=\"" +  meas_time + "\", value = " + str(self.reader.crystal_life)
+                sql_frequency = "insert into sputterchamber_qcm_frequency set time=\"" +  meas_time + "\", value = " + str(self.reader.frequency)
+                print sql_frequency
+
+                sqlInsert(sql_thickness)
+                sqlInsert(sql_crystal_life)
+                sqlInsert(sql_frequency)
+                time.sleep(10)
+
+class ChillerReader(threading.Thread):
+    def __init__(self):
+        for port in ports:
+            chiller = polyscience_4100.Polyscience_4100('/dev/' + port)
+            if not (chiller.read_status() == 'error'):
+                print 'Chiller: ' + port
+                self.chiller = chiller
+                ports.remove(port)
+                break
+
+        threading.Thread.__init__(self)
+        self.temp = -9999
+        self.flow = -9999
+        self.temp_amb = -9999
+        self.pressure = -9999
+        self.temp_setpoint = -9999
+        self.status = 'Off'
+
+    def run(self):
+        while not quit:
+             #self.chiller_temp = network_comm('130.225.86.120', 9759, 'read_temperature')
+            self.temp = self.chiller.read_temperature()
+            self.flow = self.chiller.read_flow_rate()
+            self.temp_amb = self.chiller.read_ambient_temperature()
+            self.pressure = self.chiller.read_pressure()
+            self.temp_setpoint = self.chiller.read_setpoint()
+            self.status = self.chiller.read_status()
+            time.sleep(5)
 
 class ChillerSaver(threading.Thread):
     def __init__(self, reader):
@@ -69,23 +129,23 @@ class ChillerSaver(threading.Thread):
             if (time_trigged):
                 self.last_recorded_time = time.time()
                 meas_time = sqlTime()
-                sql_pressure = "insert into chiller_sputterchamber_pressure set time=\"" +  meas_time + "\", pressure = " + str(self.reader.chiller_pressure)
-                sql_flow = "insert into chiller_sputterchamber_flow set time=\"" +  meas_time + "\", flow = " + str(self.reader.chiller_flow)
-                sql_temperature = "insert into chiller_sputterchamber_temperature set time=\"" +  meas_time + "\", temperature = " + str(self.reader.chiller_temp)
-                sql_temperature_amb = "insert into chiller_sputterchamber_temperature_ambient set time=\"" +  meas_time + "\", temperature = " + str(self.reader.chiller_temp_amb)
-                sql_temperature_setpoint = "insert into chiller_sputterchamber_temperature_setpoint set time=\"" +  meas_time + "\", temperature = " + str(self.reader.chiller_temp_setpoint)
-                chiller_status = network_comm('130.225.86.120', 9759, 'read_status')
+                sql_pressure = "insert into chiller_sputterchamber_pressure set time=\"" +  meas_time + "\", pressure = " + str(self.reader.pressure)
+                sql_flow = "insert into chiller_sputterchamber_flow set time=\"" +  meas_time + "\", flow = " + str(self.reader.flow)
+                sql_temperature = "insert into chiller_sputterchamber_temperature set time=\"" +  meas_time + "\", temperature = " + str(self.reader.temp)
+                sql_temperature_amb = "insert into chiller_sputterchamber_temperature_ambient set time=\"" +  meas_time + "\", temperature = " + str(self.reader.temp_amb)
+                sql_temperature_setpoint = "insert into chiller_sputterchamber_temperature_setpoint set time=\"" +  meas_time + "\", temperature = " + str(self.reader.temp_setpoint)
+                #chiller_status = network_comm('130.225.86.120', 9759, 'read_status')
                 try:
-                    if(chiller_status == 'On'):
-                        print sql_pressure
+                    if(self.reader.status == 'On'):
+                        #print sql_pressure
                         sqlInsert(sql_pressure)
-                        print sql_flow
+                        #print sql_flow
                         sqlInsert(sql_flow)
-                        print sql_temperature
+                        #print sql_temperature
                         sqlInsert(sql_temperature)
-                        print sql_temperature_amb
+                        #print sql_temperature_amb
                         sqlInsert(sql_temperature_amb)
-                        print sql_temperature_setpoint
+                        #print sql_temperature_setpoint
                         sqlInsert(sql_temperature_setpoint)
                     else:
                         print 'Chiller is off'
@@ -94,15 +154,24 @@ class ChillerSaver(threading.Thread):
 
 
 if __name__ == '__main__':
+    ports = FindSerialPorts.find_ports()
 
     quit = False
     chiller_reader = ChillerReader()
-    chiller_reader.start()
+    time.sleep(2)
+    qcm_reader = QcmReader()
+    time.sleep(2)
 
+    chiller_reader.start()
+    qcm_reader.start()
+    
     time.sleep(5)
 
     chiller_saver = ChillerSaver(chiller_reader)
     chiller_saver.start()
+
+    qcm_saver = QcmSaver(qcm_reader)
+    qcm_saver.start()
 
     while not quit:
         try:
