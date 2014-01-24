470ce446562477dc6136f752c74eafcadcbb6e6e
Kenneth Nielsen <k.nielsen81@gmail.com>
2014-01-24 13:59:57 +0100

    More of above

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 
 """PyExpLabSys"""
 
-__author__ = 'CINF <fake@non.com'
+__author__ = 'CINF <fake@non.com>'
 __version__ = '0.1'
 __website__ = 'https://github.com/CINF/PyExpLabSys'
 __license__ = 'GNU GPL3'
