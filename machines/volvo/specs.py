3d9114db701e5f2cb585caef945daed7b3e37955
Robert Jensen <jensen.robert@gmail.com>
2014-03-07 08:28:41 +0100

    More work on gui

--- machines/volvo/specs.py
+++ machines/volvo/specs.py
@@ -8,11 +8,14 @@ import numpy as np
 from PyQt4 import Qt, QtCore
 from PyQt4.QtGui import QWidget
 
+import sys
+sys.path.append('/home/cinf/PyExpLabSys/PyExpLabSys/drivers')
+sys.path.append('/home/cinf/PyExpLabSys/')
 from sputter_gui import Ui_Specs
 from PyExpLabSys.common.plotters import DataPlotter
 
-import sys
-sys.path.append('/home/robertj/Documents/PyExpLabSys/PyExpLabSys/drivers')
+
+
 
 import specs_iqe11
 
@@ -32,15 +35,15 @@ class SimplePlot(QWidget):
         self.scale = 1E-8
         self.active = False
         self.start = None
-        self.plot_length = 20
+        self.plot_length = 200
 
         # Set up plot (using pretty much all the possible options)
-        self.plots_l = ['signal1', 'signal2']
-        self.plots_r = ['aux_signal1']
+        self.plots_l = ['filament_current', 'sputter_current']
+        self.plots_r = ['temperature']
         self.plotter = DataPlotter(
             self.plots_l, right_plotlist=self.plots_r, parent=self,
-            left_log=True, title='Awesome plots',
-            yaxis_left_label='Log sine, cos', yaxis_right_label='Noisy line',
+            left_log=False, title='Ion Gun Data',
+            yaxis_left_label='Currents', yaxis_right_label='Noisy line',
             xaxis_label='Time since start [s]',
             legend='right', left_thickness=[2, 8], right_thickness=6,
             left_colors=['firebrick', 'darkolivegreen'],
@@ -50,15 +53,18 @@ class SimplePlot(QWidget):
         self.gui.horizontalLayout.addWidget(self.plotter.plot)
 
         # Connect signals
+        QtCore.QObject.connect(self.gui.operate_button,
+                               QtCore.SIGNAL('clicked()'),
+                               self.on_operate)        
+        QtCore.QObject.connect(self.gui.standby_button,
+                               QtCore.SIGNAL('clicked()'),
+                               self.on_standby)        
         QtCore.QObject.connect(self.gui.start_button,
                                QtCore.SIGNAL('clicked()'),
                                self.on_start)
         QtCore.QObject.connect(self.gui.stop_button,
                                QtCore.SIGNAL('clicked()'),
                                self.on_stop)
-        QtCore.QObject.connect(self.gui.sputter_current,
-                               QtCore.SIGNAL('valueChanged(int)'),
-                               self.on_scale)
         QtCore.QObject.connect(self.gui.quit_button,
                                QtCore.SIGNAL('clicked()'),
                                QtCore.QCoreApplication.instance().quit)
@@ -76,32 +82,38 @@ class SimplePlot(QWidget):
 
             QtCore.QTimer.singleShot(0, self.plot_iteration)
 
+    def on_standby(self):
+        """Standby button method"""
+        self.sputtergun.standby()
+
+    def on_operate(self):
+        """Standby button method"""
+        self.sputtergun.operate()
+        print "!"
+
+
     def on_stop(self):
         """Stop button method"""
         print 'stop pressed'
         self.active = False
 
-    def on_scale(self, value):
-        """Scale spin button method"""
-        print 'scale change, new value'.format(self.scale)
-        self.scale = 10 ** value
-        self.gui.scale_label.setText('1E' + str(value))
-
     def plot_iteration(self):
         """method that emulates a single data gathering and plot update"""
         elapsed = time.time() - self.start
-        self.gui.sputter_current.setProperty("value", self.sputtergun.status['filament_current'])
+        self.gui.sputter_current.setProperty("value", self.sputtergun.status['sputter_current'])
         self.gui.filament_bias.setProperty("text", str(self.sputtergun.status['filament_bias']))
-        print self.sputtergun.status['filament_current']
+        self.gui.temp_energy_module.setProperty("text", str(self.sputtergun.status['temperature']))
+        self.gui.filament_current.setProperty("text", str(self.sputtergun.status['filament_current']))
+        self.gui.accel_voltage.setProperty("text", str(self.sputtergun.status['accel_voltage']))
+        self.gui.emission_current.setProperty("text", str(self.sputtergun.status['emission_current']))
 
         if elapsed <= self.plot_length:
-
+            self.plotter.add_point('sputter_current', (elapsed, self.sputtergun.status['sputter_current']))
+            print self.sputtergun.status['sputter_current']
             value = (np.sin(elapsed) + 1.1) * self.scale
-            self.plotter.add_point('signal1', (elapsed, value))
+            self.plotter.add_point('filament_current', (elapsed, self.sputtergun.status['filament_current']))
             value = (np.cos(elapsed) + 1.1) * self.scale
-            self.plotter.add_point('signal2', (elapsed, value))
-            value = elapsed + random.random()
-            self.plotter.add_point('aux_signal1', (elapsed, value))
+            self.plotter.add_point('temperature', (elapsed, self.sputtergun.status['temperature']))
         else:
             self.active = False
 
