4acde35274c1de02d73b2cec3f9e593a580485dc
Jakob Riedel <jnri@fysik.dtu.dk>
2014-09-04 14:28:00 +0200

    Turn main program into class and update for run on raspberry pi

--- machines/fridays_finish_at_four/BarProgramme.py
+++ machines/fridays_finish_at_four/BarProgramme.py
@@ -7,119 +7,163 @@ import sys
 import os
 import time
 import subprocess
-from BarCodeScanner import *
-from PyExpLabSys.drivers.four_d_systems import PicasouLCD28PTU
+from bar_database import *
+from PyExpLabSys.drivers.four_d_systems import PicasouLCD28PTU, to_ascii
 from serial.serialutil import SerialException
-
-__version__ = 0.002
-
-latin_dict = {
-    u"¡": u"!", u"¢": u"c", u"£": u"L", u"¤": u"o", u"¥": u"Y",
-    u"¦": u"|", u"§": u"S", u"¨": u"`", u"©": u"c", u"ª": u"a",
-    u"«": u"<<", u"¬": u"-", u"­": u"-", u"®": u"R", u"¯": u"-",
-    u"°": u"o", u"±": u"+-", u"²": u"2", u"³": u"3", u"´": u"'",
-    u"µ": u"u", u"¶": u"P", u"·": u".", u"¸": u",", u"¹": u"1",
-    u"º": u"o", u"»": u">>", u"¼": u"1/4", u"½": u"1/2", u"¾": u"3/4",
-    u"¿": u"?", u"À": u"A", u"Á": u"A", u"Â": u"A", u"Ã": u"A",
-    u"Ä": u"A", u"Å": u"Aa", u"Æ": u"Ae", u"Ç": u"C", u"È": u"E",
-    u"É": u"E", u"Ê": u"E", u"Ë": u"E", u"Ì": u"I", u"Í": u"I",
-    u"Î": u"I", u"Ï": u"I", u"Ð": u"D", u"Ñ": u"N", u"Ò": u"O",
-    u"Ó": u"O", u"Ô": u"O", u"Õ": u"O", u"Ö": u"O", u"×": u"*",
-    u"Ø": u"Oe", u"Ù": u"U", u"Ú": u"U", u"Û": u"U", u"Ü": u"U",
-    u"Ý": u"Y", u"Þ": u"p", u"ß": u"b", u"à": u"a", u"á": u"a",
-    u"â": u"a", u"ã": u"a", u"ä": u"a", u"å": u"aa", u"æ": u"ae",
-    u"ç": u"c", u"è": u"e", u"é": u"e", u"ê": u"e", u"ë": u"e",
-    u"ì": u"i", u"í": u"i", u"î": u"i", u"ï": u"i", u"ð": u"d",
-    u"ñ": u"n", u"ò": u"o", u"ó": u"o", u"ô": u"o", u"õ": u"o",
-    u"ö": u"o", u"÷": u"/", u"ø": u"oe", u"ù": u"u", u"ú": u"u",
-    u"û": u"u", u"ü": u"u", u"ý": u"y", u"þ": u"p", u"ÿ": u"y", 
-    u"’":u"'"
-}
+from PyExpLabSys.drivers.vivo_technologies import ThreadedLS689A, detect_barcode_device
+from ssh_tunnel import create_tunnel, close_tunnel, get_ip_address, test_demon_connection
 
 
-bc = BeerClass()
+__version__ = 1.000
+#bar_database = BarDatabase()
+#bar_database = None
 
-
-for port in range(8):
-    try:
-        device = '/dev/ttyUSB{}'.format(port)
-        #print device
-        picaso = PicasouLCD28PTU(serial_device=device, baudrate=9600)
-        spe_version = picaso.get_spe_version()
-        picaso.screen_mode('landscape reverse')
-        if spe_version == '1.2':
-            break
-    except SerialException:
-        pass
-
-def read_barcode():
-    """Read the barcode and return the number or None on error"""
-    print 'Scan barcode now!'
-    line = sys.stdin.readline().strip()
-    os.system('clear')
-    out = int(line)
-    return out
 
 def cowsay(text):
-    p = subprocess.Popen(["cowsay", "-W34", str(text)], stdout=subprocess.PIPE)
+    """Display text in Cowsay manner"""
+    p = subprocess.Popen(["/usr/games/cowsay", "-ftux", "-W34", str(text)], stdout=subprocess.PIPE)
     out, err = p.communicate()
-    
     return out
 
-def print_to_screen():
-    picaso.put_string()
-    return
 
-def to_ascii(string):
-    for char in string:
-        if char in latin_dict:
-            string = string.replace(char,latin_dict[char])
-    return string
+class NewBarcode(Exception):
+    """Custom exception used to signal that a new barcode has been received"""
+    def __init__(self, barcode):
+        self.barcode = barcode
+        super(NewBarcode, self).__init__()
 
-def run():
-    """The main part of the program"""
-    select = None
-    data = {}
-    picaso.text_foreground_color((1, 1, 1))
-    while select != 'q':
-        #message = "Enter (e)dit, (b)uy or (q)uit and press enter: "
-        #select = raw_input(message)
-        select = 'b'
-        if select == 'b':
+
+class Bar101(object):
+    """Barcodescanner programme class """
+    def __init__(self):
+        #Initialize internal variables
+        self.tbs = None
+        self.bar_database = None
+        #Setup of display
+        for port in range(8):
             try:
-                picaso.clear_screen()
-                picaso.put_string("Welcome to the Friday Bar!")
-                picaso.move_cursor(1, 0)
-                picaso.put_string("Friday Bar System Version {}".format(__version__))
-                picaso.move_cursor(4, 0)
-                picaso.put_string('Scan your barcode!')
-                data['barcode'] = read_barcode()
-                #print cowsay('Special price for you my friend (in DKK): \n' + str(bc.get_item(data['barcode'], statement='price')))
-                picaso.clear_screen()
-                #picaso.put_string('Special price for you my friend: \n' + str(bc.get_item(data['barcode'], statement='price')) + ' DKK')
-                try:
-                    picaso.put_string(cowsay('Special price for you my friend: \n' + str(bc.get_item(data['barcode'], statement='price')) + ' DKK'))
-                except:
-                    picaso.put_string(cowsay('Invalid barcode! Are you drunk?'))
-                    time.sleep(3)
-                    continue
-                
-                time.sleep(3)
-                picaso.clear_screen()
-                #os.system('clear')
-                name = bc.get_item(data['barcode'], statement='name').decode('utf-8')
-                picaso.put_string(cowsay('Enjoy your delicious ' + to_ascii(name)))
-                time.sleep(2)
-                #time.sleep(3)
-                #os.system('clear')
-            except IOError:##ValueError:
-                print 'Wrong input!... are you drunk?'
-                return
-        elif select == 'u':
-            update()
-        
+                device = '/dev/ttyUSB{}'.format(port)
+                picaso = PicasouLCD28PTU(serial_device=device, baudrate=9600)
+                spe_version = picaso.get_spe_version()
+                picaso.screen_mode('landscape reverse')
+                if spe_version == '1.2':
+                    self.picaso = picaso
+                    break
+            except SerialException:
+                pass
 
+
+    def timer(self, timeout=3):
+        """Set timeout to None to run forever"""
+        while timeout is None or timeout > 0:
+            time.sleep(0.1)
+            if timeout is not None:
+                timeout -= 0.1
+            barcode = self.tbs.last_barcode_in_queue
+            if barcode is not None:
+                raise NewBarcode(barcode)
+
+    def start_up(self):
+        """Starting up initials"""
+        self.picaso.clear_screen()
+
+        #testing demon connection
+        attempt_number = 0
+        while not test_demon_connection():
+            self.picaso.move_cursor(0, 0)
+            attempt_number += 1
+            status_string = "Demon connection attempt {} failed".format(attempt_number)
+            self.picaso.put_string(status_string)
+        self.picaso.clear_screen()
+        status_string = "Demon connection attempt succeeded".format(attempt_number)
+        self.picaso.put_string(status_string)
+
+        # Print network interface and ip address
+        interface, ip_address = get_ip_address()
+        self.picaso.move_cursor(2, 0)
+        interface_string = "Interface: {}".format(interface)
+        self.picaso.put_string(interface_string)
+        self.picaso.move_cursor(3, 0)
+        ip_address_string = "Ip address: {}".format(ip_address)
+        self.picaso.put_string(ip_address_string)
+
+        # Start the database backend
+        self.bar_database = BarDatabase()
+
+        #Start barcode scanner
+        dev_ = detect_barcode_device()
+        print dev_
+        self.tbs = ThreadedLS689A(dev_)
+        self.tbs.start()
+        time.sleep(2)
+
+    def clean_up(self):
+        """Closing down"""
+        self.tbs.close()
+        print 'cleaning up'
+        time.sleep(1)
+
+    def query_barcode(self):
+        """Initial message"""
+        self.picaso.clear_screen()
+        self.picaso.put_string(cowsay("Welcome to the Friday Bar. Please scan your barcode!"))
+        self.picaso.move_cursor(19, 0)
+        self.picaso.put_string("Friday Bar System Version {}".format(__version__))
+        self.timer(None)
+
+    def present_beer(self, barcode):
+        # INSERT Show beer
+        import time
+        self.picaso.clear_screen()
+        t0 = time.time()
+        beer_price = str(self.bar_database.get_item(barcode, statement='price'))
+        print "database", time.time() - t0
+        cowsay_string = cowsay("Special price for you my friend: " + beer_price + " DKK")
+        print "cowsay", time.time() - t0
+        self.picaso.put_string(cowsay_string)
+        print "put string", time.time() - t0
+
+        self.timer(3)
+        # INSERT Show spiffy comment
+        self.picaso.clear_screen()
+        self.picaso.put_string(cowsay("Enjoy your delicious " + str(self.bar_database.get_item(barcode, statement='name'))))
+        self.timer(4)
+
+    def present_insult(self):
+        """Presents insult if programme handled wrong"""
+        self.picaso.clear_screen()
+        self.picaso.put_string(cowsay("You did it all wrong! Time to go home?"))
+        self.timer(2)
+
+    def run(self):
+        action, args = self.query_barcode, {}
+        while True:
+            try:
+                action(**args)
+                action, args = self.query_barcode, {}
+            except NewBarcode as new_barcode:
+                barcode_type = self.bar_database.get_type(new_barcode.barcode)
+                if barcode_type == 'beer':
+                    action = self.present_beer
+                    args = {'barcode': new_barcode.barcode}
+                elif barcode_type == 'invalid':
+                    action, args = self.present_insult, {}
+
+
+def main():
+    """Main function"""
+    bar101 = Bar101()
+    while True:
+        bar101.start_up()
+        try:
+            bar101.run()
+        except KeyboardInterrupt:
+            bar101.clean_up()
+            bar101.picaso.close()
+            break
+        except:
+            bar101.clean_up()
+            raise
 
 if __name__ == '__main__':
-    run()
-    #print cowsay('test test')
+    main()
