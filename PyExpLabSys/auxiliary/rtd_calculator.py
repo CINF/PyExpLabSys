7f69a8cef5a485c146272e37f120a8e3d96d6638
Robert Jensen <jensen.robert@gmail.com>
2014-10-06 14:40:43 +0200

    Small cleanup for better naming

--- PyExpLabSys/auxiliary/rtd_calculator.py
+++ PyExpLabSys/auxiliary/rtd_calculator.py
@@ -1,6 +1,6 @@
 class RTD_Calculator():
 
-    def __init__(self,Trt,Rrt,material='Pt'):
+    def __init__(self, Trt, Rrt, material='Pt'):
         if material == 'Pt':
             self.A = 3.9803e-3
             self.B = -5.775e-7
@@ -13,21 +13,21 @@ class RTD_Calculator():
 
         self.Trt = Trt
         self.Rrt = Rrt
-        self.R0 = self.FindR0(Trt,Rrt)
+        self.R0 = self.find_r0(Trt,Rrt)
         
-    def FindR(self,R0,T):
+    def find_r(self, R0, T):
         A = self.A
         B = self.B
         R = R0(1 + A*T + B*T**2)
         return R
     
-    def FindR0(self,Trt,Rrt):
+    def find_r0(self, Trt, Rrt):
         A = self.A
         B = self.B
         R0 = Rrt/(1 + A*Trt + B*Trt**2)
         return R0
     
-    def FindTemperature(self,R):
+    def find_temperature(self, R):
         A = self.A
         B = self.B
         R0 = self.R0
