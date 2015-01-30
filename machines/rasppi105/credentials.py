da59416e7ccd9e00b1b39d25d5caf23d986ab94e
Anders Nierhoff <anders.nierhoff@gmail.com>
2015-01-31 00:37:27 +0100

    end of day

---
+++ machines/rasppi105/credentials.py
@@ -0,0 +1,2 @@
+user = 'omicron'
+psswd = 'omicron'
--- machines/rasppi105/data_logger.py
+++ machines/rasppi105/data_logger.py
@@ -1,26 +1,75 @@
-""" Pressure and temperature logger """
 # pylint: disable=C0301,R0904, C0103
 
 import threading
-import time
 import logging
+import time
+from PyExpLabSys.common.value_logger import ValueLogger
 from PyExpLabSys.common.loggers import ContinuousLogger
 from PyExpLabSys.common.sockets import DateDataPullSocket
-from PyExpLabSys.common.sockets import LiveSocket
-from PyExpLabSys.common.value_logger import ValueLogger
-fromt PyExpLabSys.drivers.keithley_2700 as keithley2700
+import PyExpLabSys.drivers.keithley2700 as keithley2700
+import credentials
+
+class DmmReader(threading.Thread):
+    def __init__(self, dmm):
+        threading.Thread.__init__(self)
+        self.qcm = qcm_instance
+        self.rtd_resistance = -1
+        #self.rtd_temperature = -1
+        self.ttl = 20
+        self.quit = False
+
+    def value(self, channel):
+        """ Return the value of the reader """
+        self.ttl = self.ttl - 1
+        if self.ttl < 0:
+            self.quit = True
+        if channel == 0:
+            return_val = self.rtd_resistance
+        #if channel == 1:
+        #    return_val = self.rtd_temperature
+        return return_val
+
+    def run(self):
+        while not self.quit:
+            print self.ttl
+            self.rtd_resistance = self.dmm.read_resistance()
+            self.ttl = 20
+            time.sleep(2)
 
 
+logging.basicConfig(filename="logger.txt", level=logging.ERROR)
+logging.basicConfig(level=logging.ERROR)
 
-if __name__ == '__main__':
-    ports = {}
-    ports[0] = 'usb-9710_7840-if00-port0'
-    ports[1] = 'usb-9710_7840-if00-port1'
-    ports[2] = 'usb-9710_7840-if00-port2'
-    ports[3] = 'usb-9710_7840-if00-port3'
+dmm_port = '/dev/serial/by-id/usb-9710_7840-if00-port0'
+dmm = keithley2700.Keithley2700(dmm_port)
+reader = DmmReader(dmm)
+reader.daemon = True
+reader.start()
 
-    dmm = keithley2700('serial', device=ports[0])
-    print(dmm.read_software_version())
-    while True:
-        print(dmm.read_voltage())
+codenames = ['omicron_rtd_resistance',]
+loggers = {}
+for i in range(0, len(codenames)):
+    loggers[codenames[i]] = ValueLogger(reader, comp_val = 0.25, channel = i)
+    loggers[codenames[i]].start()
 
+socket = DateDataPullSocket('omicron_rtd_resistance',
+                            codenames, port=9001, timeouts=[5.0] * len(codenames))
+socket.start()
+
+db_logger = ContinuousLogger(table='dateplots_omicron',
+                                 username=credentials.user,
+                                 password=credentials.passwd,
+                                 measurement_codenames=codenames)
+db_logger.start()
+
+time.sleep(5)
+
+while reader.isAlive():
+    time.sleep(0.25)
+    for name in codenames:
+        v = loggers[name].read_value()
+        socket.set_point_now(name, v)
+        if loggers[name].read_trigged():
+            print v
+            db_logger.enqueue_point_now(name, v)
+            loggers[name].clear_trigged()
