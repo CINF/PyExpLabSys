5ae5b7a1b7a8dabb25dd87e57e239d17234b0983
Anders Nierhoff <anders.nierhoff@gmail.com>
2015-02-13 11:12:37 +0100

    clean up

--- machines/rasppi22/bakeout.py
+++ machines/rasppi22/bakeout.py
@@ -5,12 +5,12 @@ import threading
 import curses
 import wiringpi2 as wp
 
-#6: Ion pumps
-#5: Diverse rør
-#4: Main chamber + turbo
-#2: Flight tube
+#from PyExpLabSys.common.loggers import ContinuousLogger
+from PyExpLabSys.common.sockets import DateDataPullSocket
+
 
 class CursesTui(threading.Thread):
+    """ Tui for controlling the bakeout boxx"""
     def __init__(self, baker_instance):
         threading.Thread.__init__(self)
         self.baker = baker_instance
@@ -26,51 +26,67 @@ class CursesTui(threading.Thread):
         while not self.baker.quit:
             self.screen.addstr(2, 2, 'Running')
 
-            self.screen.addstr(4, 2, "Watchdog TTL: {0:.0f}  ".format(self.watchdog.time_to_live))
-            self.screen.addstr(5, 2, "Watchdog Timer: {0:.1f}".format(time.time() - self.watchdog.timer))
-            self.screen.addstr(6, 2, "Watchdog safe: " + str(self.watchdog.watchdog_safe)) 
+            self.screen.addstr(4, 2,
+                               "Watchdog TTL: {0:.0f}  ".format(
+                    self.watchdog.time_to_live))
+            self.screen.addstr(5, 2,
+                               "Watchdog Timer: {0:.1f}".format(
+                    time.time() - self.watchdog.timer))
+            self.screen.addstr(6, 2,
+                               "Watchdog safe: {}    ".format(
+                    self.watchdog.watchdog_safe)) 
 
-            self.screen.addstr(8,2, 'Current channel status:')
-            for i in range(1,7):
-                self.screen.addstr(9,6*i, str(wp.digitalRead(i)))
+            self.screen.addstr(8, 2,
+                               'Current channel status:')
+            for i in range(1, 7):
+                self.screen.addstr(9, 6*i,
+                                   str(wp.digitalRead(i)))
 
-            self.screen.addstr(12,2, 'Channel duty cycles')
-            for i in range(1,7):
-                self.screen.addstr(13,6*i, str(self.baker.dutycycles[i-1]))
+            self.screen.addstr(12, 2,
+                               "Channel duty cycles")
+            for i in range(1, 7):
+                self.screen.addstr(13, 6*i,
+                                   str(self.baker.dutycycles[i-1]))
             
             if self.baker.run_ramp == True:
                 self.screen.addstr(15, 2, str(self.baker.ramp.present()))
             else:
-                self.screen.addstr(15, 2, "                                ")  
-
+                self.screen.addstr(15, 2,
+                                   "                                ")  
+            
+            self.screen.addstr(18, 2,
+                               "l: load ramp from ramp.py     ")
+            self.screen.addstr(19, 2,
+                               "q: quit program       ")
             n = self.screen.getch()
 
-            self.screen.addstr(20,2, str(n) + '     ')
+            self.screen.addstr(20, 2,
+                               "Last key stroke: {}     ".fomrat(n))
 
             if n == ord('1'):
-                self.baker.modify_dutycycle(1,0.1)
+                self.baker.modify_dutycycle(1, 0.1)
             elif n == ord('!'):
-                self.baker.modify_dutycycle(1,-0.1)
+                self.baker.modify_dutycycle(1, -0.1)
             elif n == ord('2'):
-                self.baker.modify_dutycycle(2,0.1)
+                self.baker.modify_dutycycle(2, 0.1)
             elif n == ord('"'):
-                self.baker.modify_dutycycle(2,-0.1)
+                self.baker.modify_dutycycle(2, -0.1)
             elif n == ord('3'):
-                self.baker.modify_dutycycle(3,0.1)
+                self.baker.modify_dutycycle(3, 0.1)
             elif n == ord('#'):
-                self.baker.modify_dutycycle(3,-0.1)
+                self.baker.modify_dutycycle(3, -0.1)
             elif n == ord('4'):
-                self.baker.modify_dutycycle(4,0.1)
+                self.baker.modify_dutycycle(4, 0.1)
             elif n == 194: #... '¤':
-                self.baker.modify_dutycycle(4,-0.1)
+                self.baker.modify_dutycycle(4, -0.1)
             elif n == ord('5'):
-                self.baker.modify_dutycycle(5,0.1)
+                self.baker.modify_dutycycle(5, 0.1)
             elif n == ord('%'):
-                self.baker.modify_dutycycle(5,-0.1)
+                self.baker.modify_dutycycle(5, -0.1)
             elif n == ord('6'):
-                self.baker.modify_dutycycle(6,0.1)
+                self.baker.modify_dutycycle(6, 0.1)
             elif n == ord('&'):
-                self.baker.modify_dutycycle(6,-0.1)
+                self.baker.modify_dutycycle(6, -0.1)
             elif n == ord('l'):
                 self.baker.load_ramp()
             elif n == ord('q'):
@@ -89,6 +105,7 @@ class CursesTui(threading.Thread):
 
 
 class Watchdog(threading.Thread):
+    """ timer for ensuring that 100% power is not posible even if the program fails"""
     def __init__(self):
 	threading.Thread.__init__(self)
         wp.pinMode(0, 1)
@@ -102,6 +119,7 @@ class Watchdog(threading.Thread):
         self.reset_ttl()
 
     def reset_ttl(self):
+        """ reseting the countdown"""
         self.time_to_live = 100
 
     def decrease_ttl(self):
@@ -133,13 +151,14 @@ class Watchdog(threading.Thread):
 
 
 class Bakeout(threading.Thread):
-    def __init__(self, watchdog):
-	threading.Thread.__init__(self)
+    def __init__(self, watchdog, datasocket):
+        threading.Thread.__init__(self)
         self.watchdog = watchdog
+        self.datasocket = datasocket
         self.quit = False
         for i in range(0, 7): #Set GPIO pins to output
             wp.pinMode(i, 1)
-        self.dutycycles = [0.0,0.0,0.0,0.0,0.0,0.0]
+        self.dutycycles = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
         self.run_ramp = False
 
     def activate(self, pin):
@@ -154,21 +173,25 @@ class Bakeout(threading.Thread):
     def modify_dutycycle(self, channel, amount):
         self.dutycycles[channel-1] =  self.dutycycles[channel-1] + amount
         if  self.dutycycles[channel-1] > 1:
-             self.dutycycles[channel-1] = 1.0
+            self.dutycycles[channel-1] = 1.0
         if self.dutycycles[channel-1] < 0.01:
-             self.dutycycles[channel-1] = 0.0
+            self.dutycycles[channel-1] = 0.0
         return  self.dutycycles[channel-1]
 
     def set_dutycycle(self, channel, value):
+        """Set a single dutycycle
+        input channel number fx 5
+        input new value fx 0.2"""
         self.dutycycles[channel-1] =  value
         if  self.dutycycles[channel-1] > 1:
-             self.dutycycles[channel-1] = 1.0
+            self.dutycycles[channel-1] = 1.0
         if self.dutycycles[channel-1] < 0.01:
-             self.dutycycles[channel-1] = 0.0
+            self.dutycycles[channel-1] = 0.0
         return  self.dutycycles[channel-1]
 
 
     def set_all_dutycycles(self,value_list):
+        """ set new values in dutycyles"""
         if len(value_list) == 6:
             for index, value in enumerate(value_list):
                 channel = index + 1
@@ -176,6 +199,7 @@ class Bakeout(threading.Thread):
         return self.dutycycles
 
     def load_ramp(self,):
+        """ load a ramp defined in external script"""
         import ramp
         reload(ramp)
         self.ramp = ramp.ramp()
@@ -188,10 +212,11 @@ class Bakeout(threading.Thread):
         cycle = 0
         while not self.quit:
             self.watchdog.reset_ttl()
+            self.datasocket.set_point_now('stm312_bakeoutbox', self.dutycycles)
             if self.run_ramp == True:
                 self.set_all_dutycycles(self.ramp.present())
             try:
-                for i in range(1,7):
+                for i in range(1, 7):
                     if (1.0*cycle/totalcycles) < self.dutycycles[i-1]:
                         baker.activate(i)
                     else:
@@ -209,24 +234,27 @@ class Bakeout(threading.Thread):
                     baker.deactivate(i)
 
 
-
-if __name__ == '__main__':
+def main():
+    """ main function"""
     wp.wiringPiSetup()
-
     watchdog = Watchdog()
     watchdog.daemon = True
     watchdog.start()
     time.sleep(1)
-    baker = Bakeout(watchdog)
+    datasocket = DateDataPullSocket(
+        'stm312_bakeoutbox',
+        ['stm312_bakeoutbox'],
+        timeouts=[1.0])
+    datasocket.start()
+    baker = Bakeout(watchdog, datasocket)
     baker.start()
-
     tui = CursesTui(baker)
-    #tui.daemon = True
-    tui.start()
-
+    #tui.daemon = True                                                                                                                                                                                                                                                            tui.start()
     while not baker.quit:
         time.sleep(1)
-
     watchdog.quit = True
     time.sleep(2)
     baker.quit = True
+
+if __name__ == '__main__':
+    main()
