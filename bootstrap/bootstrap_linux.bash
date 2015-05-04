831acb1f61bcb9330789d02c408021feee188e6e
Robert Jensen <jensen.robert@gmail.com>
2015-05-04 09:06:51 +0200

    Added python-colorama to bootstrap_linux

--- bootstrap/bootstrap_linux.bash
+++ bootstrap/bootstrap_linux.bash
@@ -12,7 +12,7 @@
 apt1="openssh-server emacs python-pip graphviz screen ntp"
 
 # apt install packages line 2, python extensions
-apt2="python-mysqldb python-serial"
+apt2="python-mysqldb python-serial python-colorama"
 
 # apt install packages line 3, code checkers
 apt3="pylint"
