c4fc99eb6be77df5b1a66dd6219ea29b89df9826
Kenneth Nielsen <k.nielsen81@gmail.com>
2015-06-03 14:16:22 +0200

    Added to_ascii function

--- PyExpLabSys/drivers/four_d_systems.py
+++ PyExpLabSys/drivers/four_d_systems.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # pylint: disable=W0142
 
 """Drivers for the 4d systems displays
@@ -47,6 +48,37 @@ GRAPHICS_PARAMETERS = ['x_max', 'y_max', 'last_object_left', 'last_object_top',
                        'last_object_right', 'last_object_bottom']
 TOUCH_STATES = ['enable', 'disable', 'default']
 TOUCH_STATUSSES = ['invalid/notouch', 'press', 'release', 'moving']
+LATIN_DICT = {
+    u"¡": u"!", u"¢": u"c", u"£": u"L", u"¤": u"o", u"¥": u"Y",
+    u"¦": u"|", u"§": u"S", u"¨": u"`", u"©": u"c", u"ª": u"a",
+    u"«": u"<<", u"¬": u"-", u"­": u"-", u"®": u"R", u"¯": u"-",
+    u"°": u"o", u"±": u"+-", u"²": u"2", u"³": u"3", u"´": u"'",
+    u"µ": u"u", u"¶": u"P", u"·": u".", u"¸": u",", u"¹": u"1",
+    u"º": u"o", u"»": u">>", u"¼": u"1/4", u"½": u"1/2", u"¾": u"3/4",
+    u"¿": u"?", u"À": u"A", u"Á": u"A", u"Â": u"A", u"Ã": u"A",
+    u"Ä": u"A", u"Å": u"Aa", u"Æ": u"Ae", u"Ç": u"C", u"È": u"E",
+    u"É": u"E", u"Ê": u"E", u"Ë": u"E", u"Ì": u"I", u"Í": u"I",
+    u"Î": u"I", u"Ï": u"I", u"Ð": u"D", u"Ñ": u"N", u"Ò": u"O",
+    u"Ó": u"O", u"Ô": u"O", u"Õ": u"O", u"Ö": u"O", u"×": u"*",
+    u"Ø": u"Oe", u"Ù": u"U", u"Ú": u"U", u"Û": u"U", u"Ü": u"U",
+    u"Ý": u"Y", u"Þ": u"p", u"ß": u"b", u"à": u"a", u"á": u"a",
+    u"â": u"a", u"ã": u"a", u"ä": u"a", u"å": u"aa", u"æ": u"ae",
+    u"ç": u"c", u"è": u"e", u"é": u"e", u"ê": u"e", u"ë": u"e",
+    u"ì": u"i", u"í": u"i", u"î": u"i", u"ï": u"i", u"ð": u"d",
+    u"ñ": u"n", u"ò": u"o", u"ó": u"o", u"ô": u"o", u"õ": u"o",
+    u"ö": u"o", u"÷": u"/", u"ø": u"oe", u"ù": u"u", u"ú": u"u",
+    u"û": u"u", u"ü": u"u", u"ý": u"y", u"þ": u"p", u"ÿ": u"y",
+    u"’": u"'"
+}
+
+
+def to_ascii(string):
+    """Convert non-ascii character in string to ascii"""
+    for char in string:
+        if char in LATIN_DICT:
+            string = string.replace(char, LATIN_DICT[char])
+    return string
+
 
 
 # pylint: disable=too-many-public-methods
