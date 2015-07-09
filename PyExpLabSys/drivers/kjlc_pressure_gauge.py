345035ef978e927d51bd17db67d4cfd78734d8fa
Robert Jensen <jensen.robert@gmail.com>
2015-07-09 14:07:09 +0200

    Cleanup and preparation for non-windows counting of serial ports

--- PyExpLabSys/drivers/kjlc_pressure_gauge.py
+++ PyExpLabSys/drivers/kjlc_pressure_gauge.py
@@ -1,5 +1,4 @@
 import serial
-import SocketServer
 import time
 
 KJLC = None
@@ -7,8 +6,8 @@ KJLC = None
 class KJLC300(object):
     """ Class the implements a KJLC interface """
     
-    def __init__(self):
-        self.connection = serial.Serial(0, baudrate=1200, bytesize=8,
+    def __init__(self, port):
+        self.connection = serial.Serial(port, baudrate=1200, bytesize=8,
                                         parity='N', stopbits=1, timeout=1,
                                         writeTimeout=1)
 
@@ -42,34 +41,3 @@ class KJLC300(object):
         """
         return string[4:-1]
 
-class MyUDPHandler(SocketServer.BaseRequestHandler):
-    """ UDP handler """
-
-    def handle(self):
-        recieved_data = self.request[0].strip()
-        socket = self.request[1]
-
-        if recieved_data.startswith('get'):
-            data = KJLC.read_pressure()
-
-        socket.sendto(str(data), self.client_address)
-
-def main():
-    global KJLC
-    KJLC = KJLC300()
-    
-####    print KJLC.read_software_version()
-##    print KJLC.read_pressure()
-##    KJLC.close()
-
-    host, port = 'localhost', 9990
-    server = SocketServer.UDPServer((host, port), MyUDPHandler)
-    try:
-        print 'UPD server started. Press Ctrl-c to shut it down.'
-        server.serve_forever()
-    except KeyboardInterrupt:
-        print 'UDP server closed. Shutting down seial interface.'
-        KJLC.close()
-
-if __name__ == '__main__':
-    main()
