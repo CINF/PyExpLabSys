bb0300d202447891990d31558aff7e65c3c86b53
pi <pi@BYG-F0139.localdomain>
2015-07-23 10:35:15 -0700

    TABS end of day

--- machines/BYG-F0139/datalogger.py
+++ machines/BYG-F0139/datalogger.py
@@ -3,6 +3,8 @@
 # pylint: disable=C0301,R0904, C0103
 """ Pressure and temperature logger """
 
+from __future__ import print_function
+
 import sys
 sys.path.insert(1, '/home/pi/PyExpLabsys')
 sys.path.insert(2, '../..')
@@ -14,8 +16,10 @@ import logging
 #from PyExpLabSys.common.sockets import DateDataPullSocket
 #from PyExpLabSys.common.sockets import LiveSocket
 #from PyExpLabSys.common.value_logger import ValueLogger
+from PyExpLabSys.auxiliary.pic import pid
 #import PyExpLabSys.drivers.omegabus as omegabus
 import PyExpLabSys.drivers.omega_cni as omega_CNi32
+#import PyExpLabSys.drivers.kampstrup as kampstrup
 #import credentials
 
 #==============================================================================
@@ -63,11 +67,33 @@ class NGTempReader(threading.Thread):
         while not self.quit:
             self.ttl = 20
             time.sleep(1)
-            self.temperature = self.omega.read_temperature()
+            self.temperature = self.omega.read_temperature(address = 2)
 
+class TABS(threading.Thread):
+    """ class for control of the TABS test room """
+    def __init__(self, Multical, Omegas):
+        threading.Thread.__init__(self)
+        self.Multical = Multical
+        self.Omegas = Omegas
+        self.temperatures
+    def  update_temperatures(self,):
+        self.
+    def run(self):
+        self.update_temperatures()
+        self.update_flows
 #logging.basicConfig(filename="logger.txt", level=logging.ERROR)
 #logging.basicConfig(level=logging.ERROR)
 
-port = '/dev/serial/by-id/' + 'usb-FTDI_USB-RS232_Cable_FTWR5F6W-if00-port0'
-ng_temp = omega_CNi32.ISeries(port, 9600)
-print(ng_temp.command('R01'))
+ports = {}
+ports['Omega ceil'] = '/dev/serial/by-id/usb-FTDI_USB-RS485_Cable_FTWEA5HJ-if00-port0'
+ports['Kamp ceil'] = '/dev/serial/by-id/usb-FTDI_USB-RS485_Cable_FTYIWHC9-if00-port0'
+Omega = {}
+Omega['a'] = omega_CNi32.ISeries(ports['Omega ceil'], 9600, comm_stnd='rs485')
+
+
+
+for i in range(5):
+    print('address: ' + str(i) + ' ' + str(temp.read_temperature(address=i) ) )
+
+
+temp.close()
