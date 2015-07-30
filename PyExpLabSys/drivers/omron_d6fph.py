7b0adc8d62c9183d98f5b3921e8dd7f73952a951
Robert Jensen <jensen.robert@gmail.com>
2015-07-30 22:08:45 +0200

    Now a proper driver wrapped in a class
    Now also reads both pressure and temperature

--- PyExpLabSys/drivers/omron_d6fph.py
+++ PyExpLabSys/drivers/omron_d6fph.py
@@ -3,28 +3,47 @@
 import smbus
 import time
 
-bus = smbus.SMBus(1)
+class OmronD6fph(object):
+    """ Class for reading pressure and temperature from Omron D6F-PH series
+    Ranging not implemented for all models """
 
-DEVICE_ADDRESS = 0x6c
+    def __init__(self):
+        self.bus = smbus.SMBus(1)
+        self.device_address = 0x6c
+        self.full_range = 1000.0
+        self.init_device()
 
-init = [0x0B, 0x00]
-bus.write_i2c_block_data(DEVICE_ADDRESS, 0, init)
+    def init_device(self):
+        """ Sensor needs to be initiated after power up """
+        init_command = [0x0B, 0x00]
+        self.bus.write_i2c_block_data(self.device_address, 0, init_command)
 
+    def read_value(self, command):
+        """ Read a value from the sensor """
+        mcu_mode_command = [0xD0, 0x40, 0x18, 0x06]
+        self.bus.write_i2c_block_data(self.device_address, 0, mcu_mode_command)
+        time.sleep(0.033)
+        self.bus.write_i2c_block_data(self.device_address, 0, command)
+        self.bus.write_byte(self.device_address, 0x07)
+        high = self.bus.read_byte(self.device_address)
+        low = self.bus.read_byte(self.device_address)
+        value = int(hex(high) + hex(low)[2:], 16)
+        return value
 
-command = [0xD0, 0x40, 0x18, 0x06]
-bus.write_i2c_block_data(DEVICE_ADDRESS, 0, command)
-time.sleep(0.05)
-command = [0xD0, 0x51, 0x2C]
-bus.write_i2c_block_data(DEVICE_ADDRESS, 0, command)
-time.sleep(0.1)
-bus.write_byte(DEVICE_ADDRESS, 0x07)
-time.sleep(0.2)
-high = bus.read_byte(DEVICE_ADDRESS)
-low = bus.read_byte(DEVICE_ADDRESS)
-print 'High: ' + str(high) + ' Low: ' + str(low)
+    def read_pressure(self):
+        """ Read the pressure value """
+        value = self.read_value([0xD0, 0x51, 0x2C])
+        #TODO: Implement range calculation for all sensor models
+        print value
+        pressure = (value - 1024) * self.full_range / 60000 - self.full_range/2
+        return pressure
 
-full_range = 1000.0 # I think....
-
-value = high * 2**8 + low
-print value
-print (value - 1024) * full_range / 60000 - full_range/2
+    def read_temperature(self):
+        """ Read the temperature value """
+        value = self.read_value([0xD0, 0x61, 0x2C])
+        temperature = (value - 10214) / 37.39
+        return temperature
+    
+OMRON = OmronD6fph()
+print(OMRON.read_pressure())
+print(OMRON.read_temperature())
