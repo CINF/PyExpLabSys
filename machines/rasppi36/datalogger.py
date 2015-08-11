c12d231e165843b4fbf7d334d4995b3fb4d0f211
Robert Jensen <jensen.robert@gmail.com>
2015-08-11 11:31:31 +0200

    Updated list of gauges

--- machines/rasppi36/datalogger.py
+++ machines/rasppi36/datalogger.py
@@ -24,9 +24,9 @@ class Reader(threading.Thread):
         """ Read the temperaure """
         self.ttl = self.ttl - 1
         if channel == 0:
-            value = self.pressure['ng']
-        if channel == 1:
             value = self.pressure['old']
+        if channel == 1:
+            value = self.pressure['ng']
         if self.ttl < 0:
             self.quit = True
             value = None
@@ -35,9 +35,9 @@ class Reader(threading.Thread):
     def run(self):
         while not self.quit:
             self.ttl = 20
-            self.pressure['ng'] = self.gauges['ng'].read_pressure()
-            self.pressure['old'] = self.gauges['old'].read_pressure()
-            time.sleep(1)
+            for key in ['old', 'ng']:
+                self.pressure[key] = self.gauges[key].read_pressure()
+            time.sleep(0.1)
 
 logging.basicConfig(filename="logger.txt", level=logging.ERROR)
 logging.basicConfig(level=logging.ERROR)
@@ -51,52 +51,52 @@ for i in range(0, 2):
     _name = _mks.read_serial()
     #_name = _name.strip()
     if _name == '1107638964':
-        mks_list['old'] = mks_925_pirani.mks_comm(ports[i])
-        mks_list['old'].change_unit('MBAR')
-        print('Pirani, buffer:' + ports[i] + ', serial:' + _name)
-    elif _name == '1027033634':
         mks_list['ng'] = mks_925_pirani.mks_comm(ports[i])
         mks_list['ng'].change_unit('MBAR')
+        print('Pirani, ng buffer:' + ports[i] + ', serial:' + _name)
+    elif _name == '1027033634':
+        mks_list['old'] = mks_925_pirani.mks_comm(ports[i])
+        mks_list['old'].change_unit('MBAR')
         print('Pirani, old buffer:'+ ports[i] + ', serial:' + _name)
     else:
         print('Pirani, Unknown:'+ ports[i] + ', serial:' + _name)
         print(_mks.read_serial())
 
-"""
-measurement = Reader(mks_list)
-measurement.start()
+if len(mks_list) == 2:
+    measurement = Reader(mks_list)
+    measurement.start()
+    
+    time.sleep(2.5)
+    
+    codenames = ['mr_buffer_pressure', 'microreactorng_pressure_buffer']
 
-time.sleep(2.5)
+    loggers = {}
+    for i in range(len(codenames)):
+        loggers[codenames[i]] = ValueLogger(measurement, comp_val = 0.1,
+                                            low_comp = 1e-4, comp_type = 'log',
+                                            channel=i)
+        loggers[codenames[i]].start()
 
-codenames = ['microreactorng_pressure_buffer', 'mr_buffer_pressure']
-loggers = {}
-loggers[codenames[0]] = ValueLogger(measurement, comp_val = 0.1,
-                                    low_comp = 1e-4, comp_type = 'log',
-                                    channel=0)
-loggers[codenames[0]].start()
-loggers[codenames[1]] = ValueLogger(measurement, comp_val = 0.1,
-                                    low_comp = 1e-4, comp_type = 'log',
-                                    channel=1)
-loggers[codenames[1]].start()
+    socket = DateDataPullSocket(unichr(0x03BC) + '-reactor NG temperature', 
+                                codenames, timeouts=[1.0,1.0])
+    socket.start()
+    
+    livesocket = LiveSocket(unichr(0x03BC) + '-reactors pressures',
+                            codenames, 0.2)
+    livesocket.start()
 
-socket = DateDataPullSocket(unichr(0x03BC) + '-reactor NG temperature', codenames, timeouts=[1.0, 1.0])
-socket.start()
-
-livesocket = LiveSocket(unichr(0x03BC) + '-reactors pressures', codenames, 2)
-livesocket.start()
-
-db_logger = {} 
-db_logger[codenames[0]] = ContinuousLogger(table='dateplots_microreactorNG',
-                                 username=credentials.user_new,
-                                 password=credentials.passwd_new,
-                                 measurement_codenames=codenames)
-
-db_logger[codenames[1]] = ContinuousLogger(table='dateplots_microreactor',
+    db_logger = {} 
+    db_logger[codenames[0]] = ContinuousLogger(table='dateplots_microreactor',
                                  username=credentials.user_old,
                                  password=credentials.passwd_old,
                                  measurement_codenames=codenames)
-db_logger[codenames[0]].start()
-db_logger[codenames[1]].start()
+
+    db_logger[codenames[1]] = ContinuousLogger(table='dateplots_microreactorNG',
+                                 username=credentials.user_new,
+                                 password=credentials.passwd_new,
+                                 measurement_codenames=codenames)
+    db_logger[codenames[0]].start()
+    db_logger[codenames[1]].start()
 
 while measurement.isAlive():
     time.sleep(0.25)
@@ -105,9 +105,9 @@ while measurement.isAlive():
         socket.set_point_now(name, v)
         livesocket.set_point_now(name, v)
         if loggers[name].read_trigged():
-            print name
-            print v
+            print "Codename: {}".format(name)
+            print "Value: {}".format(v)
             print '---'
             db_logger[name].enqueue_point_now(name, v)
             loggers[name].clear_trigged()
-"""
+
