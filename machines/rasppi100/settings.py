3ef8334f75c05daf495cbcdbb053e68081090fc0
Robert Jensen <jensen.robert@gmail.com>
2015-10-02 10:31:22 +0200

    Turbo logger is now a machine-independant app

---
+++ machines/rasppi100/settings.py
@@ -0,0 +1,7 @@
+name = unichr(0x03BC) + '-reactor'
+port = '/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A602SOEK-if00-port0'
+table_prefix = 'mr_chamber'
+
+user = 'microreactor'
+passwd = 'microreactor'
+table = 'dateplots_microreactor'
