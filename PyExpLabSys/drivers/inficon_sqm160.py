bda6c48e40119b0c7c6837dbbc4572e3085f1688
Robert Jensen <jensen.robert@gmail.com>
2015-10-05 11:04:35 +0200

    Cleanup of driver

--- PyExpLabSys/drivers/inficon_sqm160.py
+++ PyExpLabSys/drivers/inficon_sqm160.py
@@ -1,23 +1,26 @@
+""" Driver for Inficon SQM160 QCM controller """
 import serial
 import time
 
 class InficonSQM160():
+    """ Driver for Inficon SQM160 QCM controller """
     def __init__(self, port='/dev/ttyUSB0'):
-        self.f = serial.Serial(port=port,
+        self.serial = serial.Serial(port=port,
                                baudrate=9600,
                                timeout=2,
                                bytesize=serial.EIGHTBITS,
                                xonxoff=True)
         
-    def comm(self,command):
+    def comm(self, command):
+        """ Implements actual communication with device """
         length = chr(len(command) + 34)
         crc = self.crc_calc(length + command)        
         command = '!' + length + command + crc[0] + crc[1]
         error = 0
         while (error > -1) and (error < 20):
-            self.f.write(command)
+            self.serial.write(command)
             time.sleep(0.1)
-            reply = self.f.read(self.f.inWaiting())
+            reply = self.serial.read(self.serial.inWaiting())
             crc = self.crc_calc(reply[1:-2])
             try:
                 crc_ok = (reply[-2] == crc[0] and reply[-1] == crc[1])
@@ -30,8 +33,8 @@ class InficonSQM160():
                 error = error + 1 
         return return_val
 
-
-    def crc_calc(self, input_string):
+    @staticmethod
+    def crc_calc(input_string):
         """ Calculate crc value of command """
         command_string = []
         for i in range(0, len(input_string)):
@@ -49,13 +52,15 @@ class InficonSQM160():
         crc1_mask = int('1111111', 2)
         crc1 = chr((crc & crc1_mask) + 34)
         crc2 = chr((crc >> 7) + 34)
-        return(crc1,crc2)
+        return(crc1, crc2)
         
     def show_version(self):
+        """ Read the firmware version """
         command = '@'
         return(self.comm(command))
 
     def show_film_parameters(self, film):
+        """ Read the film paramters """
         command = 'A1?'
         print self.comm(command)
         
@@ -81,19 +86,20 @@ class InficonSQM160():
         return(frequency)
 
     def crystal_life(self,channel=1):
+        """ Read crystal life """
         command = 'R' + str(channel)
         value_string = self.comm(command)
         life = float(value_string)        
         return(life)
 		
 if __name__ == "__main__":
-    inficon = InficonSQM160()
-    inficon.show_version()
+    INFICON = InficonSQM160()
+    print(INFICON.show_version())
 
-    #inficon.rate(1)
-    #inficon.thickness(1)
-    #inficon.frequency(1)
-    #inficon.CrystalLife(1)
-    #inficon.show_film_parameters(1)
+    #print(inficon.rate(1))
+    #print(inficon.thickness(1))
+    #print(inficon.frequency(1))
+    #print(inficon.CrystalLife(1))
+    #print(inficon.show_film_parameters(1))
     
 
