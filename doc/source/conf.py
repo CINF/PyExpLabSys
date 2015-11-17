2e6a6f3b3ae441e2f5c7794b679f454a8a38c395
Kenneth Nielsen <k.nielsen81@gmail.com>
2015-11-17 13:27:14 +0100

    Small read the docs experiment

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -18,6 +18,8 @@ import sys, os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(1, os.path.join(os.path.abspath('.'), '..', '..'))
 
+print 'YEAH YEAH YEAH'
+
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
