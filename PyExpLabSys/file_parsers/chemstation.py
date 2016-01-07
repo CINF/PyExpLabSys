b2206de9eef5c57aee50877bb51f91f1ae1882fd
Kenneth Nielsen <k.nielsen81@gmail.com>
2016-01-07 11:21:45 +0100

    Added parser for chemstation binary format

--- PyExpLabSys/file_parsers/chemstation.py
+++ PyExpLabSys/file_parsers/chemstation.py
@@ -1,6 +1,6 @@
 """File parser for Chemstation files"""
 
-from __future__ import print_function, unicode_literals
+from __future__ import print_function, unicode_literals, division
 from collections import defaultdict
 
 import codecs
@@ -8,8 +8,13 @@ import re
 import os
 import time
 import math
+import struct
+from struct import unpack
 from xml.etree import ElementTree
 
+import numpy
+
+from PyExpLabSys.thirdparty.cached_property import cached_property
 from PyExpLabSys.common.supported_versions import python2_and_3
 python2_and_3(__file__)
 
@@ -198,3 +203,158 @@ class Injection(object):
                 content_dict[header] = content_dict[header].strip()
 
         return content_dict
+
+
+# Constants used for binary file parsing
+ENDIAN = '>'
+STRING = ENDIAN + '{}s'
+UINT8 = ENDIAN + 'B'
+UINT16 = ENDIAN + 'H'
+INT16 = ENDIAN + 'h'
+INT32 = ENDIAN + 'i'
+
+
+def parse_utf16_string(file_, encoding='UTF16'):
+    """Parse a pascal type UTF16 encoded string from a binary file object"""
+    string_length = unpack(UINT8, file_.read(1))[0]
+    parsed = unpack(STRING.format(2 * string_length),
+                    file_.read(2 * string_length))
+    return parsed[0].decode(encoding)
+
+
+class CHFile(object):
+    """Class that implementats the Agilent .ch file format version 179
+
+    .. note:: Not all aspects of the file header is understood, so there may and probably
+       is information that is not parsed. See the method :meth:`._parse_header_status` for
+       an overview of which parts of the header is understood.
+
+    """
+
+    # Fields is a table of name, offset and type. Types 'x-time' and 'utf16' are specially
+    # handled, the rest are format arguments for struct unpack
+    fields = (
+        ('start_time', 282, 'x-time'),
+        ('end_time', 286, 'x-time'),
+        ('version_string', 326, 'utf16'),
+        ('description', 347, 'utf16'),
+        ('sample', 858, 'utf16'),
+        ('operator', 1880, 'utf16'),
+        ('date', 2391, 'utf16'),
+        ('inlet', 2492, 'utf16'),
+        ('instrument', 2533, 'utf16'),
+        ('method', 2574, 'utf16'),
+        ('software version', 3601, 'utf16'),
+        ('software name', 3089, 'utf16'),
+        ('software revision', 3802, 'utf16'),
+        ('units', 4172, 'utf16'),
+        ('detector', 4213, 'utf16'),
+        ('yscaling', 4732, ENDIAN + 'd')
+    )
+    # The start position of the data
+    data_start = 6144
+    # The versions of the file format supported by this implementation
+    supported_versions = {179}
+
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.metadata = {}
+        with open(self.filepath, 'rb') as file_:
+            self._parse_header(file_)
+            self.values = self._parse_data(file_)
+
+    def _parse_header(self, file_):
+        """Parse the header"""
+        # Parse and check version
+        length = unpack(UINT8, file_.read(1))[0]
+        parsed = unpack(STRING.format(length), file_.read(length))
+        version = int(parsed[0])
+        if not version in self.supported_versions:
+            raise ValueError('Unsupported file version {}'.format(version))
+        self.metadata['magic_number_version'] = version
+
+        # Parse all metadata fields
+        for name, offset, type_ in self.fields:
+            file_.seek(offset)
+            if type_ == 'utf16':
+                self.metadata[name] = parse_utf16_string(file_)
+            elif type_ == 'x-time':
+                self.metadata[name] = unpack(ENDIAN + 'f', file_.read(4))[0] / 60000
+            else:
+                self.metadata[name] = unpack(type_, file_.read(struct.calcsize(type_)))[0]
+
+    def _parse_header_status(self):
+        """Documents the state of how many bytes of the header is understood"""
+        file_ = open(self.filepath, 'rb')
+
+        print('Header parsing status')
+        # Map positions to fields for all the known fields
+        knowns = {item[1]: item for item in self.fields}
+        # A couple of places has a \x01 byte before a string, these we simply skip
+        skips = {325, 3600}
+        # Jump to after the magic number version
+        file_.seek(4)
+
+        # Initialize variables for unknown bytes
+        unknown_start = None
+        unknown_bytes = b''
+        # While we have not yet reached the data
+        while file_.tell() < self.data_start:
+            current_position = file_.tell()
+            # Just continue on skip bytes
+            if current_position in skips:
+                file_.read(1)
+                continue
+
+            # If we know about a data field that starts at this point
+            if current_position in knowns:
+                # If we have collected unknown bytes, print them out and reset
+                if unknown_bytes != b'':
+                    print('Unknown at', unknown_start, repr(unknown_bytes.rstrip(b'\x00')))
+                    unknown_bytes = b''
+                    unknown_start = None
+
+                # Print out the position, type, name and value of the known value
+                print('Known field at {: >4},'.format(current_position), end=' ')
+                name, position, type_ = knowns[current_position]
+                if type_ == 'x-time':
+                    print('x-time, "{: <19}'.format(name + '"'), unpack(ENDIAN + 'f', file_.read(4))[0] / 60000)
+                elif type_ == 'utf16':
+                    print(' utf16, "{: <19}'.format(name + '"'), parse_utf16_string(file_))
+                else:
+                    size = struct.calcsize(type_)
+                    print('{: >6}, "{: <19}'.format(type_, name + '"'), unpack(type_, file_.read(size))[0])
+            else:  # We do not know about a data field at this position If we have already
+                # collected 4 zero bytes, assume that we are done with this unkonw field,
+                # print and reset
+                if unknown_bytes[-4:] == b'\x00\x00\x00\x00':
+                    print('Unknown at', unknown_start, repr(unknown_bytes.rstrip(b'\x00')))
+                    unknown_bytes = b''
+                    unknown_start = None
+
+                # Read one byte and save it
+                one_byte = file_.read(1)
+                if unknown_bytes == b'':
+                    # Only start a new collection of unknown bytes, if this byte is not a zero byte
+                    if one_byte != b'\x00':
+                        unknown_bytes = one_byte
+                        unknown_start = file_.tell() - 1
+                else:
+                    unknown_bytes += one_byte
+
+        file_.close()
+
+    def _parse_data(self, file_):
+        """Parse the data"""
+        # Go to the end of the file and calculate how many points 8 byte floats there are
+        file_.seek(0, 2)
+        n_points = (file_.tell() - self.data_start) // 8
+
+        # Read the data into a numpy array
+        file_.seek(self.data_start)
+        return numpy.fromfile(file_, dtype='<d', count=n_points) * self.metadata['yscaling']
+
+    @cached_property
+    def times(self):
+        """Return the time values for the data set (x-value)"""
+        return numpy.linspace(self.metadata['start_time'], self.metadata['end_time'], len(self.values))
