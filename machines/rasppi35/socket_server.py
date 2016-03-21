8b9e2ac776948e4012f22d0c7441475f8fe56f6e
Robert Jensen <jensen.robert@gmail.com>
2016-03-21 16:36:24 +0100

    Code cleanup

--- machines/rasppi35/socket_server.py
+++ machines/rasppi35/socket_server.py
@@ -1,5 +1,5 @@
-# pylint: disable=R0913,W0142,C0103 
-
+""" MFC Control for VHP """
+from __future__ import print_function
 import threading
 import time
 import PyExpLabSys.drivers.brooks_s_protocol as brooks
@@ -12,7 +12,7 @@ class FlowControl(threading.Thread):
     def __init__(self, mfcs, pullsocket, pushsocket):
         threading.Thread.__init__(self)
         self.mfcs = mfcs
-        print mfcs
+        print(mfcs)
         self.pullsocket = pullsocket
         self.pushsocket = pushsocket
         self.running = True
@@ -21,7 +21,7 @@ class FlowControl(threading.Thread):
         while self.running:
             time.sleep(0.1)
             qsize = self.pushsocket.queue.qsize()
-            print qsize
+            print(qsize)
             while qsize > 0:
                 element = self.pushsocket.queue.get()
                 mfc = element.keys()[0]
@@ -29,31 +29,37 @@ class FlowControl(threading.Thread):
                 qsize = self.pushsocket.queue.qsize()
 
             for mfc in self.mfcs:
-                flow =  self.mfcs[mfc].read_flow()
+                flow = self.mfcs[mfc].read_flow()
                 #print(mfc + ': ' + str(flow))
                 self.pullsocket.set_point_now(mfc, flow)
 
 
-port = '/dev/serial/by-id/usb-FTDI_USB-RS485_Cable_FTWGRR44-if00-port0'
-devices = ['F25600004', 'F25600005', 'F25600006', 'F25600001', 'F25600002', 'F25600003', 'F25698001']
-datasocket = DateDataPullSocket('vhp_mfc_control',
-                                devices,
-                                timeouts=[3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0],
-                                port=9000)
-datasocket.start()
+def main():
+    """ Main function """
+    port = '/dev/serial/by-id/usb-FTDI_USB-RS485_Cable_FTWGRR44-if00-port0'
+    devices = ['F25600004', 'F25600005', 'F25600006', 'F25600001', 'F25600002',
+               'F25600003', 'F25698001']
+    datasocket = DateDataPullSocket('vhp_mfc_control',
+                                    devices,
+                                    timeouts=[3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0],
+                                    port=9000)
+    datasocket.start()
 
-pushsocket = DataPushSocket('vhp_push_control', action='enqueue')
-pushsocket.start()
+    pushsocket = DataPushSocket('vhp_push_control', action='enqueue')
+    pushsocket.start()
 
-i = 0
-mfcs = {}
-for device in devices:
-    mfcs[device] = brooks.Brooks(device, port=port)
-    print mfcs[device].read_flow()
+    i = 0
+    mfcs = {}
+    for device in devices:
+        mfcs[device] = brooks.Brooks(device, port=port)
+        print(mfcs[device].read_flow())
 
-fc = FlowControl(mfcs, datasocket, pushsocket)
-fc.start()
+    fc = FlowControl(mfcs, datasocket, pushsocket)
+    fc.start()
 
-while True:
-    pass
+    while True:
+        time.sleep(0.5)
+
+if __name__ == '__main__':
+    main()        
 
