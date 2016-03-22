a49eb2d05b7b4494a295b7ba1b8bfb32703ce15a
Robert Jensen <jensen.robert@gmail.com>
2016-03-22 20:28:03 +0100

    Python 3 fixes

--- machines/rasppi28/datalogger.py
+++ machines/rasppi28/datalogger.py
@@ -1,10 +1,9 @@
-""" Pressure and temperature logger """
-# pylint: disable=C0301,R0904, C0103
-
+""" Pressure and temperature logger PS"""
+from __future__ import print_function
 import threading
 import time
 import logging
-from PyExpLabSys.common.loggers import ContinuousLogger
+from PyExpLabSys.common.database_saver import ContinuousDataSaver
 from PyExpLabSys.common.sockets import DateDataPullSocket
 from PyExpLabSys.common.value_logger import ValueLogger
 #from PyExpLabSys.common.sockets import LiveSocket
@@ -43,53 +42,57 @@ class PressureReader(threading.Thread):
                     self.main = press[1]
                     self.ttl = 50
             except IndexError:
-                print "av"
+                print("av")
             time.sleep(5)
 
+def main():
+    """ Main function """
+    logging.basicConfig(filename="logger.txt", level=logging.ERROR)
+    logging.basicConfig(level=logging.ERROR)
 
-logging.basicConfig(filename="logger.txt", level=logging.ERROR)
-logging.basicConfig(level=logging.ERROR)
+    ports = '/dev/serial/by-id/'
+    ports += 'usb-Prolific_Technology_Inc._USB-Serial_Controller_D-if00-port0'
+    xgs_instance = xgs600.XGS600Driver(ports)
+    print(xgs_instance.read_all_pressures())
 
-ports = '/dev/serial/by-id/'
-ports += 'usb-Prolific_Technology_Inc._USB-Serial_Controller_D-if00-port0'
-xgs_instance = xgs600.XGS600Driver(ports)
-print xgs_instance.read_all_pressures()
+    pressure = PressureReader(xgs_instance)
+    pressure.start()
 
-pressure = PressureReader(xgs_instance)
-pressure.start()
+    time.sleep(2.5)
 
-time.sleep(2.5)
+    codenames = ['ps_qms_pressure', 'ps_chamber_pressure']
+    loggers = {}
+    loggers[codenames[0]] = ValueLogger(pressure, comp_val=0.1, low_comp=1e-11,
+                                        comp_type='log', channel=0)
+    loggers[codenames[0]].start()
+    loggers[codenames[1]] = ValueLogger(pressure, comp_val=0.1, low_comp=1e-11,
+                                        comp_type='log', channel=1)
+    loggers[codenames[1]].start()
 
-codenames = ['ps_qms_pressure', 'ps_chamber_pressure']
-loggers = {}
-loggers[codenames[0]] = ValueLogger(pressure, comp_val = 0.1, low_comp=1e-11,
-                                    comp_type = 'log', channel = 0)
-loggers[codenames[0]].start()
-loggers[codenames[1]] = ValueLogger(pressure, comp_val = 0.1, low_comp=1e-11,
-                                    comp_type = 'log', channel = 1)
-loggers[codenames[1]].start()
+    #livesocket = LiveSocket('PS', codenames, 2)
+    #livesocket.start()
 
-#livesocket = LiveSocket('PS', codenames, 2)
-#livesocket.start()
-
-socket = DateDataPullSocket('PS pressure logger',
-                            codenames, timeouts=2 * [1.0], port=9001)
-socket.start()
+    socket = DateDataPullSocket('PS pressure logger',
+                                codenames, timeouts=2 * [1.0], port=9001)
+    socket.start()
 
 
-db_logger = ContinuousLogger(table='dateplots_ps',
-                             username=credentials.user,
-                             password=credentials.passwd,
-                             measurement_codenames=codenames)
-db_logger.start()
+    db_logger = ContinuousDataSaver(continuous_data_table='dateplots_ps',
+                                    username=credentials.user,
+                                    password=credentials.passwd,
+                                    measurement_codenames=codenames)
+    db_logger.start()
 
-while pressure.isAlive():
-    time.sleep(0.5)
-    for name in codenames:
-        v = loggers[name].read_value()
-        #livesocket.set_point_now(name, v)
-        socket.set_point_now(name, v)
-        if loggers[name].read_trigged():
-            print v
-            db_logger.enqueue_point_now(name, v)
-            loggers[name].clear_trigged()
+    while pressure.isAlive():
+        time.sleep(0.5)
+        for name in codenames:
+            value = loggers[name].read_value()
+            #livesocket.set_point_now(name, value)
+            socket.set_point_now(name, value)
+            if loggers[name].read_trigged():
+                print(value)
+                db_logger.save_point_now(name, value)
+                loggers[name].clear_trigged()
+
+if __name__ == '__main__':
+    main()
