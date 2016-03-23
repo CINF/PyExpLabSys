3aa3a2b12e910a2326934dd74c14a8f4905b1d80
Kenneth Nielsen <k.nielsen81@gmail.com>
2016-03-23 12:34:25 +0000

    Added sequential logging criterium logger

--- PyExpLabSys/common/value_logger.py
+++ PyExpLabSys/common/value_logger.py
@@ -70,3 +70,106 @@ class ValueLogger(threading.Thread):
                 self.status['trigged'] = True
                 self.last['time'] = time.time()
                 self.last['val'] = self.value
+
+
+class LoggingCriteriumChecker(object):
+    """Class that performs a logging criterium check and stores last values for a series
+    of meaurements
+
+    """
+
+    def __init__(self, codenames=(()), types=(()), criteria=(()), time_outs=None,
+                 low_compare_values=None):
+        """Initialize the logging criterium checker
+
+        .. note:: If given, codenames, types and criteria must be sequences with the same
+            number of elements
+
+        Args:
+            codename (sequence): A sequence of codenames
+            types (sequence): A sequence of logging criteria types ('lin' or 'log')
+            criteria (sequence): A sequence of floats indicating the values change
+                that should trigger a log
+            time_outs (sequence): An (optional) sequence of floats or integers that
+                indicate the logging timeouts in seconds. Defaults to 600.
+            low_compare_values (sequence): An (optional) sequence of lower limits under
+                which the logging criteri will never trigger
+        """
+        error_message = None
+        if len(types) != len(codenames):
+            error_message = 'The must be exactly as many types as codenames'
+        if len(criteria) != len(codenames):
+            error_message = 'The must be exactly as many criteria as codenames'
+        if low_compare_values is not None and len(low_compare_values) != len(codenames):
+            error_message = 'If low_compare_values is given, it must contain as many '\
+                            'values as there are codenames'
+        if time_outs is not None and len(time_outs) != len(codenames):
+            error_message = 'If time_outs is given, it must contain as many '\
+                            'values as there are codenames'
+        if error_message is not None:
+            raise ValueError(error_message)
+
+        # Init local variables
+        if low_compare_values is None:
+            low_compare_values = [None for _ in codenames]
+        if time_outs is None:
+            time_outs = [600 for _ in codenames]
+
+        self.last_values = {}
+        self.last_time = {}
+        self.measurements = {}
+        for codename, type_, criterium, time_out, low_compare in zip(codenames, types,
+                                                                     criteria, time_outs,
+                                                                     low_compare_values):
+            self.add_measurement(codename, type_, criterium, time_out, low_compare)
+
+    @property
+    def codenames(self):
+        """Return the codenames"""
+        return list(self.measurements.keys())
+
+    def add_measurement(self, codename, type_, criterium, time_out=600, low_compare=None):
+        """Add a measurement channel"""
+        self.measurements[codename] = {
+            'type': type_, 'criterium': criterium, 'low_compare': low_compare,
+            'time_out': time_out,
+        }
+        # Unix timestamp
+        self.last_time[codename] = 0
+
+    def check(self, codename, value):
+        """Check a new value"""
+        try:
+            measurement = self.measurements[codename]
+        except KeyError:
+            raise KeyError('Codename \'{}\' is unknown'.format(codename))
+
+        # Pull out last value and update it with current
+        last = self.last_values.get(codename)
+        self.last_values[codename] = value
+
+        # Always trigger for the first value
+        if last is None:
+            self.last_time[codename] = time.time()
+            return True
+
+        # Always trigger on a timeout
+        if time.time() - self.last_time[codename] > measurement['time_out']:
+            self.last_time[codename] = time.time()
+            return True
+
+        # Check if below lower compare value
+        if measurement['low_compare'] is not None and value < measurement['low_compare']:
+            return False
+
+        # Compare
+        abs_diff = abs(value - last)
+        if measurement['type'] == 'lin':
+            if abs_diff > measurement['criterium']:
+                self.last_time[codename] = time.time()
+                return True
+        elif measurement['type'] == 'log':
+            if abs_diff / abs(last) > measurement['criterium']:
+                self.last_time[codename] = time.time()
+                return True
+        return False
