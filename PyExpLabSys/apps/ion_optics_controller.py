da803ddac767e46e9fc310d7c3097dea476e23f9
Robert Jensen <jensen.robert@gmail.com>
2016-04-12 09:03:48 +0200

    Improved ion optics controller and moved to apps

--- PyExpLabSys/apps/ion_optics_controller.py
+++ PyExpLabSys/apps/ion_optics_controller.py
@@ -1,4 +1,4 @@
-""" Temperature controller """
+""" Ion Optics Control software """
 from __future__ import print_function
 import time
 import threading
@@ -7,9 +7,8 @@ import PyExpLabSys.drivers.stahl_hv_400 as stahl_hv_400
 from PyExpLabSys.common.sockets import DateDataPullSocket
 from PyExpLabSys.common.sockets import DataPushSocket
 
-
 class CursesTui(threading.Thread):
-    """ Text user interface for furnace heating control """
+    """ Text user interface for ion optics control """
     def __init__(self, ioc_class):
         threading.Thread.__init__(self)
         self.start_time = time.time()
@@ -27,17 +26,27 @@ class CursesTui(threading.Thread):
             self.screen.addstr(2, 2, 'Running')
             val = self.ioc.status['temperature']
             self.screen.addstr(3, 2, "Device Temeperature: {0:.2f}C  ".format(val))
+            if self.ioc.status['output_error'] is False:
+                self.screen.addstr(4, 2, "All channels ok ")
+            else:
+                self.screen.addstr(4, 2, "Error in channel")
 
+            self.screen.addstr(7, 13, 'Set voltage')
+            self.screen.addstr(7, 27, 'Measured')
+            self.screen.addstr(7, 38, 'Status')
             for i in range(0, len(self.ioc.lenses)):
                 lens = self.ioc.lenses[i]
-                channel_string = "Channel " + str(i + 1) + ", " + lens + ": {0:.3f}V   "
-                self.screen.addstr(5 + i, 2, channel_string.format(self.ioc.voltages[lens]))
+                ch_string = "Channel " + str(i + 1) + ":  {0: >9.2f}V   {1: >7.1f}V     {2}  "
+                set_v = self.ioc.set_voltages[lens]
+                actual_v = self.ioc.actual_voltages[lens]
+                status = self.ioc.status['channel_status'][i+1]
+                self.screen.addstr(8 + i, 2, ch_string.format(set_v, actual_v, status))
 
             val = self.ioc.pushsocket.queue.qsize()
-            self.screen.addstr(14, 2, "queue size: {0:.0f}".format(val))
+            self.screen.addstr(16, 2, "queue size: {0:.0f}".format(val))
 
             val = time.time() - self.start_time
-            self.screen.addstr(15, 2, "Run time: {0:.0f}s".format(val))
+            self.screen.addstr(17, 2, "Run time: {0:.0f}s".format(val))
 
             key_pressed = self.screen.getch()
             if key_pressed == ord('q'):
@@ -67,42 +76,44 @@ class IonOpticsControl(threading.Thread):
         self.pushsocket.start()
         self.ion_optics = stahl_hv_400.StahlHV400(port)
         self.lenses = lenses
-        self.voltages = {}
+        self.set_voltages = {}
+        self.actual_voltages = {}
         for lens in self.lenses:
-            self.voltages[lens] = 0
+            self.set_voltages[lens] = 0
+            self.actual_voltages[lens] = 0
         self.status = {}
+        self.status['channel_status'] = {}
+        for i in range(1, len(self.lenses)+1):
+            self.status['channel_status'][i] = False
         self.status['temperature'] = None
-        self.status['output_ok'] = None
+        self.status['output_error'] = None
         self.quit = False
 
     def run(self):
+        current_lens = 1
         while not self.quit:
             self.status['temperature'] = self.ion_optics.read_temperature()
-            self.status['output_ok'] = True # Not a permanent solution...
+            if self.status['temperature'] > 50:
+                for lens in self.lenses:
+                    self.set_voltages[lens] = 0
+                    
+            self.status['channel_status'] = self.ion_optics.check_channel_status()
+            self.status['output_error'] = False in self.status['channel_status']
+            
+            actual_voltage = self.ion_optics.query_voltage(current_lens)
+            self.actual_voltages[self.lenses[current_lens-1]] = actual_voltage
+            if current_lens == len(self.lenses):
+                current_lens = 1
+            else:
+                current_lens += 1
+
             qsize = self.pushsocket.queue.qsize()
             while qsize > 0:
                 element = self.pushsocket.queue.get()
                 lens = str(list(element.keys())[0])
                 value = element[lens]
-                self.voltages[lens] = value
+                self.set_voltages[lens] = value
                 channel_number = self.lenses.index(lens) + 1
                 self.ion_optics.set_voltage(channel_number, value)
                 qsize = self.pushsocket.queue.qsize()
-            time.sleep(1)
-
-
-def main():
-    """ Main function """
-    # The ordering of the list will set the corresponding channel number
-    lenses = ['lens_a', 'lens_b', 'lens_c', 'lens_d', 'lens_e']
-    port = '/dev/serial/by-id/usb-Stahl_Electronics_HV_Series_HV069-if00-port0'
-    ioc = IonOpticsControl(port, 'TOF', lenses)
-    ioc.start()
-    time.sleep(1)
-
-    tui = CursesTui(ioc)
-    tui.daemon = True
-    tui.start()
-
-if __name__ == '__main__':
-    main()
+            time.sleep(0.1)
---
+++ machines/rasppi74/ion_optics_tof.py
@@ -0,0 +1,20 @@
+""" Ion Optics Controll for TOF """
+from PyExpLabSys.apps.ion_optics_controller import CursesTui as CursesTui
+from PyExpLabSys.apps.ion_optics_controller import IonOpticsControl as IonOpticsControl
+import time
+
+def main():
+    """ Main function """
+    # The ordering of the list will set the corresponding channel number
+    lenses = ['lens_a', 'lens_b', 'lens_c', 'lens_d', 'lens_e']
+    port = '/dev/serial/by-id/usb-Stahl_Electronics_HV_Series_HV069-if00-port0'
+    ioc = IonOpticsControl(port, 'TOF', lenses)
+    ioc.start()
+    time.sleep(1)
+
+    tui = CursesTui(ioc)
+    tui.daemon = True
+    tui.start()
+
+if __name__ == '__main__':
+    main()
