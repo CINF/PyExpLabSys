2edd8f03f39fc1a70ae2abd35fb665ba320f9344
Robert Jensen <jensen.robert@gmail.com>
2016-04-12 08:20:24 +0200

    Implemented remaining functionality and ported to Python 3

--- PyExpLabSys/drivers/stahl_hv_400.py
+++ PyExpLabSys/drivers/stahl_hv_400.py
@@ -1,6 +1,8 @@
 """ Driver for Stahl HV 400 Ion Optics Supply """
 from __future__ import print_function
 import serial
+from PyExpLabSys.common.supported_versions import python2_and_3
+python2_and_3(__file__)
 
 class StahlHV400(object):
     """ Driver for Stahl HV 400 Ion Optics Supply """
@@ -38,9 +40,16 @@ class StahlHV400(object):
 
     def query_voltage(self, channel):
         """ Something is all wrong here... """
-        reply = self.comm(self.serial_number + ' Q' + str(channel).zfill(2))
-        #print(self.serial_number + ' Q' + str(channel).zfill(2))
-        return reply
+        error = 20
+        while error > 0:
+            try:
+                reply = self.comm(self.serial_number + ' Q' + str(channel).zfill(2))
+                value = float(reply[:-1].replace(',', '.'))
+                error = -1
+            except ValueError:
+                error = error - 1
+                value = '-99999'
+        return value
 
     def set_voltage(self, channel, value):
         """ Set the voltage of a channel """
@@ -51,6 +60,8 @@ class StahlHV400(object):
         assert isinstance(channel, int)
         self.comm(self.serial_number + ' CH' + str(channel).zfill(2) +
                   ' ' + '{0:.6f}'.format(fraction))
+        command = self.serial_number + ' DIS L CH' + str(channel).zfill(2) + ' {0:.2f}V'
+        self.comm(command.format(value))
         return True # Consider to run check_channel_status
 
     def read_temperature(self):
@@ -61,10 +72,39 @@ class StahlHV400(object):
 
     def check_channel_status(self):
         """ Check status of channel """
-        self.comm(self.serial_number + 'LOCK')
+        reply = self.comm(self.serial_number + ' LOCK')
+        channel_1_4 = bin(ord(reply[0]))[-4:]
+        channel_5_8 = bin(ord(reply[1]))[-4:]
+        channel_status = {}
+        channel_status[4] = channel_1_4[0] == '0'
+        channel_status[3] = channel_1_4[1] == '0'
+        channel_status[2] = channel_1_4[2] == '0'
+        channel_status[1] = channel_1_4[3] == '0'
+        channel_status[8] = channel_5_8[0] == '0'
+        channel_status[7] = channel_5_8[1] == '0'
+        channel_status[6] = channel_5_8[2] == '0'
+        channel_status[5] = channel_5_8[3] == '0'
+        return channel_status
 
 if __name__ == '__main__':
     HV400 = StahlHV400('/dev/ttyUSB0')
-    HV400.set_voltage(1, 0)
-    print(HV400.read_temperature())
-    print(HV400.query_voltage(1))
+    HV400.set_voltage(1, -159.1)
+    HV400.set_voltage(2, -69.1)
+    #HV400.set_voltage(3, -47.9) # 55.9
+    #HV400.set_voltage(4, -47.9) # 46.9
+    #HV400.set_voltage(5, -44.9) # 44.9
+    #HV400.set_voltage(6, 0)
+    #HV400.set_voltage(7, 0)
+    #HV400.set_voltage(8, 0)
+    #print(HV400.read_temperature())
+    print(HV400.check_channel_status())
+    status = (HV400.check_channel_status())
+    print(False in status)
+    #print(HV400.query_voltage(1))
+    #print(HV400.query_voltage(2))
+    #print(HV400.query_voltage(3))
+    #print(HV400.query_voltage(4))
+    #print(HV400.query_voltage(5))
+    #print(HV400.query_voltage(6))
+    #print(HV400.query_voltage(7))
+    #print(HV400.query_voltage(8))
