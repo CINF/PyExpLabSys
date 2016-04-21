0263c584b2325c8b88523e9c46836a2dd1d6358d
Robert Jensen <jensen.robert@gmail.com>
2016-04-21 09:07:04 +0200

    Cleanup + update to new live sockets

--- machines/rasppi04/datalogger.py
+++ machines/rasppi04/datalogger.py
@@ -1,16 +1,14 @@
 """ Pressure and temperature logger """
-# pylint: disable=C0301,R0904, C0103
 from __future__ import print_function
 import threading
 import time
-import logging
-from PyExpLabSys.common.loggers import ContinuousLogger
+from PyExpLabSys.common.database_saver import ContinuousDataSaver
 from PyExpLabSys.common.sockets import DateDataPullSocket
 from PyExpLabSys.common.sockets import LiveSocket
 from PyExpLabSys.common.value_logger import ValueLogger
 import PyExpLabSys.drivers.xgs600 as xgs600
 import PyExpLabSys.drivers.agilent_34972A as agilent_34972A
-
+import credentials
 
 class MuxReader(threading.Thread):
     """ Analog reader """
@@ -26,7 +24,7 @@ class MuxReader(threading.Thread):
         self.ttl = self.ttl - 1
         if self.ttl < 0:
             self.quit = True
-        return(self.temperature)
+        return self.temperature
 
     def run(self):
         while not self.quit:
@@ -49,7 +47,7 @@ class PressureReader(threading.Thread):
         self.ttl = self.ttl - 1
         if self.ttl < 0:
             self.quit = True
-        return(self.pressure)
+        return self.pressure
 
     def run(self):
         while not self.quit:
@@ -62,44 +60,51 @@ class PressureReader(threading.Thread):
                 print("av")
                 self.pressure = 0
 
-logging.basicConfig(filename="logger.txt", level=logging.ERROR)
-logging.basicConfig(level=logging.ERROR)
+def main():
+    """ Main code """
+    mux_instance = agilent_34972A.Agilent34972ADriver('volvo-agilent-34972a')
+    port = '/dev/serial/by-id/usb-Prolific_Technology_Inc._USB-Serial_Controller-if00-port0'
+    xgs_instance = xgs600.XGS600Driver(port)
 
-mux_instance = agilent_34972A.Agilent34972ADriver('volvo-agilent-34972a')
-#mux_instance = agilent_34972A.Agilent34972ADriver('10.54.6.144')
-xgs_instance = xgs600.XGS600Driver('/dev/serial/by-id/usb-Prolific_Technology_Inc._USB-Serial_Controller-if00-port0')
+    analog_measurement = MuxReader(mux_instance)
+    analog_measurement.start()
 
-analog_measurement = MuxReader(mux_instance)
-analog_measurement.start()
+    pressure = PressureReader(xgs_instance)
+    print('-')
+    pressure.start()
 
-pressure = PressureReader(xgs_instance)
-pressure.start()
+    time.sleep(2.5)
 
-time.sleep(2.5)
+    codenames = ['volvo_pressure', 'volvo_temperature']
+    loggers = {}
+    loggers[codenames[0]] = ValueLogger(pressure, comp_val=0.1, comp_type='log')
+    loggers[codenames[0]].start()
+    loggers[codenames[1]] = ValueLogger(analog_measurement, comp_val=0.5, comp_type='lin')
+    loggers[codenames[1]].start()
 
-codenames = ['volvo_pressure', 'volvo_temperature']
-loggers = {}
-loggers[codenames[0]] = ValueLogger(pressure, comp_val = 0.1, comp_type = 'log')
-loggers[codenames[0]].start()
-loggers[codenames[1]] = ValueLogger(analog_measurement, comp_val = 0.5, comp_type = 'lin')
-loggers[codenames[1]].start()
+    socket = DateDataPullSocket('Volvo data logger', codenames, timeouts=[1.0, 1.0])
+    socket.start()
 
-socket = DateDataPullSocket('Volvo data logger', codenames, timeouts=[1.0, 1.0])
-socket.start()
+    livesocket = LiveSocket('Volvo data logger', codenames)
+    livesocket.start()
 
-livesocket = LiveSocket('Volvo data logger', codenames, 2)
-livesocket.start()
+    db_logger = ContinuousDataSaver(continuous_data_table='dateplots_volvo',
+                                    username=credentials.user,
+                                    password=credentials.passwd,
+                                    measurement_codenames=codenames)
+    db_logger.start()
 
-db_logger = ContinuousLogger(table='dateplots_volvo', username='volvo', password='volvo', measurement_codenames=['volvo_pressure', 'volvo_temperature'])
-db_logger.start()
+    #while pressure.isAlive() and analog_measurement.isAlive():
+    while True:
+        time.sleep(0.25)
+        for name in codenames:
+            value = loggers[name].read_value()
+            socket.set_point_now(name, value)
+            livesocket.set_point_now(name, value)
+            if loggers[name].read_trigged():
+                print(value)
+                db_logger.save_point_now(name, value)
+                loggers[name].clear_trigged()
 
-while pressure.isAlive() and analog_measurement.isAlive():
-    time.sleep(0.25)
-    for name in codenames:
-        v = loggers[name].read_value()
-        socket.set_point_now(name, v)
-        livesocket.set_point_now(name, v)
-        if loggers[name].read_trigged():
-            print(v)
-            db_logger.enqueue_point_now(name, v)
-            loggers[name].clear_trigged()
+if __name__ == '__main__':
+    main()
