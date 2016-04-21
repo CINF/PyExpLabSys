7bdd5f00ee555dcd79f6b7f2be41c4a15b5e1a4a
Robert Jensen <jensen.robert@gmail.com>
2016-04-21 08:14:58 +0200

    Upgraded to new live sockets

--- machines/rasppi20/chiller_logger.py
+++ machines/rasppi20/chiller_logger.py
@@ -31,7 +31,7 @@ def main():
         loggers[codenames[i]].start()
 
     live_socket_name = 'Sputterchamber PS'
-    live_socket = LiveSocket(live_socket_name, codenames, 2)
+    live_socket = LiveSocket(live_socket_name, codenames)
     live_socket.start()
     LOG.info('Live socket init and started with name "%s"', live_socket_name)
 
