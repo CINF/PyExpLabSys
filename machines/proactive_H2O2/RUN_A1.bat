b00894d9b1848109085124a1138fd8f1c0bc1434
Kenneth Nielsen <k.nielsen81@gmail.com>
2016-07-26 09:51:01 +0200

    Minor change

--- machines/proactive_H2O2/RUN_A1.bat
+++ machines/proactive_H2O2/RUN_A1.bat
@@ -1,2 +1,2 @@
 
-START "" pythonw my_stepped_program.py A 1 steps_A1.yaml
+START "" pythonw voltage_current_program.py A 1 steps_A1.yaml
--- machines/proactive_H2O2/RUN_A2.bat
+++ machines/proactive_H2O2/RUN_A2.bat
@@ -1,2 +1,2 @@
 
-START "" pythonw my_stepped_program.py A 2 steps_A2.yaml
+START "" pythonw voltage_current_program.py A 2 steps_A2.yaml
