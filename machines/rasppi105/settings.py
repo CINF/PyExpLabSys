f35e463cb65acdcac7b472af1ae8b9a750abe9f3
Robert Jensen <jensen.robert@gmail.com>
2016-11-22 08:04:29 +0100

    Added reactor pressure to settings

--- machines/rasppi105/settings.py
+++ machines/rasppi105/settings.py
@@ -7,15 +7,21 @@ setup = 'palle'
 
 channels = {}
 channels[0] = {'host':'rasppi24','port':9000,
+               'command':'M11213502A', 'codename':'mgw_reactor_pressure',
+               'comp_value': 1}
+
+
+channels[1] = {'host':'rasppi24','port':9000,
                'command':'M11200362B', 'codename':'mgw_mfc_flow_01',
                'comp_value': 0.1}
 
 
-channels[1] = {'host':'rasppi105','port':9000,
+channels[2] = {'host':'rasppi105','port':9000,
                'command':'3F2320902001', 'codename':'mgw_mfc_flow_02',
                'comp_value': 0.1}
 
-channels[2] = {'host':'rasppi105','port':9000,
+channels[3] = {'host':'rasppi105','port':9000,
                'command':'3F2320901001', 'codename':'mgw_mfc_flow_03',
                'comp_value': 0.1}
+
 
