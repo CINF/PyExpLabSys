345ebcf162318e21ab6f9311d1a07bb74d3e00a7
Robert Jensen <jensen.robert@gmail.com>
2016-12-12 10:46:54 +0100

    Cleanup + update to Python3

--- PyExpLabSys/drivers/omegabus.py
+++ PyExpLabSys/drivers/omegabus.py
@@ -1,53 +1,67 @@
-import serial
+""" Driver for OmegaBus devices """
+from __future__ import print_function
 import time
+import logging
+import serial
+from PyExpLabSys.common.supported_versions import python2_and_3
+# Configure logger as library logger and set supported python versions
+LOGGER = logging.getLogger(__name__)
+LOGGER.addHandler(logging.NullHandler())
+python2_and_3(__file__)
 
-class OmegaBus():
-
-    def __init__(self, device='/dev/ttyUSB0'):
-        self.f = serial.Serial(device,9600)
+class OmegaBus(object):
+    """ Driver for OmegaBus devices """
+    def __init__(self, device='/dev/ttyUSB1'):
+        self.ser = serial.Serial(device, 9600)
         time.sleep(0.1)
 
-    def ReadValue(self,channel):
-        self.f.write("$" + str(channel) + "RD" + "\r")
-        time.sleep(0.1)
-        temp_string = self.f.read(self.f.inWaiting())
+    def comm(self, command):
+        """ Handles serial protocol """
+        command = command + "\r"
+        command = command.encode('ascii')
+        self.ser.write(command)
+        time.sleep(0.2)
+        answer = self.ser.read(self.ser.inWaiting())
+        answer = answer.decode()
+        return answer
+
+    def read_value(self, channel):
+        """ Read the measurement value """
+        temp_string = self.comm("$" + str(channel) + "RD")
         if temp_string[1] == "*":
             temp_string = temp_string[3:]
         temp_fahrenheit = float(temp_string)
         temp_celsius = 5*(temp_fahrenheit-32)/9
         return float(temp_celsius)
 
-    def ReadMax(self,channel):
-        self.f.write("$" + str(channel) + "RMX" + "\r")
-        time.sleep(0.25)
-        temp_string = self.f.read(self.f.inWaiting())
+    def read_max(self, channel):
+        """ The maximum read-out value """
+        temp_string = self.comm("$" + str(channel) + "RMX")
         if temp_string[1] == "*":
             temp_string = temp_string[3:]
         return float(temp_string)
 
-    def ReadMin(self,channel):
-        self.f.write("$" + str(channel) + "RMN" + "\r")
-        time.sleep(0.25)
-        temp_string = self.f.read(self.f.inWaiting())
+    def read_min(self, channel):
+        """ The minimum read-out value """
+        temp_string = self.comm("$" + str(channel) + "RMN")
         if temp_string[1] == "*":
             temp_string = temp_string[2:]
         return float(temp_string)
 
-    def ReadSetup(self):
-        self.f.write("$" + "1RS" + "\r")
-        time.sleep(0.25)
-        rs_string = self.f.read(self.f.inWaiting())
+    def read_setup(self):
+        """ Read Device setup information """
+        rs_string = self.comm("$" + "1RS")
         if rs_string[1] == "*":
             rs_string = rs_string[2:]
         byte1 = rs_string[0:2]
         byte2 = rs_string[2:4]
         byte3 = rs_string[4:6]
-        byte4 = rs_string[6:8]
+        #byte4 = rs_string[6:8]
 
         setupstring = ""
-        setupstring += "Base adress: " + chr(int(byte1,16)) + "\n"
+        setupstring += "Base adress: " + chr(int(byte1, 16)) + "\n"
 
-        bits_2 = (bin(int(byte2,16))[2:]).zfill(8)
+        bits_2 = (bin(int(byte2, 16))[2:]).zfill(8)
         setupstring += "No linefeed\n" if bits_2[0] == '0' else "Linefeed\n"
         if bits_2[2] == '0': #bits_2[1] will contain the parity if not none
             setupstring += "Parity: None"  + "\n"
@@ -55,31 +69,26 @@ class OmegaBus():
         if bits_2[4:8] == '0010':
             setupstring += "Baud rate: 9600"  + "\n"
 
-        bits_3 = (bin(int(byte3,16))[2:]).zfill(8)
+        bits_3 = (bin(int(byte3, 16))[2:]).zfill(8)
 
         setupstring += "Channel 3 enabled\n" if bits_3[0] == '1' else "Channel 3 disabled\n"
         setupstring += "Channel 2 enabled\n" if bits_3[1] == '1' else "Channel 2 disabled\n"
         setupstring += "Channel 1 enabled\n" if bits_3[2] == '1' else "Channel 1 disabled\n"
-        setupstring += "No cold junction compensation\n" if bits_3[3] == '1' else "Cold junction compensation enabled\n"
+        if bits_3[3] == '1':
+            setupstring += "No cold junction compensation\n"
+        else:
+            setupstring += "Cold junction compensation enabled\n"
         setupstring += "Unit: Fahrenheit\n" if bits_3[4] == '1' else "Unit: Celsius\n"
-
-        #print (bin(int(byte4,16))[2:]).zfill(8)
-        
+        #print (bin(int(byte4,16))[2:]).zfill(8
         return setupstring
 
-    def ChangeTemperatureScale(self,scale):
-        if not (scale=='f' or scale=='c'):
-            return(False)
 
 if __name__ == "__main__":
-    omega = OmegaBus()
-    print omega.ReadValue(1)
-    print omega.ReadValue(2)
-    print omega.ReadValue(3)
-    print omega.ReadValue(4)
-    #print omega.ReadMin(1)
-    #print omega.ReadMax(1)
-
-    #print omega.ReadSetup()
-
-    #print ChangeTemperatureScale('f')
+    OMEGA = OmegaBus()
+    print(OMEGA.read_value(1))
+    print(OMEGA.read_value(2))
+    print(OMEGA.read_value(3))
+    print(OMEGA.read_value(4))
+    #print(OMEGA.read_min(1))
+    #print(OMEGA.read_max(1))
+    #print(OMEGA.read_setup())
