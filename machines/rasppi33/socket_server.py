a4ab6da6294e00de523ab4a1271aa8038bf81981
Robert Jensen <jensen.robert@gmail.com>
2016-12-19 13:59:31 +0100

    Marked Python 3 support

--- machines/rasppi33/socket_server.py
+++ machines/rasppi33/socket_server.py
@@ -1,9 +1,11 @@
 # pylint: disable=R0913,W0142,C0103 
-
+""" Valve controll for VHP-setup """
 import time
 import PyExpLabSys.common.valve_control as valve_control
 from PyExpLabSys.common.sockets import DateDataPullSocket
 from PyExpLabSys.common.sockets import DataPushSocket
+from PyExpLabSys.common.supported_versions import python2_and_3
+python2_and_3(__file__)
 
 valve_names = [0] * 20
 for i in range(0, 20):
