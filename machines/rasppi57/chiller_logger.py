cbb75cb485492f1f5c1304dca31940dcf14dab93
Robert Jensen <jensen.robert@gmail.com>
2016-12-19 14:09:03 +0100

    Marked Python 3 status + cleanup

--- machines/rasppi57/chiller_logger.py
+++ machines/rasppi57/chiller_logger.py
@@ -1,14 +1,15 @@
 # pylint: disable=C0103
 """This file logs data from the chiller at the thetaprobe"""
-
 import time
 import math
 from PyExpLabSys.common.value_logger import ValueLogger
 from PyExpLabSys.common.chiller_reader import ChillerReader
-from PyExpLabSys.common.loggers import ContinuousLogger
+from PyExpLabSys.common.database_saver import ContinuousDataSaver
 from PyExpLabSys.common.sockets import LiveSocket
 from PyExpLabSys.common.utilities import get_logger
+from PyExpLabSys.common.supported_versions import python2_and_3
 import credentials
+python2_and_3(__file__)
 
 
 LOG = get_logger('chiller_logger_thetaprobe')
@@ -22,15 +23,15 @@ def main():
     reader.start()
     LOG.info('ChillerReader started')
 
-    codenames = ['thetaprobe_chiller_temperature', 
-                 'thetaprobe_chiller_flow', 
-                 'thetaprobe_chiller_temperature_ambient', 
-                 'thetaprobe_chiller_pressure', 
+    codenames = ['thetaprobe_chiller_temperature',
+                 'thetaprobe_chiller_flow',
+                 'thetaprobe_chiller_temperature_ambient',
+                 'thetaprobe_chiller_pressure',
                  'thetaprobe_chiller_temperature_setpoint']
     LOG.debug('Using codenames %s', codenames)
     loggers = {}
     for i in range(0, len(codenames)):
-        loggers[codenames[i]] = ValueLogger(reader, comp_val = 0.1, channel = i)
+        loggers[codenames[i]] = ValueLogger(reader, comp_val=0.1, channel=i)
         loggers[codenames[i]].start()
 
     live_socket_name = 'Thetaprobe chiller'
@@ -39,12 +40,12 @@ def main():
     LOG.info('Live socket init and started with name "%s"', live_socket_name)
 
     db_table = 'dateplots_thetaprobe'
-    db_logger = ContinuousLogger(table=db_table,
-                                 username=credentials.user,
-                                 password=credentials.passwd,
-                                 measurement_codenames=codenames)
+    db_logger = ContinuousDataSaver(continuous_data_table=db_table,
+                                    username=credentials.user,
+                                    password=credentials.passwd,
+                                    measurement_codenames=codenames)
     db_logger.start()
-    LOG.info('ContinuousLogger init and started on table "%s"',  db_table)
+    LOG.info('ContinuousLogger init and started on table "%s"', db_table)
 
     time.sleep(5)
 
@@ -56,7 +57,7 @@ def main():
                 live_socket.set_point_now(name, value)
                 if loggers[name].read_trigged():
                     LOG.debug('Log value %s for codename "%s"', value, name)
-                    db_logger.enqueue_point_now(name, value)
+                    db_logger.save_point_now(name, value)
                     loggers[name].clear_trigged()
 
 
