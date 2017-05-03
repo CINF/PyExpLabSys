a19e135bd1050cad80333ba4aff5ca6d0d1b0d10
Robert Jensen <jensen.robert@gmail.com>
2017-05-03 07:05:15 +0200

    Test program can now do actual measurements

--- machines/rasppi78/tester.py
+++ machines/rasppi78/tester.py
@@ -1,6 +1,7 @@
 import time
 import PyExpLabSys.drivers.agilent_34972A as agilent_34972A
 import PyExpLabSys.drivers.keithley_smu as keithley_smu
+from PyExpLabSys.common.database_saver import DataSetSaver, CustomColumn
 
 mux = agilent_34972A.Agilent34972ADriver(interface='usbtmc',
                                          connection_string='USB0::0x0957::0x2007::MY49011193::INSTR')
@@ -36,3 +37,26 @@ smu.set_voltage(0.01)
 current = smu.read_current()
 print(current)
 print(0.01/current)
+
+comment = 'IV-test'
+data_set_saver = DataSetSaver("measurements_dummy", "xy_values_dummy", "dummy", "dummy")
+data_set_saver.start()
+data_set_time = time.time()
+metadata = {"Time": CustomColumn(data_set_time, "FROM_UNIXTIME(%s)"), "comment": comment,
+            "type": 1, "preamp_range": 0, "sem_voltage": 0}
+for label in ["Ch1", "Ch2", "Ch3", "Ch4", "Ch5", "Ch6", "Ch7", "Ch8",]:
+    metadata["mass_label"] = label
+    data_set_saver.add_measurement(label, metadata)
+
+for channel in range(1, 9):
+    print('Channel: ' + str(channel))
+    mux.scpi_comm('ROUTE:CLOSE (@10' + str(channel) + ')' )
+    time.sleep(0.2)
+    V, I = smu.iv_scan(-0.2, 0.2, 0.001)    
+    for i in range(0, len(I)):
+        data_set_saver.save_point('Ch' + str(channel), (V[i], I[i]))
+    mux.scpi_comm('ROUTE:OPEN (@10' + str(channel) + ')' )
+    time.sleep(0.2)
+
+time.sleep(1)
+mux.scpi_comm('ROUTE:CLOSE (@101)')
