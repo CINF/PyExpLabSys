6b9407aa5a7e9a121be22d1a9a74b2bd5bb9c5a8
Robert Jensen <jensen.robert@gmail.com>
2017-05-24 08:26:38 +0200

    Error cleanup + marked Python 3 status

--- machines/rasppi81/rtd_calibrator.py
+++ machines/rasppi81/rtd_calibrator.py
@@ -1,12 +1,14 @@
+""" Simple program to log the resistance of a micro-reactor """
+from __future__ import print_function
 import time
 import PyExpLabSys.drivers.agilent_34972A as agilent_34972A
 from PyExpLabSys.common.database_saver import DataSetSaver, CustomColumn
+from PyExpLabSys.common.supported_versions import python2_and_3
+python2_and_3(__file__)
 
-#mux = agilent_34972A.Agilent34972ADriver(interface='usbtmc',
-#                                         connection_string='USB0::0x0957::0x2007::MY57000209::INSTR')
 
-class rtd_calibrator(object):
-
+class RtdCalibrator(object):
+    """ Simple class to log the resistance of a micro-reactor """
     def __init__(self, connection_string):
         self.mux = agilent_34972A.Agilent34972ADriver(interface='usbtmc',
                                                       connection_string=connection_string)
@@ -22,6 +24,7 @@ class rtd_calibrator(object):
         self.data['Temperature'] = None
 
     def update_values(self):
+        """ Update the current status of the reactor """
         comm_string = "CONFIGURE:RESISTANCE (@101,102,103,105,110,111,112)"
         self.mux.scpi_comm(comm_string)
         comm_string = "SENSE:RESISTANCE:NPLC 10, (@101,102,103,105,110,111,112)"
@@ -49,34 +52,38 @@ class rtd_calibrator(object):
 
         return True
 
+
     def start_measurement(self, comment):
-        data_set_saver = DataSetSaver("measurements_dummy", "xy_values_dummy", "dummy", "dummy")
+        """ Start a measurement """
+        data_set_saver = DataSetSaver("measurements_dummy", "xy_values_dummy",
+                                      "dummy", "dummy")
         data_set_saver.start()
         data_set_time = time.time()
 
-        metadata = {"Time": CustomColumn(data_set_time, "FROM_UNIXTIME(%s)"), "comment": comment,
-                    "type": 65, "preamp_range": 0, "sem_voltage": 0}
+        metadata = {"Time": CustomColumn(data_set_time, "FROM_UNIXTIME(%s)"),
+                    "comment": comment, "type": 65}
 
-        labels = ["2W Heater 1", "2W Heater 2", "2W RTD Source", "2W RTD Sense", "Short circuit check",
-                      "RTD Source Sense Left", "RTD Source Sense Right", "4W RTD", "Temperature"]
+        labels = ["2W Heater 1", "2W Heater 2", "2W RTD Source", "2W RTD Sense",
+                  "Short circuit check", "RTD Source Sense Left",
+                  "RTD Source Sense Right", "4W RTD", "Temperature"]
         
         for label in labels:
             metadata["mass_label"] = label
             data_set_saver.add_measurement(label, metadata)
 
-        for i in range(0, 2):
+        for _ in range(0, 2):
             self.update_values()
             for label in labels:
                 print('Channel: ' + label + ': ' + str(self.data[label]))
-                data_set_saver.save_point(label, (time.time() - data_set_time, self.data[label]))
+                data_set_saver.save_point(label, (time.time() - data_set_time,
+                                                  self.data[label]))
             time.sleep(1)
 
 
     
 if __name__ == '__main__':
-    CALIBRATOR = rtd_calibrator('USB0::0x0957::0x2007::MY57000209::INSTR')
+    CALIBRATOR = RtdCalibrator('USB0::0x0957::0x2007::MY57000209::INSTR')
     CALIBRATOR.update_values()
     print(CALIBRATOR.data)
 
     CALIBRATOR.start_measurement(comment='R/T test')
-
