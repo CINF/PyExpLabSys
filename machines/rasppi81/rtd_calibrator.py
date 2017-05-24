46355187be9b215235b1708ef4352b056f453b00
Robert Jensen <jensen.robert@gmail.com>
2017-05-24 08:16:07 +0200

    RTD Calibrator now fully functional (although primitive...)

--- machines/rasppi81/rtd_calibrator.py
+++ machines/rasppi81/rtd_calibrator.py
@@ -2,64 +2,81 @@ import time
 import PyExpLabSys.drivers.agilent_34972A as agilent_34972A
 from PyExpLabSys.common.database_saver import DataSetSaver, CustomColumn
 
-mux = agilent_34972A.Agilent34972ADriver(interface='usbtmc',
-                                         connection_string='USB0::0x0957::0x2007::MY57000209::INSTR')
+#mux = agilent_34972A.Agilent34972ADriver(interface='usbtmc',
+#                                         connection_string='USB0::0x0957::0x2007::MY57000209::INSTR')
+
+class rtd_calibrator(object):
+
+    def __init__(self, connection_string):
+        self.mux = agilent_34972A.Agilent34972ADriver(interface='usbtmc',
+                                                      connection_string=connection_string)
+        self.data = {}
+        self.data['2W Heater 1'] = None
+        self.data['2W Heater 2'] = None
+        self.data['2W RTD Source'] = None
+        self.data['2W RTD Sense'] = None
+        self.data['4W RTD'] = None
+        self.data['Short circuit check'] = None
+        self.data['RTD Source Sense Left'] = None
+        self.data['RTD Source Sense Right'] = None
+        self.data['Temperature'] = None
+
+    def update_values(self):
+        comm_string = "CONFIGURE:RESISTANCE (@101,102,103,105,110,111,112)"
+        self.mux.scpi_comm(comm_string)
+        comm_string = "SENSE:RESISTANCE:NPLC 10, (@101,102,103,105,110,111,112)"
+        self.mux.scpi_comm(comm_string)
+        values = self.mux.read_single_scan()
+        self.data['2W Heater 1'] = values[0]
+        self.data['2W Heater 2'] = values[2]
+        self.data['2W RTD Source'] = values[1]
+        self.data['2W RTD Sense'] = values[6]
+        self.data['Short circuit check'] = values[3]
+        self.data['RTD Source Sense Left'] = values[4]
+        self.data['RTD Source Sense Right'] = values[5]
+
+        comm_string = "CONFIGURE:FRESISTANCE (@102)"
+        self.mux.scpi_comm(comm_string)
+        comm_string = "SENSE:FRESISTANCE:NPLC 100, (@102)"
+        self.mux.scpi_comm(comm_string)
+        values = self.mux.read_single_scan()
+        self.data['4W RTD'] = values[0]
+
+        comm_string = "CONFIGURE:TEMPERATURE (@104)"
+        self.mux.scpi_comm(comm_string)
+        values = self.mux.read_single_scan()
+        self.data['Temperature'] = values[0]
+
+        return True
+
+    def start_measurement(self, comment):
+        data_set_saver = DataSetSaver("measurements_dummy", "xy_values_dummy", "dummy", "dummy")
+        data_set_saver.start()
+        data_set_time = time.time()
+
+        metadata = {"Time": CustomColumn(data_set_time, "FROM_UNIXTIME(%s)"), "comment": comment,
+                    "type": 65, "preamp_range": 0, "sem_voltage": 0}
+
+        labels = ["2W Heater 1", "2W Heater 2", "2W RTD Source", "2W RTD Sense", "Short circuit check",
+                      "RTD Source Sense Left", "RTD Source Sense Right", "4W RTD", "Temperature"]
+        
+        for label in labels:
+            metadata["mass_label"] = label
+            data_set_saver.add_measurement(label, metadata)
+
+        for i in range(0, 2):
+            self.update_values()
+            for label in labels:
+                print('Channel: ' + label + ': ' + str(self.data[label]))
+                data_set_saver.save_point(label, (time.time() - data_set_time, self.data[label]))
+            time.sleep(1)
 
 
+    
+if __name__ == '__main__':
+    CALIBRATOR = rtd_calibrator('USB0::0x0957::0x2007::MY57000209::INSTR')
+    CALIBRATOR.update_values()
+    print(CALIBRATOR.data)
 
-#print(mux.read_configuration())
+    CALIBRATOR.start_measurement(comment='R/T test')
 
-#mux.set_scan_list(['101,102,103,104,105,106,107,108'])
-
-#comm_string = "CONFIGURE:VOLT (@101,102,103,104,105,106,107,108)"
-comm_string = "CONFIGURE:RESISTANCE (@101,102,103,105,110,111,112)"
-mux.scpi_comm(comm_string)
-wire_2 = mux.read_single_scan()
-
-comm_string = "CONFIGURE:FRESISTANCE (@102)"
-mux.scpi_comm(comm_string)
-wire_4 = mux.read_single_scan()
-
-comm_string = "CONFIGURE:TEMPERATURE (@104)"
-mux.scpi_comm(comm_string)
-temperature = mux.read_single_scan()
-
-print('Heater 1, 2W: ' + str(wire_2[0]))
-print('Heater 2, 2W: ' + str(wire_2[2]))
-print('RTD Probe, 2W: ' + str(wire_2[1]))
-print('RTD Sense, 2W: ' + str(wire_2[6]))
-print('RTD, 4W: ' + str(wire_4[0]))
-print('Short circuit-check: ' + str(wire_2[3]))
-print('RTD Probe-sense left, 2W: ' + str(wire_2[4]))
-print('RTD Probe-sense left, 2W: ' + str(wire_2[5]))
-print('Temperature: ' + str(temperature[0]))
-
-
-
-time.sleep(0.2)
-
-
-"""
-comment = 'IV-test'
-data_set_saver = DataSetSaver("measurements_dummy", "xy_values_dummy", "dummy", "dummy")
-data_set_saver.start()
-data_set_time = time.time()
-metadata = {"Time": CustomColumn(data_set_time, "FROM_UNIXTIME(%s)"), "comment": comment,
-            "type": 1, "preamp_range": 0, "sem_voltage": 0}
-for label in ["Ch1", "Ch2", "Ch3", "Ch4", "Ch5", "Ch6", "Ch7", "Ch8",]:
-    metadata["mass_label"] = label
-    data_set_saver.add_measurement(label, metadata)
-
-for channel in range(1, 9):
-    print('Channel: ' + str(channel))
-    mux.scpi_comm('ROUTE:CLOSE (@10' + str(channel) + ')' )
-    time.sleep(0.2)
-    V, I = smu.iv_scan(-0.2, 0.2, 0.001)    
-    for i in range(0, len(I)):
-        data_set_saver.save_point('Ch' + str(channel), (V[i], I[i]))
-    mux.scpi_comm('ROUTE:OPEN (@10' + str(channel) + ')' )
-    time.sleep(0.2)
-
-time.sleep(1)
-mux.scpi_comm('ROUTE:CLOSE (@101)')
-"""
