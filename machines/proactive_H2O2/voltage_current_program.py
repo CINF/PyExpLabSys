3bf9d24f1f4d206bc3c31aa5d670f5cb7ff540ed
Kenneth Nielsen <k.nielsen81@gmail.com>
2017-07-05 16:17:54 +0200

    Added license information to proactive programs

--- machines/proactive_H2O2/voltage_current_program.py
+++ machines/proactive_H2O2/voltage_current_program.py
@@ -1,6 +1,26 @@
 # pylint: disable=global-statement
 
-"""My example stepped program"""
+"""The Voltage Current Ramp Program
+
+Copyright (C) 2012 Kenneth Nielsen and Robert Jensen
+
+The Voltage Current Ramp Program is free software: you can
+redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software
+Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+The Voltage Current Ramp Program is distributed in the hope
+that it will be useful, but WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License for more
+details.
+
+You should have received a copy of the GNU General Public License
+along with The CINF Data Presentation Website.  If not, see
+<http://www.gnu.org/licenses/>.
+
+"""
 
 from __future__ import print_function, unicode_literals, division
 
