514330107db7c245d6e8420041362a647e9f016a
Robert Jensen <jensen.robert@gmail.com>
2017-12-14 06:10:55 +0000

    Cleanup of driver

--- PyExpLabSys/drivers/stmicroelectronics_l3g4200d.py
+++ PyExpLabSys/drivers/stmicroelectronics_l3g4200d.py
@@ -1,11 +1,11 @@
 """ Driver for STMicroelectronics L3G4200D 3 axis gyroscope """
+import time
 import os
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if on_rtd:
     pass
 else:
     import smbus
-import time
 from PyExpLabSys.common.supported_versions import python2_and_3
 python2_and_3(__file__)
 
@@ -17,7 +17,7 @@ class L3G4200D(object):
         self.device_address = 0x68
         # Set output data rate to 200, bandwidth cut-off to 12.5Hz
         self.bus.write_byte_data(self.device_address, 0x20, 0x4F)
-        # Set full scale range, 0x00: 250dps, 0x30: 2000dps, block updates until lsb and msb are read
+        # Set full scale range, 0x00: 250dps, 0x30: 2000dps, block until lsb and msb are read
         self.full_scale = 250 # This should go in a self-consistent table...
         self.bus.write_byte_data(self.device_address, 0x23, 0x80)
         time.sleep(0.5)
@@ -29,34 +29,28 @@ class L3G4200D(object):
 
     def read_values(self):
         """ Read a value from the sensor """
-        #data = self.bus.read_i2c_block_data(self.device_address, 0x28, 2)
-        #byte1 = data[0]
-        #byte2 = data[1]
-        #print(byte1, byte2)
         byte1 = self.bus.read_byte_data(self.device_address, 0x28)
         byte2 = self.bus.read_byte_data(self.device_address, 0x29)
-        #print(byte1, byte2)
-        #print('---')
+
         x_value = byte2 * 256 + byte1
-        if x_value > 32767:
-            x_value = x_value - 65536
-        x_value = 1.0 * x_value * self.full_scale / 65536
+        if x_value > (2**15)-1:
+            x_value = x_value - 2**16
+        x_value = 1.0 * x_value * self.full_scale / (2**15)
 
         byte1 = self.bus.read_byte_data(self.device_address, 0x2A)
         byte2 = self.bus.read_byte_data(self.device_address, 0x2B)
         y_value = byte2 * 256 + byte1
-        if y_value > 32767:
-            y_value = y_value - 65536
-        y_value = 1.0 * y_value * self.full_scale / 65536
+        if y_value > (2**15)-1:
+            y_value = y_value - 2**16
+        y_value = 1.0 * y_value * self.full_scale / (2**15)
 
         byte1 = self.bus.read_byte_data(self.device_address, 0x2C)
         byte2 = self.bus.read_byte_data(self.device_address, 0x2D)
         z_value = byte2 * 256 + byte1
-        if z_value > 32767:
-            z_value = z_value - 65536
-        z_value = 1.0 * z_value * self.full_scale / 65536
+        if z_value > (2**15)-1:
+            z_value = z_value - 2**16
+        z_value = 1.0 * z_value * self.full_scale / (2**15)
 
-        
         return(x_value, y_value, z_value)
 
 if __name__ == '__main__':
