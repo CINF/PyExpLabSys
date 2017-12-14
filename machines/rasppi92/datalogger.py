f10e165d2594b3bf3f304384c1ca97130c161d14
Robert Jensen <jensen.robert@gmail.com>
2017-12-14 06:11:22 +0000

    Added accelerometer to the logging

--- machines/rasppi92/datalogger.py
+++ machines/rasppi92/datalogger.py
@@ -10,15 +10,16 @@ from PyExpLabSys.common.sockets import LiveSocket
 from PyExpLabSys.common.sockets import DateDataPullSocket
 from PyExpLabSys.common.value_logger import ValueLogger
 import PyExpLabSys.drivers.stmicroelectronics_l3g4200d as l3g4200d
+import PyExpLabSys.drivers.stmicroelectronics_ais328dq as ais328dq
 from PyExpLabSys.common.supported_versions import python2_and_3
 import credentials
 python2_and_3(__file__)
 
 class Reader(threading.Thread):
     """ Pressure reader """
-    def __init__(self, l3g4200d):
+    def __init__(self, sensor):
         threading.Thread.__init__(self)
-        self.l3g4200d = l3g4200d
+        self.sensor = sensor
         self.rms_vibration = None
         self.quit = False
         self.ttl = 20
@@ -42,7 +43,7 @@ class Reader(threading.Thread):
             values = np.zeros(avg_length)
             t_start = time.time()
             for i in range(0, avg_length):
-                raw = self.l3g4200d.read_values()
+                raw = self.sensor.read_values()
                 values[i] = math.sqrt(raw[0]**2 + raw[1]**2 + raw[2]**2)
             measurement_time = time.time() - t_start
             print(measurement_time)
@@ -56,17 +57,24 @@ def main():
     logging.basicConfig(level=logging.ERROR)
 
     l3g4200d_instance = l3g4200d.L3G4200D()
-    reader = Reader(l3g4200d_instance)
-    reader.start()
+    l3g4200d_reader = Reader(l3g4200d_instance)
+    l3g4200d_reader.start()
+
+    ais328dq_instance = ais328dq.AIS328DQTR()
+    ais328dq_reader = Reader(ais328dq_instance) 
+    ais328dq_reader.start()
 
     time.sleep(2.5)
 
-    codenames = ['b307_049_vibration_measurement']
+    codenames = ['b307_049_vibration_measurement', 'b307_049_vibration_measurement_accl']
 
     loggers = {}
-    loggers[codenames[0]] = ValueLogger(reader, comp_val=0.01, maximumtime=600,
+    loggers[codenames[0]] = ValueLogger(l3g4200d_reader, comp_val=0.02, maximumtime=600,
                                         comp_type='lin', channel=0)
     loggers[codenames[0]].start()
+    loggers[codenames[1]] = ValueLogger(ais328dq_reader, comp_val=0.01, maximumtime=100,
+                                        comp_type='lin', channel=0)
+    loggers[codenames[1]].start()
 
     livesocket = LiveSocket('307 Vibraton logger', codenames)
     livesocket.start()
@@ -81,7 +89,7 @@ def main():
                                     measurement_codenames=codenames)
     db_logger.start()
 
-    while reader.isAlive():
+    while l3g4200d_reader.isAlive():
         time.sleep(10)
         for name in codenames:
             value = loggers[name].read_value()
