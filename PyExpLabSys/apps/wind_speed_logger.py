087e7c47e1a8fb9d9a94c402624b998e711307f0
Robert Jensen <jensen.robert@gmail.com>
2017-12-15 13:29:05 +0100

    First draft of windspeed logger

--- PyExpLabSys/apps/wind_speed_logger.py
+++ PyExpLabSys/apps/wind_speed_logger.py
@@ -2,83 +2,86 @@
 from __future__ import print_function
 import threading
 import time
+import sys
 from PyExpLabSys.common.value_logger import ValueLogger
 from PyExpLabSys.common.database_saver import ContinuousDataSaver
 from PyExpLabSys.common.sockets import DateDataPullSocket
 from PyExpLabSys.common.sockets import LiveSocket
 from PyExpLabSys.common.supported_versions import python2_and_3
-from ABE_DeltaSigmaPi import DeltaSigma
+from ABE_ADCPi import ADCPi
 from ABE_helpers import ABEHelpers
-import credentials
 python2_and_3(__file__)
+try:
+    sys.path.append('/home/pi/PyExpLabSys/machines/' + sys.argv[1])
+except IndexError:
+    print('You need to give the name of the raspberry pi as an argument')
+    print('This will ensure that the correct settings file will be used')
+    exit()
+import credentials # pylint: disable=F0401
+import settings # pylint: disable=F0401
 
 class WindReader(threading.Thread):
     """ Read Cooling water pressure """
     def __init__(self, adc):
         threading.Thread.__init__(self)
         self.adc = adc
-        self.average_length = 10
-        self.out_voltage = None
-        self.raw_voltage = None
+        self.average_length = 20
+        self.windspeeds = {}
+        for channel in settings.channels.keys():
+            self.windspeeds[channel] = -1
         self.quit = False
 
     def value(self, channel):
         """ Return the value of the reader """
-        if channel == 0:
-            value = self.out_voltage
-        if channel == 1:
-            value = self.raw_voltage
+        value = self.windspeeds[channel]
         return value
 
     def run(self):
         time.sleep(0.1)
         while not self.quit:
-            out_voltage = 0
-            raw_voltage = 0
-            for _ in range(0, self.average_length):
-                out_voltage += self.adc.read_voltage(5)
-                raw_voltage += self.adc.read_voltage(2)
-            self.out_voltage = out_voltage / self.average_length
-            self.raw_voltage = raw_voltage / self.average_length
-
+            for channel in settings.channels.keys():
+                voltage = 0
+                for _ in range(0, self.average_length):
+                    voltage += self.adc.read_voltage(channel)
+                self.windspeeds[channel] = voltage / self.average_length
+                print(str(channel) + ': ' + str(self.windspeeds[channel]))
 
 def main():
     """ Main function """
     i2c_helper = ABEHelpers()
     bus = i2c_helper.get_smbus()
-    adc_instance = DeltaSigma(bus, 0x68, 0x69, 18)
+    adc_instance = ADCPi(bus, 0x68, 0x69, 18)
 
     windreader = WindReader(adc_instance)
     windreader.daemon = True
     windreader.start()
 
-    codenames = ['b312_fumehood_01_windspeed_out', 'b312_fumehood_01_windspeed_raw']
     loggers = {}
-    for i in range(0, 2):
-        loggers[codenames[i]] = ValueLogger(windreader, comp_val=0.01, channel=i)
-        loggers[codenames[i]].start()
-    socket = DateDataPullSocket('Fumehood Wind Speed', codenames, timeouts=2.0)
+    for channel, codename in settings.channels.items():
+        loggers[codename] = ValueLogger(windreader, comp_val=0.001, channel=channel)
+        loggers[codename].start()
+    socket = DateDataPullSocket('Fumehood Wind Speed', list(settings.channels.values()), timeouts=2.0)
     socket.start()
 
-    live_socket = LiveSocket('Fumehood Wind Speed', codenames)
+    live_socket = LiveSocket('Fumehood Wind Speed',  list(settings.channels.values()))
     live_socket.start()
 
     db_logger = ContinuousDataSaver(continuous_data_table='dateplots_dummy',
                                     username=credentials.user,
                                     password=credentials.passwd,
-                                    measurement_codenames=codenames)
+                                    measurement_codenames=list(settings.channels.values()))
     db_logger.start()
 
-    time.sleep(5)
+    time.sleep(10)
 
     while windreader.is_alive():
         time.sleep(0.25)
-        for name in codenames:
+        for name in settings.channels.values():
             value = loggers[name].read_value()
             socket.set_point_now(name, value)
             live_socket.set_point_now(name, value)
             if loggers[name].read_trigged():
-                print(value)
+                print(name + ': ' + str(value))
                 db_logger.save_point_now(name, value)
                 loggers[name].clear_trigged()
 
