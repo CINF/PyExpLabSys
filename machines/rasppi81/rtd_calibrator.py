7f50e5d21af28091171398b4ced2ffef83683ab1
Robert Jensen <jensen.robert@gmail.com>
2018-01-08 14:17:40 +0100

    Now supports SMU as test probe

--- machines/rasppi81/rtd_calibrator.py
+++ machines/rasppi81/rtd_calibrator.py
@@ -2,6 +2,7 @@
 from __future__ import print_function
 import time
 import socket
+import PyExpLabSys.drivers.keithley_smu as keithley_smu
 import PyExpLabSys.drivers.agilent_34972A as agilent_34972A
 from PyExpLabSys.common.database_saver import DataSetSaver, CustomColumn
 from PyExpLabSys.common.supported_versions import python2_and_3
@@ -10,11 +11,13 @@ python2_and_3(__file__)
 
 class RtdCalibrator(object):
     """ Simple class to log the resistance of a micro-reactor """
-    def __init__(self, connection_string, temperature_host=None, only_4w=False):
+    def __init__(self, connection_string, temperature_host=None, only_4w=False, use_smu=True):
         self.mux = agilent_34972A.Agilent34972ADriver(interface='usbtmc',
                                                       connection_string=connection_string)
+        self.smu = keithley_smu.KeithleySMU(interface='serial', device='/dev/ttyUSB0', baudrate=4800)
         self.temperature_host = temperature_host
         self.only_4w = only_4w
+        self.use_smu = use_smu
         self.data = {}
         self.data['2W Heater 1'] = None
         self.data['2W Heater 2'] = None
@@ -58,9 +61,23 @@ class RtdCalibrator(object):
             self.data['Short circuit check'] = 0
             self.data['RTD Source Sense Left'] = 0
             self.data['RTD Source Sense Right'] = 0
+            self.data['4W RTD'] = 0
 
-        values = self.mux.read_single_scan()
-        self.data['4W RTD'] = values[0]
+        if self.use_smu is False:
+            values = self.mux.read_single_scan()
+            self.data['4W RTD'] = values[0]
+        else:
+            #self.smu.set_integration_time(100, 1)
+            #self.smu.set_integration_time(100, 2)
+            t = time.time()
+            source_current = self.smu.read_current(1)
+            source_voltage = self.smu.read_voltage(1)
+            probe_voltage = self.smu.read_voltage(2)
+            print(time.time() - t)
+            print(source_current, source_voltage, probe_voltage)
+            self.data['2W RTD Source'] = source_voltage / source_current
+            self.data['4W RTD'] = probe_voltage / source_current
+
 
         if self.temperature_host is None:
             comm_string = "CONFIGURE:TEMPERATURE (@104)"
@@ -92,7 +109,8 @@ class RtdCalibrator(object):
         data_set_time = time.time()
 
         metadata = {"Time": CustomColumn(data_set_time, "FROM_UNIXTIME(%s)"),
-                    "comment": comment, "type": 65}
+                    "comment": comment, "type": 65,
+                    "preamp_range": 9, "sem_voltage": -1}
 
         labels = ["2W Heater 1", "2W Heater 2", "2W RTD Source", "2W RTD Sense",
                   "Short circuit check", "RTD Source Sense Left",
@@ -114,10 +132,11 @@ class RtdCalibrator(object):
     
 if __name__ == '__main__':
     #CALIBRATOR = RtdCalibrator('USB0::0x0957::0x2007::MY57000209::INSTR', temperature_host='rasppi88')
-    CALIBRATOR = RtdCalibrator('USB0::0x0957::0x2007::MY57000209::INSTR', only_4w=True, temperature_host='rasppi88')
+    CALIBRATOR = RtdCalibrator('USB0::0x0957::0x2007::MY57000209::INSTR', only_4w=True, use_smu=True,
+                               temperature_host='rasppi88')
     for i in range(0, 1):
         CALIBRATOR.update_values()
         print(CALIBRATOR.data)
         
 
-    CALIBRATOR.start_measurement(comment='2.1')
+    CALIBRATOR.start_measurement(comment='3.2 - 5.0mA')
