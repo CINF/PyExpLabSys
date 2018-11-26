5748556c49c61c3db1b060b4be6af23dadf6d9d8
Jakob Ejler Sørensen <lillebror.jakobes@gmail.com>
2018-11-26 16:57:48 +0000

    Add MaxSizeQueue to prevent hanging if a queue is unused

--- PyExpLabSys/drivers/deltaco_TB_298.py
+++ PyExpLabSys/drivers/deltaco_TB_298.py
@@ -6,7 +6,7 @@ import evdev
 import select
 from time import sleep, time
 from threading import Thread
-from queue import Queue, Empty
+from queue import Queue, Empty, Full
 
 from PyExpLabSys.common.supported_versions import python3_only
 python3_only(__file__)
@@ -53,13 +53,28 @@ def detect_keypad_device():
 STOP = object()
 
 
+class MaxSizeQueue(Queue):
+    """MaxSizeQueue:
+    A subclassed queue.Queue object that pops the first item if raising a
+    queue.Full exception when adding an item. Otherwise the queue will block
+    until an item is removed from the queue.
+    """
+    
+    def put(self, item):
+        while True:
+            try:
+                super().put(item, block=False)
+                break
+            except Full:
+                super().get()
+
 class ThreadedKeypad(Thread):
     """Threaded keypad reader
 
     Attributes:
-        line_queue (queue.Queue): Queue of enter separated lines entered
-        event_queue (queue.Queue): Queue of key pad events
-        key_pressed (queue.Queue): Queue of pressed keys (only one entry will be added if
+        line_queue (MaxSizeQueue): Queue of enter separated lines entered
+        event_queue (MaxSizeQueue): Queue of key pad events
+        key_pressed_queue (MaxSizeQueue): Queue of pressed keys (only one entry will be added if
             the key is held down)
         device (evdev.InputDevice): The input device
     """
@@ -67,39 +82,64 @@ class ThreadedKeypad(Thread):
     def __init__(self, device_path, line_chars='0123456789', max_queue_sizes=None):
         """Instantiate local variables
 
-        Holds 3 queues
+        Holds 3 non-blocking queues that record inputs. If a queue is full, the first item
+        is popped and the newest item is put in the end of the queue.
+        When starting a function in a program that makes use of another queue, make sure
+        to flush the queue before the main loop so you get the newest value first.
+        Alternatively, if you want to keep an unused queue for history purposes, you may
+        want to increase the max_queue_size of that queue.
 
         Args:
             device_path (str): Path of the inpu device to use e.g: '/dev/input/event0'
             line_chars (str): String of accepted characters in a line. Default value is
                 '0123456789'
-            max_queue_sizes: Dict used to overwrite the default max queue size of 1000
+            max_queue_sizes: Dict used to overwrite the default max queue size of 1024
                 elements. The keys are 'line', 'event' and 'key_pressed' and the values
-                are ints.
+                are positive ints (0 is infinite).
         """
         super().__init__()
         self._continue_reading = True
         self.device = evdev.InputDevice(device_path)
         self._gathered_line = ''
         self.line_chars = line_chars
-
+        self.led_on = False
+        for (name, value) in self.device.leds(verbose=True):
+            if name == 'LED_NUML':
+                self.led_on = True
+                
         # Create queues
         _max_queue_sizes = {'line': 1024, 'event': 1024, 'key_pressed': 1024}
         if max_queue_sizes:
             _max_queue_sizes.update(max_queue_sizes)
-        self.line_queue = Queue(maxsize=_max_queue_sizes['line'])
-        self.event_queue = Queue(maxsize=_max_queue_sizes['event'])
-        self.key_pressed_queue = Queue(maxsize=_max_queue_sizes['key_pressed'])
+        self.line_queue = MaxSizeQueue(maxsize=_max_queue_sizes['line'])
+        self.event_queue = MaxSizeQueue(maxsize=_max_queue_sizes['event'])
+        self.key_pressed_queue = MaxSizeQueue(maxsize=_max_queue_sizes['key_pressed'])
 
     def run(self):
         """Main run method"""
         while self._continue_reading:
             # Check if there is anything to read on the device
-            read_list, _, _ = select.select([self.device.fd], [], [], 0.1)
-            if read_list:
-                for event in self.device.read():
-                    if event.type == evdev.ecodes.EV_KEY:
-                        self.handle_event(event)
+            try:
+                read_list, _, _ = select.select([self.device.fd], [], [], 0.5) # previous timeout 0.2
+                if read_list:
+                    for event in self.device.read():
+                        if event.type == evdev.ecodes.EV_KEY:
+                            self.handle_event(event)
+                #sleep(0.01)
+                # Read LED Num Lock state
+                for (name, value) in self.device.leds(verbose=True):
+                    # Set True if LED is on
+                    if name == 'LED_NUML':
+                        self.led_on = True
+                        break
+                else:
+                    self.led_on = False
+            except OSError as e:
+                print('\n"OSError" encountered\n{}'.format(e))
+                self.stop()
+            except Exception as e:
+                print('\nOther "Exception" encountered\n{}'.format(e))
+                self.stop()
 
     def handle_event(self, event):
         """Handle an event"""
@@ -125,8 +165,8 @@ class ThreadedKeypad(Thread):
     def stop(self):
         """Stop the thread and put deltaco_TP_298.STOP in queues"""
         self._continue_reading = False
-        while self.is_alive():
-            sleep(0.1)
+        #while self.is_alive(): # Commented as thread cannot be closed from
+        #    sleep(0.1)         # within otherwise
         self.device.close()
         self.line_queue.put(STOP)
         self.event_queue.put(STOP)
