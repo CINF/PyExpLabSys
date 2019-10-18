4d0003c75c18bb61f7e5637747575dae24611f8b
Jakob Ejler Sørensen <lillebror.jakobes@gmail.com>
2019-10-18 13:08:02 +0200

    Add logging output to data_logger

--- machines/rasppi83/data_logger.py
+++ machines/rasppi83/data_logger.py
@@ -12,12 +12,17 @@ from PyExpLabSys.common.supported_versions import python2_and_3
 import credentials
 python2_and_3(__file__)
 
+FORMAT = '%(asctime)s -- %(name)s:%(message)s'
+logging.basicConfig(filename="temperature.log", format=FORMAT, level=logging.WARNING)
+LOGGER = logging.getLogger('data_logger')
+
 class TcReader(threading.Thread):
     """ Communicates with the Omega ?? """
     
-    def __init__(self, port, datasocket, crit_logger=None, db_logger=None, codename='', output=False):
+    def __init__(self, port, datasocket, crit_logger=None, db_logger=None, codename='', output=False, logger=LOGGER):
         super().__init__()
-        print('Initializing connection')
+        self.logger = logger
+        self.logger.info('Initializing connection')
         self.comm = minimalmodbus.Instrument(port, 1)
         self.comm.serial.baudrate = 9600
         self.comm.serial.parity = serial.PARITY_EVEN
@@ -31,16 +36,16 @@ class TcReader(threading.Thread):
                 error = error + 1
         if error > 9:
             exit('Error in communication with TC reader')
-        print('Connection established')
+        self.logger.info('Connection to Omega established')
         #if self.temperature < -1000:
         #    self.temperature = None
         #threading.Thread.__init__(self)
         self.quit = False
         self.datasocket = datasocket
-        self.logger = crit_logger
+        self.crlogger = crit_logger
         self.db_logger = db_logger
         self.codename = codename
-        print('Module ready')
+        self.logger.info('TcReader ready')
         self.output = output
 
     def value(self):
@@ -56,15 +61,15 @@ class TcReader(threading.Thread):
             self.db_logger.stop()
         
     def run(self):
-        time.sleep(5)
+        time.sleep(1)
         error = 0
         lasttime = time.time()
         t0 = time.time()
         try:
-            print('Entering while loop')
+            self.logger.info('Entering while loop')
             while self.isAlive() and not self.quit:
                 # Delay loop communication
-                time.sleep(0.15)
+                time.sleep(0.05)
                 try:
                     self.temperature = self.comm.read_register(4096, 1, signed=True)
                     if self.temperature < -250:
@@ -73,22 +78,23 @@ class TcReader(threading.Thread):
                         error = 0
                     # Save points to sockets
                     self.datasocket.set_point_now(self.codename, self.temperature)
-                    if self.logger is not None and self.db_logger is not None:
-                        if self.logger.check(self.codename, self.temperature):
+                    if self.crlogger is not None and self.db_logger is not None:
+                        if self.crlogger.check(self.codename, self.temperature):
                             self.db_logger.save_point_now(self.codename, self.temperature)
-                            print(self.codename + ': ' + str(self.temperature))
+                            self.logger.debug(self.codename + ': ' + str(self.temperature))
                     if self.output:
                         print(self.temperature, time.time()-t0, time.time()-lasttime)
                         lasttime = time.time()
-                except:
+                except Exception as e:
                     error += 1
-                    print('Error value: {}'.format(error))
+                    self.logger.warning('Error value: {}'.format(error), exc_info=True)
                     if error > 9:
                         self.stop()
                         raise
         except KeyboardInterrupt:
-            print('Force quit activated')
+            self.logger.debug('Force quit activated (CTRL+C)')
             self.stop()
+        self.logger.debug('Logger stopped by exiting run function properly')
 
 CODENAMES = {'Sample': 'omicron_tpd_sample_temperature',
              'Base': 'omicron_tpd_temperature',
@@ -96,9 +102,6 @@ CODENAMES = {'Sample': 'omicron_tpd_sample_temperature',
                 
 def main():
     """ Main function """
-    logging.basicConfig(filename="logger.txt", level=logging.ERROR)
-    logging.basicConfig(level=logging.ERROR)
-
     ports = dict()
     ports['Sample'] = '/dev/serial/by-id/usb-FTDI_USB-RS485_Cable_FTY5BU0H-if00-port0'
     ports['Base'] =   '/dev/serial/by-id/usb-FTDI_USB-RS485_Cable_FTY3GX3T-if00-port0'
@@ -107,8 +110,8 @@ def main():
     logger = LoggingCriteriumChecker(
         codenames=[CODENAMES['Base']],
         types=['lin'],
-        criteria=[0.33],
-        time_outs=[300],
+        criteria=[0.73],
+        time_outs=[600],
         low_compare_values=[-200],
         )
 
@@ -122,29 +125,31 @@ def main():
                                     measurement_codenames=[CODENAMES['Base']])
     db_logger.start()
 
-
-    measurement = TcReader(ports['Base'], datasocket, crit_logger=logger, db_logger=db_logger, codename=CODENAMES['Base'])
+    logger1 = LOGGER.getChild('Base_reader')
+    measurement = TcReader(ports['Base'], datasocket, crit_logger=logger, db_logger=db_logger, codename=CODENAMES['Base'], logger=logger1)
     measurement.start()
 
-    sample_logger = TcReader(ports['Sample'], datasocket, codename=CODENAMES['Sample'])
+    logger2 = LOGGER.getChild('Sample_reader')
+    sample_logger = TcReader(ports['Sample'], datasocket, codename=CODENAMES['Sample'], logger=logger2)
     sample_logger.start()
 
-    time.sleep(5)
-    string = 'Base: {: <0 6.4} C, Sample: {: <0 6.4f} C'
+    time.sleep(2)
+    string = 'Base: {: <6.4} C, Sample: {: <6.4} C'
     while True:
         try:
             time.sleep(1)
             print(string.format(measurement.temperature, sample_logger.temperature))
         except ValueError:
-            print(repr(measurement.temperature))
+            print('ValueError')
+            print(repr(measurement.temperature), type(measurement.temperature))
             print(repr(sample_logger.temperature))
         except KeyboardInterrupt:
             measurement.stop()
             sample_logger.stop()
             time.sleep(2)
-            print('\nTcReaders stopped')
+            LOGGER.info('\nTcReaders stopped')
             datasocket.stop()
-            print('Pullsocket stopped\nExiting.')
+            LOGGER.info('Pullsocket stopped\nExiting.')
             break
             
 if __name__ == '__main__':
