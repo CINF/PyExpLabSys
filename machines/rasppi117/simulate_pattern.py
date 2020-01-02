31da76318741bab78102f9d162689e617640facd
Jakob Ejler Sørensen <lillebror.jakobes@gmail.com>
2020-01-02 16:21:59 +0100

    Bug fixes and upgraded output (error, meaningful intensities)

--- machines/rasppi117/simulate_pattern.py
+++ machines/rasppi117/simulate_pattern.py
@@ -103,6 +103,18 @@ def normalize_beam_profile(num=100, moodkwargs=dict()):
     bp_ax.plot_wireframe(X, Y, Z/area*10)
     return area, bp_ax
 
+# Print out Z profile in ascii format
+def print_ascii(profile, output_name='default_ascii_output'):
+    if output_name.lower()[-4:] != '.txt':
+        output_name += '.txt'
+    f = open(output_name, 'w')
+    for i in range(len(profile)):
+        string = ''
+        for j in range(len(profile[i])):
+            string += str(profile[i, j]) + '\t'
+        string = string[:-1] + '\n'
+        f.write(string)
+    f.close()
 
 if __name__ == '__main__':
 
@@ -114,10 +126,27 @@ if __name__ == '__main__':
     from matplotlib import cm
     from matplotlib.patches import Circle
 
-    # Radius of aperture
-    radius_ap = 2.25 # defining spot size
-    # Radius of mask
-    r_mask = 2.5 # desired deposition area
+    ###########################################################################
+    ### User constants ###
+    # Radius of aperture - defining spot size (usually always 2.25
+    radius_ap = 2.25
+    # Radius of mask - desired deposition area / sample area
+    r_mask = 2.5
+    # Center of mask - change to "offset/misalign" sample (mm)
+    center_mask = (0, 0)
+
+    # Type of sample outline - comment/uncomment
+    mask_type = 'circle'
+    #mask_type = 'square' # in this case, some output may still be hardcoded for "circle"
+
+    NP_diameter = 5 # (nm) nanoparticle size
+    charge = 0.14985 # pmol clusters (output from CalculateCoverage plugin)
+    # 0.067 pmol corresponds to 5% 5 nm NPs on a 5mm circular area (non-rastered)
+
+    # output options
+    ascii = 0
+    plot = 1
+    ###########################################################################
 
     if len(sys.argv) > 0:
         filename = sys.argv[1]
@@ -161,14 +190,32 @@ if __name__ == '__main__':
 
 
     # Data
-    num = 100
+    num = 200
     X = np.linspace(-1, 1, num)*10 # mm
     Y = np.linspace(-1, 1, num)*10 # mm
+    cell_length = X[1] - X[0]
+    cell_area = cell_length**2
+    print('Cell size = {} mm^2'.format(cell_area))
     #dx, dy = dx*10, dy*10
     X, Y = np.meshgrid(X, Y)
-    center_mask = (0, 0)
 
     Z = np.zeros((len(X), len(Y)))
+
+    if mask_type == 'square':
+        x_mask = np.array([-r_mask,-r_mask,r_mask,r_mask,-r_mask]) + center_mask[0]
+        y_mask = np.array([-r_mask,r_mask,r_mask,-r_mask,-r_mask]) + center_mask[1]
+        z_mask = np.zeros(len(x_mask))
+    elif mask_type == 'circle':
+        theta = np.linspace(0, 2*np.pi, 100)
+        x_mask = r_mask * np.cos(theta) + center_mask[0]
+        y_mask = r_mask * np.sin(theta) + center_mask[1]
+        z_mask = x_mask*0
+    if mask_type == 'square':
+        index_mask = np.logical_and(X + center_mask[0] < r_mask, X + center_mask[0] > -r_mask)
+        index_mask = np.logical_and(index_mask, Y + center_mask[1] < r_mask)
+        index_mask = np.logical_and(index_mask, Y + center_mask[1] > -r_mask)
+    elif mask_type == 'circle':
+        index_mask = (X + center_mask[0])**2 + (Y + center_mask[1])**2 < r_mask**2
 
     # Load offset and data
     speed = data['speed'] # mm/s
@@ -183,33 +230,37 @@ if __name__ == '__main__':
     start_point = (z*data['step_size'], y*data['step_size']) # coordinates for start algorithm
 
     # Insert centers of raster pattern
-    #DEPO = [151.9, 75.96]
-    #TIME = [5.34, 1.843]
-    DEPO = [10.67]
-    TIME = [0.5864]
-    deposition_rate = sum(DEPO)/sum(TIME) # percent coverage per hour (%/h) relative to 4.5 mm aperture
-    spacing_num = 2
     center_list = []
+    hardcoded_delay = 0.1 #s
     (z, y) = start_point
     for (axis, dist) in pattern:
         if axis == 'Z':
             z0 = z
             z += dist*data['step_size']
+            step_num = int(abs(z-z0)/cell_length)
+            if step_num < 2:
+                step_num = 2
             # Include endpoint on purpose (motor stops briefly)
-            centers, step = np.linspace(z0, z, 10*int(abs(z - z0)+1)/spacing_num, retstep=True)
+            centers, step = np.linspace(z0, z, step_num, retstep=True)
+            centers = centers[1:] # don't count start coordinate
             center_list.append((zip(centers, np.ones(len(centers))*y), abs(step/speed)))
-            center_list.append(([(centers[-1], y)], 1.2))
+            center_list.append(([(centers[-1], y)], hardcoded_delay))
         elif axis == 'Y':
             y0 = y
             y += dist*data['step_size']
+            step_num = int(abs(y-y0)/cell_length)
+            if step_num < 2:
+                step_num = 2
             # Include endpoint on purpose (motor stops briefly)
-            centers, step = np.linspace(y0, y, 10*int(abs(y - y0)+1)/spacing_num, retstep=True)
+            centers, step = np.linspace(y0, y, step_num, retstep=True)
+            centers = centers[1:] # don't count start coordinate
             center_list.append((zip(np.ones(len(centers))*z, centers), abs(step/speed)))
-            center_list.append(([(z, centers[-1])], 1.2))
+            center_list.append(([(z, centers[-1])], hardcoded_delay))
         else:
             raise NameError('Unknown axis identifier encountered in pattern: {}'.format(axis))
-    #print(center_list)
-    print('Chosen pattern is valid.\nStarting simulation...')
+    print('Starting simulation...')
+    print('Note a hardcoded delay of {} seconds is added every time a motion'.format(hardcoded_delay))
+    print('changes direction due to delay in communication with the motors.\n')
 
     area, bp_ax = normalize_beam_profile(moodkwargs=moodkwargs)
 
@@ -217,8 +268,10 @@ if __name__ == '__main__':
     total_time = 0
     previous_point = None
     for (coordinates, dt) in center_list:
+        #print(coordinates)
         for c in coordinates:
-            total_time += dt    
+            #print(c, dt)
+            total_time += dt
             # Sketch raster pattern
             circle = Circle(xy=c, radius=radius_ap, facecolor='b', linewidth=0, alpha=0.05)
             sketch.add_patch(circle)
@@ -229,62 +282,68 @@ if __name__ == '__main__':
                     markeredgecolor='k',
                     )
             previous_point = c
-            Z += skewed_gaussian(X - c[0], Y - c[1], **moodkwargs)/area * deposition_rate * dt/3600.
+            Z += skewed_gaussian(X - c[0], Y - c[1], **moodkwargs)#/area * dt/3600.
 
-    print('Time for 1 round: ' + str(total_time) + 's = ' + str(total_time/60.) + 'min')
-    circle_mask = Circle(xy=center_mask, radius=r_mask, edgecolor='k', facecolor='none', linewidth=2)
-    sketch.add_patch(circle_mask)
+    print('Time for 1 round: {:.1f} s = {:.1f} min'.format(total_time, total_time/60))
+    # Sketch deposition area
+    sketch.plot(x_mask, y_mask, 'k-', linewidth=2)
     try:
         plt.axis('equal')
     except NotImplementedError:
         pass
     sketch.axis([-10, 10, -10, 10])
 
-    theta = np.linspace(0, 2*np.pi, 100)
-    x_mask = r_mask * np.cos(theta)
-    y_mask = r_mask * np.sin(theta)
-    z_mask = x_mask*0
-
-    tfrac = sum(TIME)/(total_time/3600)
-    print(tfrac)
-    Z = Z*tfrac
+    # Renormalize to match with coverage:
+    Z = Z/np.sum(Z)
+    Z = Z * charge*1e-12*6.022e23 * np.pi*(NP_diameter*1e-6/2)**2/cell_area*100
     maximum = np.max(Z)
-    average = np.average(Z[ X**2 + Y**2 <= r_mask**2 ])
-    print('Average inside dep area (per raster run): {}'.format(average*total_time/3600/sum(TIME)))
-    print('Average inside dep area (total): {}'.format(average))
+    average = np.average(Z[ (X + center_mask[0])**2 + (Y + center_mask[1])**2 <= r_mask**2 ])
+    std = np.std(Z[ (X + center_mask[0])**2 + (Y + center_mask[1])**2 <= r_mask**2 ])
+    nominal_coverage = charge*1e-12*6.022e23 * np.pi * (NP_diameter*1e-6/2)**2 / (np.pi * r_mask**2) * 100
+    correction_factor = np.sqrt(nominal_coverage/average)
+    print('')
+    print('Average inside dep area (total): {:.3f} % ± {:.3f} % (2*std)\n'.format(average, 2*std))
+    print('Maximum local coverage: {:.3f} %'.format(maximum))
+    print('Minimum local coverage: {:.3f} %'.format(np.min(Z[index_mask])))
+
+    print('Nominal coverage: {:.3f} %'.format(nominal_coverage))
+    print('"Nominal coverage" assumes all the charge is distributed evenly across the sample')
+    print('i.e. within "r_mask". This gives a correction factor for the coverage calculator plugin')
+    print('Correction factor: {}'.format(correction_factor))
+    print('leading to an effective "APERTURE_DIAMETER" = {}'.format(correction_factor*2*r_mask))
+
+    # Make 3D plot of surface
     for i in range(3):
         projection.plot(x_mask, y_mask, z_mask + maximum*float(i)/3, linewidth=2, color='k', alpha=0.6)
-    projection.plot(x_mask, y_mask, z_mask + average, linewidth=2, color='k', alpha=0.6)
-    projection.set_zlabel('Coverage (\%)')
-    #i = [X**2 + Y**2 > r_mask**2]
-    #surf = projection.plot_wireframe(X[i], Y[i], Z[i], color='b')
-    #surf = projection.scatter(X[i], Y[i], zs=Z[i], c='b', s=5)
-    i =  [X**2 + Y**2 <= r_mask**2]
+    projection.plot(x_mask, y_mask, z_mask + average, linewidth=2, color='g')
+    projection.set_zlabel('Relative Coverage (%)')
+    i =  X**2 + Y**2 <= r_mask**2
     j1 = np.array([X**2 + Y**2 > r_mask**2])
     j2 = np.array([X**2 + Y**2 < 6**2])
     j = j1[0] == j2[0]
-    #surf = projection.scatter(X[i], Y[i], zs=Z[i], c='r', s=15)
-    #surf2 = projection.scatter(X[j], Y[j], zs=Z[j], c='b', s=20)
-    #surf = projection.plot_wireframe(X[i], Y[i], Z[i], color='r')
-    surf3 = projection.plot_surface(X, Y, Z, cmap=cm.hot, vmin=20, vmax=40)
+    surf3 = projection.plot_surface(X, Y, Z, cmap=cm.hot, vmin=average-3*std, vmax=average+3*std)
     for ax in [sketch, projection]:
         ax.set_xlabel('x (mm)')
         ax.set_ylabel('y (mm)')
         ax.set_title(filename)
     fig.colorbar(surf3)
 
+    # Make contour plot of coverage
     plt.figure()
-    plt.contourf(X, Y, Z, [5, 10, 15, 20, 25, 30, 35, 40])
+    plt.contourf(X, Y, Z)
     plt.colorbar()
-    if 1:
-        step = 10/2
-        plt.plot([-step,-step,step,step,-step], [-step,step,step,-step,-step], 'k-', linewidth=2)
+    plt.plot(x_mask, y_mask, 'k-', linewidth=2)
     try:
         plt.axis('equal')
     except NotImplementedError:
         pass
 
     total_intensity = np.sum(Z)
-    in_mask_intensity = np.sum(Z[X**2 + Y**2 <= r_mask**2])
-    print('Percent of charge within mask: {} %'.format(in_mask_intensity/total_intensity*100))
-    plt.show()
+    in_mask_intensity = np.sum(Z[index_mask])
+    print('Percent of charge within mask: {:.3f} %'.format(in_mask_intensity/total_intensity*100))
+    if ascii:
+        output_name = filename.split('.')[0] + '_' + mood
+        print_ascii(Z, output_name)
+    if plot:
+        plt.show()
+
