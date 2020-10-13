7daadf19e56d102f187c8ffcbf8d7e701b8655db
Robert Jensen <jensen.robert@gmail.com>
2020-10-13 09:24:28 +0100

    First version of driver for ocs-3l oxygen sensor.
    So far only the bit-banged driver is here, converting to uart
    should be very simple from here.

---
+++ PyExpLabSys/drivers/ocs_3l_oxygen_sensor.py
@@ -0,0 +1,149 @@
+"""
+Bit-banged driver for oxygen sensor. Can also work with
+a proper 3.3V uart.
+
+Notice that this sensor is highly cross-sensitive to other
+gasses than oxygen.
+"""
+import time
+
+import wiringpi as wp
+
+
+class OCS3L(object):
+    def __init__(self):
+        wp.wiringPiSetup()
+        wp.pinMode(1, 0)
+        time.sleep(0.1)
+
+    # Todo: This most likely does not work for the final checksum
+    # calculation, that contains 9 digits
+    def _checksum(self, bool_list):
+        value = 0
+        for n in range(0, 8):
+            value += bool_list[7 - n] * 2**n
+            # value = value % 256
+        return value
+
+    def _uart_read(self):
+        # Not implemented
+        pass
+
+    def _bitbanged_read(self):
+        """
+        Read the uart with bit-banged gpio, this could of course be easier
+        implemented with a proper uart if one has the luxury of such a
+        gadget available.
+        """
+        while wp.digitalRead(0) == 1:
+            pass
+
+        time.sleep(0.4)
+
+        while wp.digitalRead(0) == 1:
+            pass
+
+        # Now we are certain that we have reached the signal
+
+        raw_bits = []
+        value = True
+        # Todo: To optimize this, look for the second 9x0 1x1 9x0 marker.
+        t = time.perf_counter()
+        for i in range(0, 150):
+            while wp.digitalRead(0) == value:
+                pass  # Keep reading until bit changes
+            raw_bits.append((value, time.perf_counter() - t))
+            value = not value
+            if raw_bits[-1][1] > 0.1:
+                break
+
+        parsed_bytes = []
+
+        old_bit_time = 0
+        for bit in raw_bits:
+            bit_time = bit[1] - old_bit_time
+            old_bit_time = bit[1]
+            if bit_time > 0.01:
+                break
+
+            # At 9600 baud, a bit is 104microseconds
+            number_of_bytes = round((bit_time * 1e6) / 104)
+            for i in range(0, number_of_bytes):
+                parsed_bytes.append(1 if bit[0] else 0)
+
+        # These bit-patterns are fixed, errors here would indicate a
+        # read error in the bit-banged read.
+        sanity_check = (
+            parsed_bytes[8:0:-1] == [0, 0, 0, 1, 0, 1, 1, 0] and
+            parsed_bytes[18:10:-1] == [0, 0, 0, 0, 1, 0, 0, 1] and
+            parsed_bytes[28:20:-1] == [0, 0, 0, 0, 0, 0, 0, 1] and
+            parsed_bytes[58:50:-1] == [0, 0, 0, 0, 0, 0, 0, 0] and
+            # According to datsheet, this should be 0x63, but turns out
+            # to be always 0x00
+            parsed_bytes[68:60:-1] == [0, 0, 0, 0, 0, 0, 0, 0] and
+            parsed_bytes[98:90:-1] == [0, 0, 0, 0, 0, 0, 0, 0] and
+            parsed_bytes[108:100:-1] == [0, 0, 0, 0, 0, 0, 0, 0]
+        )
+        if not sanity_check:
+            parsed_bytes = None
+        return parsed_bytes
+
+    def _read_oxygen(self, parsed_bytes):
+        concentration_raw = parsed_bytes[38:30:-1] + parsed_bytes[48:40:-1]
+        concentration = 0
+        for n in range(0, 16):
+            concentration += concentration_raw[15 - n] * 2**n
+        concentration = concentration / 10.0
+        return concentration
+
+    def _read_temperature(self, parsed_bytes):
+        temperature = 0
+        temp_raw = parsed_bytes[78:70:-1] + parsed_bytes[88:80:-1]
+        for n in range(0, 16):
+            temperature += temp_raw[15 - n] * 2**n
+        temperature = temperature / 10.0
+        return temperature
+
+    def _check_full_checksum(self, parsed_bytes):
+        expected_checksum = 0
+        for val in [
+                0x16, 0x09, 0x01,
+                self._checksum(parsed_bytes[38:30:-1]),
+                self._checksum(parsed_bytes[48:40:-1]),
+                self._checksum(parsed_bytes[78:70:-1]),
+                self._checksum(parsed_bytes[88:80:-1])
+        ]:
+            expected_checksum += val
+            expected_checksum = expected_checksum % 256
+        expected_checksum = 256 - expected_checksum
+
+        checksum = list(reversed(parsed_bytes[110:]))
+        filled_checksum = (9 - len(checksum)) * [1] + checksum
+        success = self._checksum(filled_checksum) == expected_checksum
+        # print(hex(expected_checksum), hex(self._checksum(filled_checksum)))
+        return success
+
+    def read_oxygen_and_temperature(self):
+        parsed_bytes = self._bitbanged_read()
+        if parsed_bytes is None:
+            return None
+
+        checksum = self._check_full_checksum(parsed_bytes)
+        if not checksum:
+            return None
+
+        concentration = self._read_oxygen(parsed_bytes)
+        temperature = self._read_temperature(parsed_bytes)
+
+        # msg = 'Oxygen concentration: {}%, Temperature: {}C. Checksum: {}'
+        # print(msg.format(concentration, temperature, checksum))
+        return(concentration, temperature)
+
+
+if __name__ == '__main__':
+    oxygen_sensor = OCS3L()
+
+    while True:
+        readout = oxygen_sensor.read_oxygen_and_temperature()
+        if readout is not None:
+            print('Oxygen: {}%. Temperature: {}C'.format(readout[0], readout[1]))
