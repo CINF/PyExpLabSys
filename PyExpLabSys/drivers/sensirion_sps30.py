58e535e79578b03be14306b117c9fddff25590c3
Robert Jensen <jensen.robert@gmail.com>
2021-06-01 21:23:34 +0200

    Slightly more advanced error handling.

--- PyExpLabSys/drivers/sensirion_sps30.py
+++ PyExpLabSys/drivers/sensirion_sps30.py
@@ -2,6 +2,7 @@
 import time
 import serial
 import struct
+import logger
 
 
 class SensirionSPS30():
@@ -27,6 +28,7 @@ class SensirionSPS30():
         return check_sum
 
     def comm(self, command, data=[]):
+        read_success = True
         length = len(data)
         first = [
             0x7E,  # Start
@@ -41,7 +43,10 @@ class SensirionSPS30():
         actual_command = first + last
         self.serial.write(actual_command)
 
-        assert ord(self.serial.read(1)) == 0x7e
+        if not ord(self.serial.read(1)) == 0x7e:
+            read_success = False
+            logger.warning('First char should have been 0x7e')
+
         chars = bytes()
         char = self.serial.read(1)
         while not ord(char) == 0x7E:
@@ -54,12 +59,16 @@ class SensirionSPS30():
         chars = chars.replace(b'\x7D\x31', b'\x11')
         chars = chars.replace(b'\x7D\x33', b'\x13')
 
-        assert chars[0] == 0  # Address, 0x7E is already stripped
+        if not  chars[0] == 0:
+            # Address, 0x7E is already stripped
         expected_checksum = self._calculate_checksum(chars[:-1])
         assert expected_checksum == chars[-1]
 
         state = chars[2]
-        if state > 0:
+        if state == 0x43:
+            print('SPS30: Command {} not allowd in current state'.format(
+                hex(command)))
+        elif state > 0:
             print('SPS30: Error state: {}'.format(state))
 
         # Do not return header and checksum
@@ -94,7 +103,8 @@ class SensirionSPS30():
         shows up, this is just an on/off indicator.
         """
         command = 0xD2  # Device status
-        data = [0]  # Do not clear after reading
+        # data = [0]  # Do not clear after reading
+        data = [1]  # Clear after reading
         reply = self.comm(command, data)
         status_ok = True
         for byte in reply:
@@ -113,21 +123,20 @@ class SensirionSPS30():
 
     def read_measurement(self):
         command = 0x03  # Read
-        data = self.comm(command)
-
+        # Todo, assertion error is a bit primitive, we should make
+        # a dedicated exception.
+        error = 0
+        while -1 < error < 50:
+            try:
+                data = self.comm(command)
+                error = -1
+            except AssertionError:
+                error +=1
+        if error > 0:
+            return None
         # Unpack 10 big-endian floats - 40 bytes in total
         parsed_data = struct.unpack(">ffffffffff", data)
-        print('MC PM1.0 [μg/m3]: {:.2f}'.format(parsed_data[0]))
-        print('MC PM2.5 [μg/m3]: {:.2f}'.format(parsed_data[1]))
-        print('MC PM4.0 [μg/m3]: {:.2f}'.format(parsed_data[2]))
-        print('MC PM10 [μg/m3]: {:.2f}'.format(parsed_data[3]))
-        print('NC NM0.5 [#/cm3]: {:.2f}'.format(parsed_data[4]))
-        print('NC NM1.0 [#/cm3]: {:.2f}'.format(parsed_data[5]))
-        print('NC NM2.5 [#/cm3]: {:.2f}'.format(parsed_data[6]))
-        print('NC NM4.0 [#/cm3]: {:.2f}'.format(parsed_data[7]))
-        print('NC NM10 [#/cm3]: {:.2f}'.format(parsed_data[8]))
-        print('Typical size: [μm]: {:.2f}'.format(parsed_data[9]))
-
+        return parsed_data
 
 if __name__ == '__main__':
     dust_sensor = SensirionSPS30()
@@ -140,4 +149,14 @@ if __name__ == '__main__':
 
     for i in range(0, 3):
         time.sleep(1.1)  # Todo: Read status to know if measurement is ready
-        dust_sensor.read_measurement()
+        parsed_data = dust_sensor.read_measurement()
+        print('MC PM1.0 [μg/m3]: {:.2f}'.format(parsed_data[0]))
+        print('MC PM2.5 [μg/m3]: {:.2f}'.format(parsed_data[1]))
+        print('MC PM4.0 [μg/m3]: {:.2f}'.format(parsed_data[2]))
+        print('MC PM10 [μg/m3]: {:.2f}'.format(parsed_data[3]))
+        print('NC NM0.5 [#/cm3]: {:.2f}'.format(parsed_data[4]))
+        print('NC NM1.0 [#/cm3]: {:.2f}'.format(parsed_data[5]))
+        print('NC NM2.5 [#/cm3]: {:.2f}'.format(parsed_data[6]))
+        print('NC NM4.0 [#/cm3]: {:.2f}'.format(parsed_data[7]))
+        print('NC NM10 [#/cm3]: {:.2f}'.format(parsed_data[8]))
+        print('Typical size: [μm]: {:.2f}'.format(parsed_data[9]))
