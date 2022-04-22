4662f1991f561aae107d1fe09ec29309f4d7436f
Jakob Ejler Sørensen <lillebror.jakobes@gmail.com>
2022-04-22 17:00:01 +0200

    Add livesocket to data_logger. Add AUTOSTART.

--- machines/rasppi08/data_logger.py
+++ machines/rasppi08/data_logger.py
@@ -4,18 +4,19 @@ import logging
 import time
 from PyExpLabSys.common.database_saver import ContinuousDataSaver
 from PyExpLabSys.common.value_logger import LoggingCriteriumChecker
-from PyExpLabSys.common.sockets import DateDataPullSocket
+from PyExpLabSys.common.sockets import DateDataPullSocket, LiveSocket
 import PyExpLabSys.drivers.omega_cni as omega_cni
 import PyExpLabSys.drivers.NGC2D as NGC2D
 import credentials
 
 class Reader(threading.Thread):
-    def __init__(self, iongauge, omega, codenames, pullsocket):
+    def __init__(self, iongauge, omega, codenames, pullsocket, livesocket):
         threading.Thread.__init__(self)
         self.iongauge = iongauge
         self.omega = omega
         self.codenames = codenames
         self.pullsocket = pullsocket
+        self.livesocket = livesocket
         self.values = dict()
         for codename in codenames:
             self.values[codename] = None
@@ -25,17 +26,22 @@ class Reader(threading.Thread):
         """Main thread activity"""
         while not self.quit:
             # Pressure
-            pressure = float(self.iongauge.ReadPressure())
+            try:
+                pressure = float(self.iongauge.ReadPressure())
+            except ValueError:
+                pressure = None
             if pressure == -1:
                 print('Pressure gauge off - log as atmosphere')
                 pressure = 1000
             self.values[self.codenames[0]] = pressure
             self.pullsocket.set_point_now(self.codenames[0], pressure)
+            self.livesocket.set_point_now(self.codenames[0], pressure)
 
             # Temperature
             temperature = self.omega.read_temperature()
             self.values[self.codenames[1]] = temperature
             self.pullsocket.set_point_now(self.codenames[1], temperature)
+            self.livesocket.set_point_now(self.codenames[1], temperature)
             time.sleep(0.2)
         else:
             self.pullsocket.stop()
@@ -54,7 +60,12 @@ if __name__ == '__main__':
     codenames = ['omicron_nanobeam_pressure',
                  'omicron_nanobeam_temperature']
     pullsocket = DateDataPullSocket('nanobeam_state', codenames, timeouts=4, port=9000)
-    reader = Reader(iongauge, omega, codenames, pullsocket)
+    pullsocket.start()
+
+    livesocket = LiveSocket('nanobeam_live', codenames)
+    livesocket.start()
+
+    reader = Reader(iongauge, omega, codenames, pullsocket, livesocket)
     reader.daemon = True
     reader.start()
     print('Codenames used: {}'.format(codenames))
@@ -63,7 +74,7 @@ if __name__ == '__main__':
     criterium_checker = LoggingCriteriumChecker(
         codenames=codenames,
         types=['log', 'lin'],
-        criteria=[0.15, 0.5],
+        criteria=[0.15, 0.9],
         time_outs=[600, 600],
         )
 
@@ -82,14 +93,17 @@ if __name__ == '__main__':
     while run:
         try:
             time.sleep(1)
-            for codename in codenames:
-                value = reader.values[codename]
-                if value is None:
-                    print('NONE encountered - check equiptment!')
-                else:
-                    if criterium_checker.check(codename, value):
-                        print(codename, value, (time.time() - t0)/60.)
-                        db_logger.save_point_now(codename, value)
+            if reader.isAlive():
+                for codename in codenames:
+                    value = reader.values[codename]
+                    if value is None:
+                        print('NONE encountered - check equiptment!')
+                    else:
+                        if criterium_checker.check(codename, value):
+                            print(codename, value, (time.time() - t0)/60.)
+                            db_logger.save_point_now(codename, value)
+            else:
+                raise KeyboardInterrupt('Reader has stopped')
         except:
             print('Stopping everything:')
             run = False
