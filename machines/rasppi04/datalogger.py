517b140cf4fa3742bdb18d130b48ad34adb5f67d
Jakob Ejler Sørensen <lillebror.jakobes@gmail.com>
2022-04-26 15:05:01 +0000

    volvo: Add temporary pressure logger as xy

--- machines/rasppi04/datalogger.py
+++ machines/rasppi04/datalogger.py
@@ -3,6 +3,9 @@ from __future__ import print_function
 import threading
 import time
 from PyExpLabSys.common.database_saver import ContinuousDataSaver
+### <TMP ###
+from PyExpLabSys.common.database_saver import DataSetSaver, CustomColumn
+### TMP> ###
 from PyExpLabSys.common.sockets import DateDataPullSocket
 from PyExpLabSys.common.sockets import LiveSocket
 from PyExpLabSys.common.value_logger import ValueLogger
@@ -11,6 +14,10 @@ import PyExpLabSys.drivers.agilent_34972A as agilent_34972A
 from PyExpLabSys.common.supported_versions import python2_and_3
 import credentials
 python2_and_3(__file__)
+
+# Set to False to not save pressure in xy_table high res
+SAVE_PRESSURE = False
+SAVE_HOURS = 48
 
 class MuxReader(threading.Thread):
     """ Analog reader """
@@ -41,15 +48,19 @@ class PressureReader(threading.Thread):
         threading.Thread.__init__(self)
         self.xgs = xgs
         self.pressure = None
+        self.rough = None
         self.quit = False
         self.ttl = 20
 
-    def value(self):
+    def value(self, channel=0):
         """ Read the pressure """
         self.ttl = self.ttl - 1
         if self.ttl < 0:
             self.quit = True
-        return self.pressure
+        if channel == 0:
+            return self.pressure
+        elif channel == 1:
+            return self.rough
 
     def run(self):
         while not self.quit:
@@ -61,6 +72,11 @@ class PressureReader(threading.Thread):
             except IndexError:
                 print("av")
                 self.pressure = 0
+            try:
+                self.rough = press[1]
+            except IndexError:
+                print("Rough read error")
+                self.rough = 0
 
 def main():
     """ Main code """
@@ -78,12 +94,14 @@ def main():
     time.sleep(2.5)
 
     #codenames = ['volvo_pressure', 'volvo_temperature']
-    codenames = ['volvo_pressure']
+    codenames = ['volvo_pressure', 'volvo_rough_pressure']
     loggers = {}
-    loggers[codenames[0]] = ValueLogger(pressure, comp_val=0.1, comp_type='log', low_comp=1e-9)
+    loggers[codenames[0]] = ValueLogger(pressure, comp_val=0.1, comp_type='log', low_comp=1e-9, channel=0)
     loggers[codenames[0]].start()
-    #loggers[codenames[1]] = ValueLogger(analog_measurement, comp_val=0.5, comp_type='lin')
-    #loggers[codenames[1]].start()
+    loggers[codenames[1]] = ValueLogger(pressure, comp_val=0.1, comp_type='log', low_comp=1e-11, channel=1)
+    loggers[codenames[1]].start()
+    #loggers[codenames[2]] = ValueLogger(analog_measurement, comp_val=0.5, comp_type='lin')
+    #loggers[codenames[2]].start()
 
     socket = DateDataPullSocket('Volvo data logger', codenames, timeouts=1.0)
     socket.start()
@@ -97,13 +115,49 @@ def main():
                                     measurement_codenames=codenames)
     db_logger.start()
 
+    ### <TMP ###
+    if SAVE_PRESSURE:
+        tmp_logger = DataSetSaver(
+            'measurements_dummy',
+            'xy_values_dummy',
+            credentials.dummyuser,
+            credentials.dummypasswd,
+        )
+        tmp_logger.start()
+
+        time.sleep(2)
+
+        t0 = time.time()
+        meta_data = {
+            "Time": CustomColumn(t0, "FROM_UNIXTIME(%s)"),
+            "comment": 'Main chamber pressure during depositions',
+            "type": 71, # 12 or 71 for pressure
+            "sem_voltage": -1,
+            "preamp_range": -1,
+        }
+        tmp_logger.add_measurement("pressure", meta_data)
+        #time.sleep(2)
+    ### TMP> ###
+
     #while pressure.isAlive() and analog_measurement.isAlive():
+    old_pressure = -1
+    last_save = 0
     while True:
         time.sleep(0.25)
         for name in codenames:
             value = loggers[name].read_value()
             socket.set_point_now(name, value)
             livesocket.set_point_now(name, value)
+            ### <TMP ###
+            if name == 'volvo_pressure' and SAVE_PRESSURE:
+                t_now = time.time() - t0
+                if value != old_pressure or t_now - last_save > 5:
+                    if t_now < SAVE_HOURS * 3600:
+                        print(t_now, value)
+                        tmp_logger.save_point('pressure', (t_now, value))
+                        old_pressure = value
+                        last_save = t_now
+            ### TMP> ###
             if loggers[name].read_trigged():
                 print(value)
                 db_logger.save_point_now(name, value)
