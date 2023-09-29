8da738a780cc3116fd48eedf6810a36d4d433546
Jakob Ejler Sørensen <lillebror.jakobes@gmail.com>
2023-09-29 13:14:14 +0200

    Return pylint to bootstrap

--- bootstrap/bootstrap_linux.bash
+++ bootstrap/bootstrap_linux.bash
@@ -22,7 +22,7 @@ declare -a apt3=("libpython3-dev" "python3-dev")
 # packages to be installed by pip
 pip3packages="minimalmodbus pyusb python-usbtmc pyserial pyyaml mysqlclient"
 # Put packages into this array, whose installation sometimes fail
-declare -a pip3problempackages=("")
+declare -a pip3problempackages=("pylint")
 
 # These lines will be added to the ~/.bashrc file, to modify the PATH and
 # PYTHONPATH for PyExpLabSys usage
