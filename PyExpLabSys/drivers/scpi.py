b5eabbc3fe078bebd2cc19ba357d3de51a602228
Robert Jensen <jensen.robert@gmail.com>
2023-10-31 11:14:54 +0100

    Allwo different encodings, imporve network performance.
    Also includes a special case for communicating with 2182a via
    6220. This should be abstracted somehow.

--- PyExpLabSys/drivers/scpi.py
+++ PyExpLabSys/drivers/scpi.py
@@ -21,28 +21,19 @@ LOGGER.addHandler(logging.NullHandler())
 
 class SCPI(object):
     """ Driver for scpi communication """
-
-    def __init__(
-        self,
-        interface,
-        device='',
-        tcp_port=5025,
-        hostname='',
-        baudrate=9600,
-        visa_string='',
-        gpib_address=None,
-        line_ending='\r',
-    ):
+    def __init__(self, interface, device='', tcp_port=5025, hostname='',
+                 baudrate=9600, visa_string='', gpib_address=None, line_ending='\r',
+                 encoding='ascii'):
         self.device = device
         self.line_ending = line_ending
         self.interface = interface
+        self.encoding = encoding
         if self.interface == 'file':
             self.comm_dev = open(self.device, 'w')
             self.comm_dev.close()
         if self.interface == 'serial':
-            self.comm_dev = serial.Serial(
-                self.device, baudrate, timeout=2, xonxoff=True
-            )
+            self.comm_dev = serial.Serial(self.device, baudrate,
+                                          timeout=3, xonxoff=True)
         if self.interface == 'lan':
             self.comm_dev = telnetlib.Telnet(hostname, tcp_port)
         if self.interface == 'usbtmc':
@@ -58,42 +49,56 @@ class SCPI(object):
         """ Implements actual communication with SCPI instrument """
         return_string = ""
         if self.interface == 'file':
-            self.comm_dev = open(self.device, 'w')
-            self.comm_dev.write(command)
-            time.sleep(0.02)
+            self.comm_dev = open(self.device, 'wb')
+            self.comm_dev.write(bytes(command + self.line_ending, self.encoding))
+            self.comm_dev.flush()
             self.comm_dev.close()
-            time.sleep(0.05)
-            if '?' in command:
-                self.comm_dev = open(self.device, 'r')
-                return_string = self.comm_dev.readline()
+            if ('?' in command) or (expect_return is True):
+                self.comm_dev = open(self.device, 'rb')
+                time.sleep(0.002)
+                t = time.time()
+                while len(return_string) == 0:
+                    dt = time.time() - t
+                    if dt > 1:
+                        raise ValueError
+                    return_string = self.comm_dev.read(1)
+                return_string += self.comm_dev.readline()
+                return_string = return_string.decode(self.encoding)
                 self.comm_dev.close()
-        command_text = command + self.line_ending
 
+        command_text = command + self.line_ending
         if self.interface == 'serial':
             self.comm_dev.write(command_text.encode('ascii'))
             if command.endswith('?') or (expect_return is True):
-                return_string = ''.encode('ascii')
+                return_string = ''.encode(self.encoding)
                 while True:
                     next_char = self.comm_dev.read(1)
+                    # print(next_char)
                     if ord(next_char) == ord(self.line_ending):
                         break
                     return_string += next_char
-                return_string = return_string.decode()
+                return_string = return_string.decode(self.encoding)
 
         if self.interface == 'lan':
             lan_time = time.time()
-            self.comm_dev.write(command_text.encode('ascii'))
-            if (command.find('?') > -1) or (expect_return is True):
-                return_string = self.comm_dev.read_until(
-                    chr(10).encode('ascii'), 2
-                ).decode()
+            command_text = command + '\n'
+            # print('Command text ', repr(command_text))
+            self.comm_dev.write(command_text.encode(self.encoding))
+            if command.endswith('?') or (expect_return is True):
+                raw = self.comm_dev.expect([b'\n'], 2)
+                return_string = raw[2].decode().strip()
+
+                # ENT? return an extra newlinw from the inline reply
+                if command_text.find('ENT?'):
+                    # extra_n = self.comm_dev.read_until(b'\n', 0.1).decode()
+                    self.comm_dev.read_until(b'\n', 0.1).decode()
+
             LOGGER.info('Return string length: ' + str(len(return_string)))
-            LOGGER.info(
-                'lan_time for coomand '
-                + command_text.strip()
-                + ': '
-                + str(time.time() - lan_time)
-            )
+            LOGGER.info('lan_time for command ' + command_text.strip() +
+                        ': ' + str(time.time() - lan_time))
+            LOGGER.info('Return string length: ' + str(len(return_string)))
+            LOGGER.info('lan_time for command ' + command_text.strip() +
+                        ': ' + str(time.time() - lan_time))
 
         if self.interface == 'usbtmc':
             if command.find('?') > -1:
@@ -104,7 +109,7 @@ class SCPI(object):
 
         if self.interface == 'gpib':
             self.comm_dev.write(command_text)
-            if (command.find('?') > -1) or expect_return:
+            if command.endswith('?') or expect_return:
                 return_string = self.comm_dev.read().strip().decode()
         return return_string
 
