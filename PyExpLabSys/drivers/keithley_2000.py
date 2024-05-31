fb78f5310fa909123783c06d9db918eb240d9b19
Robert Jensen <jensen.robert@gmail.com>
2024-05-31 11:49:33 +0200

    Change communication to pyvisa
    The old scpi driver is getting very old and pyvisa seems to be
    a much more well supported kid on the block.

--- PyExpLabSys/drivers/keithley_2000.py
+++ PyExpLabSys/drivers/keithley_2000.py
@@ -1,35 +1,35 @@
 """ Simple driver for Keithley 2000 DMM """
 import time
-from PyExpLabSys.drivers.scpi import SCPI
+
+import pyvisa
 
 
-class Keithley2000(SCPI):
+class Keithley2000:
     """
     Simple driver for Keithley 2000 DMM
     """
 
-    def __init__(
-        self, interface, hostname='', device='', baudrate=9600, gpib_address=None
-    ):
+    def __init__(self, interface, device='', baudrate=9600, gpib_address=None):
+        rm = pyvisa.ResourceManager('@py')
         if interface == 'serial':
-            SCPI.__init__(
-                self,
-                interface=interface,
-                device=device,
-                baudrate=baudrate,
-                line_ending='\n',
+            conn_string = 'ASRL{}::INSTR'.format(device)
+            self.instr = rm.open_resource(conn_string)
+            self.instr.set_visa_attribute(
+                pyvisa.constants.VI_ATTR_ASRL_FLOW_CNTRL,
+                pyvisa.constants.VI_ASRL_FLOW_XON_XOFF,
             )
-            self.comm_dev.timeout = 2
-            self.comm_dev.rtscts = False
-            self.comm_dev.xonxoff = False
+            self.instr.read_termination = '\n'
+            self.instr.write_termination = '\n'
+            self.instr.baud_rate = baudrate
         if interface == 'gpib':
-            SCPI.__init__(self, interface=interface, gpib_address=gpib_address)
+            pass
 
     def set_bandwith(self, measurement='voltage:ac', bandwidth=None):
         scpi_cmd = 'SENSE:{}:DETector:BANDwidth'.format(measurement)
         if bandwidth is not None:
-            DMM.scpi_comm(scpi_cmd + ' {}'.format(bandwidth))
-        value_raw = DMM.scpi_comm(scpi_cmd + '?')
+            self.instr.write(scpi_cmd + ' {}'.format(bandwidth))
+        # value_raw = DMM.scpi_comm(scpi_cmd + '?')
+        value_raw = self.instr.query(scpi_cmd + '?')
         value = float(value_raw)
         return value
 
@@ -42,13 +42,15 @@ class Keithley2000(SCPI):
         if value < 0:
             value = 0
         if value == 0:
-            self.scpi_comm(':SENSE:VOLT:DC:RANGE:AUTO ON')
-            self.scpi_comm(':SENSE:VOLT:AC:RANGE:AUTO ON')
+            self.instr.write(':SENSE:VOLT:DC:RANGE:AUTO ON')
+            self.instr.write(':SENSE:VOLT:AC:RANGE:AUTO ON')
         else:
-            self.scpi_comm(':SENSE:VOLT:DC:RANGE {:.5f}'.format(value))
-            self.scpi_comm(':SENSE:VOLT:AC:RANGE {:.5f}'.format(value))
-
-        actual_range_raw = self.scpi_comm(':SENSE:VOLTAGE:AC:RANGE?')
+            self.instr.write(':SENSE:VOLT:DC:RANGE {:.5f}'.format(value))
+            self.instr.write(':SENSE:VOLT:AC:RANGE {:.5f}'.format(value))
+        # The instrument cannot process this infinitiely fast and might end up
+        # lacking behind and filling up buffers - wait a little while to prevent this
+        # time.sleep(0.4)
+        actual_range_raw = self.instr.query(':SENSE:VOLTAGE:AC:RANGE?')
         actual_range = float(actual_range_raw)
         return actual_range
 
@@ -69,16 +71,16 @@ class Keithley2000(SCPI):
                 nplc = 0.01
             if nplc > 60:
                 nplc = 60
-            self.scpi_comm(set_msg.format(nplc))
-        current_nplc = float(self.scpi_comm(read_msg))
+            self.instr.write(set_msg.format(nplc))
+        current_nplc = float(self.instr.query(read_msg))
         return current_nplc
 
     def configure_measurement_type(self, measurement_type=None):
         """Setup measurement type"""
         if measurement_type is not None:
             # todo: Ensure type is an allow type!!!!
-            self.scpi_comm(':CONFIGURE:{}'.format(measurement_type))
-        actual = self.scpi_comm(':CONFIGURE?')
+            self.instr.write(':CONFIGURE:{}'.format(measurement_type))
+        actual = self.instr.query(':CONFIGURE?')
         return actual
 
     def set_trigger_source(self, external):
@@ -88,20 +90,20 @@ class Keithley2000(SCPI):
         otherwise immediate triggering will be chosen.
         """
         if external:
-            self.scpi_comm('TRIGGER:SOURCE External')
+            self.instr.write('TRIGGER:SOURCE External')
         else:
-            self.scpi_comm('TRIGGER:SOURCE Immediate')
+            self.instr.write('TRIGGER:SOURCE Immediate')
         return external
 
     def read_dc_voltage(self):
         """Read a voltage"""
-        raw = self.scpi_comm(':MEASURE:VOLTAGE:DC?')
+        raw = self.instr.query(':MEASURE:VOLTAGE:DC?')
         voltage = float(raw)
         return voltage
 
     def read_ac_voltage(self):
         """Read a voltage"""
-        raw = self.scpi_comm(':MEASURE:VOLTAGE:AC?')
+        raw = self.instr.query(':MEASURE:VOLTAGE:AC?')
         voltage = float(raw)
         return voltage
 
@@ -114,41 +116,44 @@ class Keithley2000(SCPI):
                 # Todo: This is not good enough
                 print('Keithley 2000 TIMEOUT!')
                 break
-        raw = self.scpi_comm(':DATA?')
+        raw = self.instr.query(':DATA?')
         voltage = float(raw)
         return voltage
 
     def measurement_available(self):
-        meas_event = int(self.scpi_comm('STATUS:MEASUREMENT:EVENT?'))
+        # todo: Check if pyvisa has some neat trick for this
+        meas_event = int(self.instr.query('STATUS:MEASUREMENT:EVENT?'))
         mav_bit = 5
         mav = (meas_event & 2 ** mav_bit) == 2 ** mav_bit
         return mav
 
 
 if __name__ == '__main__':
-    GPIB = 16
-    DMM = Keithley2000(interface='gpib', gpib_address=GPIB)
-    DMM.set_trigger_source(external=True)
-    exit()
+    device = '/dev/serial/by-id/usb-FTDI_Chipi-X_FT6EYK1T-if00-port0'
+    DMM = Keithley2000(
+        interface='serial',
+        device=device,
+        baudrate=9600,
+    )
+    print(repr(DMM.instr.query("*IDN?")))
+    print(DMM.set_bandwith())
+
+    # Errors:
+    # +802 - RS232 overrun
+    # -113 - Undefined header
+    DMM.set_trigger_source(external=False)
+
+    DMM.set_range(0)
+    for _ in range(0, 10):
+        # print()
+        # print(DMM.read_software_version())
+        # DMM.set_trigger_source(external=False)
+        voltage = DMM.next_reading()
+        print(voltage)
 
     # TODO! Something changes with the configuration when this
     # command is called, measurement is much slower and
     # NPLC command fails?!?!
     # print(DMM.configure_measurement_type('volt:ac'))
-
-    DMM.set_range(0.1)
-    print(DMM.set_integration_time(2))
-    # print(DMM.set_bandwith())
-    # print(DMM.set_integration_time(10))
-    print(DMM.read_dc_voltage())
-
-    for i in range(0, 20):
-        # time.sleep(0.05)
-        t = time.time()
-        # meas_event = DMM.scpi_comm('STATUS:MEASUREMENT:EVENT?')
-        # print(bin(int(meas_event)))
-        while not DMM.measurement_available():
-            time.sleep(0.05)
-        reading = DMM.next_reading()
-        dt = time.time() - t
-        print('Time: {:.2f}ms. DC {:.3f}mV'.format(dt * 1e3, reading * 1e3))
+    # print(DMM.read_dc_voltage())
+    # print(DMM.read_dc_voltage())
