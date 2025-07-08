4e9f65aaa2b621ce92d2a81fdc7d4e662cf2af28
Robert Jensen <jense.robert@gmail.com>
2025-07-08 12:04:34 +0200

    Improved error handling

--- PyExpLabSys/apps/tof/tof_run.py
+++ PyExpLabSys/apps/tof/tof_run.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import time
 import pickle
@@ -64,8 +65,17 @@ class MCSRunner:
             tdms_writer.write_segment([channel])
 
     def _read_data_file(self):
-        datafile = open('TEST.mpa', 'r')
-        lines = datafile.readlines()
+        try:
+            datafile = open('TEST.mpa', 'r')
+            lines = datafile.readlines()
+            datafile.close()
+            os.remove('TEST.mpa')
+        except FileNotFoundError:
+            print('Did not find TEST.mpa - skipping the measurement')
+            self.end_measurement()
+            self.measurement_running = True
+            self._init_mcs6a()
+            return -1
 
         # Configuration lines are currently not actually used
         configuration_lines = []
@@ -91,9 +101,16 @@ class MCSRunner:
         self.spectrum += data
         self._update_data()
 
-        assert starts > 0
+        if not starts > 0:
+            # File exists but no measurements has been takens
+            ic('No starts in file!')
+            return -1
         self.starts += starts
+        msg = 'Starts in this file: {}. Total starts: {}'.format(starts, self.starts)
+        print(msg)
         total_count = sum(self.spectrum)
+        msg = 'Counts in this file: {}. Total counts: {}'.format(sum(data), total_count)
+        print(msg)
         self.pullsocket.set_point_now('total_count', total_count)
         self.pullsocket.set_point_now('starts', self.starts)
         ic(self.starts, total_count)
@@ -105,16 +122,19 @@ class MCSRunner:
     def _perform_single_measurement(self, iteration_time):
         print('Perform a measurement for {}s'.format(iteration_time))
         self._run_command('e')
-        time.sleep(0.2)
+        # time.sleep(0.2)
+        time.sleep(1.0)
         self._run_command('s')
         time.sleep(iteration_time)
         self._run_command('h')
-        time.sleep(0.2)
+        # time.sleep(0.3)
+        time.sleep(1.0)
         # Save the file
         self._run_command('t')
-        time.sleep(0.2)
+        # time.sleep(0.3)
+        time.sleep(1.0)
 
-    def measure(self, iterations, iteration_time=5):
+    def measure(self, iterations, iteration_time=15):
         if self.measurement_running:
             return False
         self._init_mcs6a()
@@ -126,6 +146,8 @@ class MCSRunner:
         self.end_measurement()
 
     def end_measurement(self):
+        self._run_command('h')
+        time.sleep(1)
         self._run_command('e')
         time.sleep(1)
         self._run_command('q')
@@ -135,7 +157,7 @@ class MCSRunner:
 
     def run(self):
         while self.running:
-            time.sleep(0.5)
+            time.sleep(1)
             print(self.starts)
             qsize = self.pushsocket.queue.qsize()
             while qsize > 0:
