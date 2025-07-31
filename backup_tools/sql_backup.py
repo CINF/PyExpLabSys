eb85ac2d846d7bd886b0ce1ae56051068bccd962
Robert Jensen <jensen.robert@gmail.com>
2025-07-31 14:33:43 +0000

    Add internal logging, backup all data, enhance metadata

--- backup_tools/sql_backup.py
+++ backup_tools/sql_backup.py
@@ -1,10 +1,12 @@
 import gzip
 import json
 import time
+import logging
 import pathlib
 import datetime
-
 from subprocess import Popen, PIPE
+
+from PyExpLabSys.common.utilities import get_logger
 
 import pymysql
 
@@ -12,15 +14,23 @@ HOST = '10.11.114.11'
 USER = 'reader'
 PASSWD = 'reader'
 DB = 'nanomadedata'
+LOGGER = get_logger(
+    'SQL backup',
+    level='info',
+    file_log=True,
+    file_name='sql_backup.log',
+    terminal_log=False,
+)
 
 
 class PyExpLabSysBackup:
     def __init__(self):
+        isotime = datetime.datetime.now().isoformat(timespec='seconds')
+        LOGGER.info('Starting backup on {}'.format(isotime))
         self.conn = pymysql.connect(host=HOST, user=USER, passwd=PASSWD, db=DB)
         self.cursor = self.conn.cursor()
         self.untreated_tables = self._find_all_tables()
         self.backed_up_tables = []
-        isotime = datetime.datetime.now().isoformat(timespec='seconds')
         self.path = pathlib.Path.cwd() / isotime
         self.path.mkdir()
         self.t_start = time.time()
@@ -56,7 +66,9 @@ class PyExpLabSysBackup:
 
     def backup_dateplots(self):
         t = time.time()
-        print('Backing up all dateplots since beginning of time')
+        msg = 'Backing up all dateplots since beginning of time'
+        LOGGER.info(msg)
+        print(msg)
         untreated_tables = self.untreated_tables.copy()
         for table in untreated_tables:
             if table == 'dateplots_descriptions':
@@ -65,14 +77,16 @@ class PyExpLabSysBackup:
                 continue
             if 'dateplot' in table:
                 stats = self.perform_backup(table, [table])
-                self.untreated_tables.remove(table)
-                self.backed_up_tables.append(table)
                 self.stats['dateplots'][table] = stats
-        print('Done in {:.0f}s'.format(time.time() - t))
+        msg = 'Backup of dateplots done in {:.0f}s'.format(time.time() - t)
+        print(msg)
+        LOGGER.info(msg)
 
     def backup_measurements(self):
         t = time.time()
-        print('Backing up all xy-measurements since beginning of time')
+        msg = 'Backing up all xy-measurements since beginning of time'
+        print(msg)
+        LOGGER.info(msg)
         # Backing up all related measurements - dateplots are also found
         # by this algorithm, but will typically be removed from
         # untreated_tables by the backup_dateplots function
@@ -82,20 +96,57 @@ class PyExpLabSysBackup:
                 name = table[table.find('_') :]
                 tables = self.find_matching_tables(name)
                 stats = self.perform_backup(table, tables)
-                self.untreated_tables.remove(table)
-                self.backed_up_tables.append(table)
                 self.stats['measurements'][name] = stats
-        print('Done in {:.0f}s'.format(time.time() - t))
+        msg = 'Backup of measurements done in {:.0f}s'.format(time.time() - t)
+        print(msg)
+        LOGGER.info(msg)
+
+    def backup_misc_tables(self):
+        t = time.time()
+        msg = 'Backing up all tables that has so far not been backed up'
+        print(msg)
+        LOGGER.info(msg)
+        untreated_tables = self.untreated_tables.copy()
+        stats = self.perform_backup('misc', untreated_tables)
+        self.stats['misc'] = stats
+        msg = 'Backup of misc done in {:.0f}s'.format(time.time() - t)
+        print(msg)
+        LOGGER.info(msg)
+
+    def backup_user(self):
+        # reader needs full access to read this:
+        # grant select on *.* to `reader`@`%`;
+        # mysqldump --opt --system=users --no-create-info  --insert-ignore --all-databases --no-data
+        pass
 
     def perform_backup(self, name, table_list):
         t = time.time()
         filename = '{}.sql.gz'.format(name)
-        print('Backing up: {} as {}'.format(table_list, filename))
+        LOGGER.info('Backing up: {} as {}'.format(table_list, filename))
         # https://stackoverflow.com/questions/3600948/python-subprocess-mysqldump-and-pipes
-        cmd = 'mysqldump --single-transaction --host={} --user={} --password={} {}'
+        # Add, --insert-ignore to ensure partial backups can be imported even a few rows are
+        # overlapping
+        cmd = 'mysqldump --single-transaction --insert-ignore --host={} --user={} --password={} {}'
         cmd = cmd.format(HOST, USER, PASSWD, DB)
+
+        # Holds max-id and number of rows for each table
+        table_stats = {}
         for table in table_list:
+            status_query = 'select max(id), count(*) from `{}` where id > 0'.format(
+                table
+            )
+            self.cursor.execute(status_query)
+            max_id, count = list(self.cursor.fetchall())[0]
+            table_stats[table] = {'max_id': max_id, 'count': count}
+            self.untreated_tables.remove(table)
+            self.backed_up_tables.append(table)
             cmd += ' ' + table
+
+        # We do not enforce table-locking between the select statements and the dump-command,
+        # however, in practice only a few miliseconds will separate the two commands - the
+        # error will be very small and always with the stats being either correct or sligltly
+        # too low. Subsequent backups will be either perfectly aligen or slightly overlapping
+        # which is not a problem due to --insert-ignore.
         p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
         backup_file = self.path / filename
         with gzip.open(self.path / filename, "wb") as f:
@@ -104,11 +155,12 @@ class PyExpLabSysBackup:
         stats = {
             'cmd': cmd,
             'tables': table_list,
+            'table_stats': table_stats,
             'time': t_total,
             'size': backup_file.stat().st_size,
             'where_clause': '',  # TODO!
         }
-        print('Done in {:.1f}s'.format(t_total))
+        LOGGER.info('Done in {:.1f}s'.format(t_total))
         return stats
 
 
@@ -116,26 +168,8 @@ if __name__ == '__main__':
     PB = PyExpLabSysBackup()
     PB.backup_dateplots()
     PB.backup_measurements()
-    exit()
-
-    # PB.write_stats()
-
-    # t = time.time()
-    # # name = 'linkam'
-    # name = 'cryostat'
-    # tables = PB.find_matching_tables(name)
-    # print('Backup {}'.format(name))
-    # PB.perform_backup(name, tables)
-    # print('Backup done - {:.0f}s'.format(time.time() - t))
-
-    # print()
+    PB.backup_misc_tables()
+    PB.write_stats()
 
     print('Untreated tables:')
     print(PB.untreated_tables)
-    print()
-    print('Backed-up_tables')
-    print(PB.backed_up_tables)
-
-
-# query = 'SELECT x*1000000, y FROM ' + XY_VALUES_TABLE
-# query += ' where measurement = ' + str(spectrum_number)
